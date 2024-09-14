24e6aa0fccfa2d018638aec953a3fe734cc6e0fd
Donova Chan Jia Jun <donovanchanjiajun2001@gmail.com>
2024-09-14 15:15:34 +0800

    feat: Utilised asynchronous rababitmq library

--- matchingService/src/index.ts
+++ matchingService/src/index.ts
@@ -3,31 +3,26 @@ import QueueMessage from "./QueueService/QueueMessage";
 import MatchRequest from "./QueueService/MatchRequest";
 
 interface QueueService {
-    sendJsonMessage(queue: string, msg: MatchRequest): void;
-    receiveMessages(queue: string, callback: (msg: QueueMessage | null) => void): void;
+    init(): Promise<void>;
+    sendJsonMessage(msg: MatchRequest): Promise<void>;
+    receiveMessages(topic: string, difficulty: string, callback: (msg: QueueMessage | null) => void): Promise<void>;
+    startConsumers(): Promise<void>;
 }
 
-var amqpService: QueueService = new AmqpService("amqp://localhost:5672");
+const testMessages: MatchRequest[] = [
+    new MatchRequest("john", "algorithm", "hard"),
+    new MatchRequest("amy", "algorithm", "medium"),
+    new MatchRequest("johhny", "graph", "easy"),
+    new MatchRequest("bob", "graph", "easy"),
+    new MatchRequest("the builder", "dp", "medium"),
+];
 
-const message: MatchRequest = {
-    userId: "John",
-    topic: "algorithm",
-    difficulty: "hard",
-}
-
-amqpService.sendJsonMessage("matchQueue", message);
-
-amqpService.receiveMessages("hello", (msg: QueueMessage | null) => {
-    let content = msg?.content.toString();
-    if (content) {
-        try {
-            var matchRequest: MatchRequest = JSON.parse(content);
-            console.log(matchRequest);
-        } catch (e) {
-            if (e instanceof Error) {
-                let name = e.message;
-                console.log(`Error occured ${name}`);
-            }
-        }
+(async () => {
+    var amqpService: QueueService = new AmqpService("amqp://localhost:5672", "gateway");
+    await amqpService.init();
+    await amqpService.startConsumers();
+    
+    for (const testMessage of testMessages) {
+        await amqpService.sendJsonMessage(testMessage);
     }
-})
+})();
