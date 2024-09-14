eddddfb5d7176d3a3cfa54e84c20e3ecc5a0da01
Donova Chan Jia Jun <donovanchanjiajun2001@gmail.com>
2024-09-14 15:15:34 +0800

    feat: Utilised asynchronous rababitmq library

--- matchingService/src/QueueService/MatchRequest.ts
+++ matchingService/src/QueueService/MatchRequest.ts
@@ -1,7 +1,25 @@
-type MatchRequest = {
-    userId: string,
-    topic: string,
-    difficulty: string,
+class MatchRequest {
+    private userId: string;
+    private topic: string;
+    private difficulty: string;
+
+    constructor(userId: string, topic: string, difficulty: string) {
+        this.userId = userId;
+        this.topic = topic;
+        this.difficulty = difficulty;
+    }
+    
+    public generateRoutingKey(): string {
+        return `${this.topic}.${this.difficulty}`;
+    }
+
+    public getTopic(): string {
+        return this.topic;
+    }
+
+    public getDifficulty(): string {
+        return this.difficulty;
+    }
 }
 
 export default MatchRequest;
