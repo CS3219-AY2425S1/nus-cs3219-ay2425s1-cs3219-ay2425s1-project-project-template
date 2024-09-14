0e543ec80ef05a103f98a7f69ab8137a5ae7469b
Donova Chan Jia Jun <donovanchanjiajun2001@gmail.com>
2024-09-14 10:24:29 +0800

    refactor: Added a layer of abstraction above amqp logic and interfaces to reduce coupling

---
+++ matchingService/src/QueueService/AmqpService.ts
@@ -0,0 +1,92 @@
+import amqp from "amqplib/callback_api";
+import { Options } from "amqplib/callback_api";
+import QueueMessage from "./QueueMessage";
+import AmqpConnectionError from "../errors/AmqpConnectionError";
+import AmqpCreateChannelError from "../errors/AmqpCreateChannelError";
+
+type ConnectCallback = (error: AmqpConnectionError | null, connection: amqp.Connection) => void;
+type CreateChannelCallback = (error: AmqpCreateChannelError | null, channel: amqp.Channel) => void;
+
+class AmqpService {
+    private connectionUrl: string;
+
+    constructor(connectionUrl: string) {
+        this.connectionUrl = connectionUrl;
+    }
+
+    private connect(callback: ConnectCallback): void {
+        amqp.connect(this.connectionUrl, callback);
+    }
+
+    private createChannel(connection: amqp.Connection, callback: CreateChannelCallback) {
+        connection.createChannel(callback);
+    }
+
+    public sendMessage(queue: string, msg: string) {
+        this.connect((error0: AmqpConnectionError | null, connection: amqp.Connection) => {
+            if (error0) {
+                return error0;
+            }
+            this.createChannel(connection, (error1: AmqpCreateChannelError | null, channel: amqp.Channel) => {
+                if (error1) {
+                    throw error1;
+                }
+        
+                const options: Options.AssertQueue = {
+                    durable: false,
+                }
+        
+                channel.assertQueue(queue, options);
+                for (let i = 0; i < 5; i++) {
+                    channel.sendToQueue(queue, Buffer.from(msg));
+                    console.log(" [x] Sent %s", msg);
+                }
+            })
+            setTimeout(function() {
+                connection.close();
+            }, 500);
+        })
+    }
+
+    public receiveMessages(queue: string, callback: (msg: QueueMessage | null) => void): void {
+        this.connect((error0: AmqpConnectionError | null, connection: amqp.Connection) => {
+            if (error0) {
+                throw error0;
+            }
+            connection.createChannel((error1: AmqpCreateChannelError | null, channel: amqp.Channel) => {
+                if (error1) {
+                    throw error1;
+                }
+                const options: amqp.Options.AssertQueue = { durable: false };
+
+                channel.assertQueue(queue, options);
+                console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+                channel.consume(queue, (msg: amqp.Message | null) => {
+                    if (msg) {
+                        callback(msg);
+                    }
+                }, { noAck: true });
+            });
+        });
+    }
+}
+
+export default AmqpService;
+
+// import amqp from "amqplib/callback_api";
+// import AmqpConnectionError from "./errors/AmqpConnectionError";
+
+// class AmqpService {
+//     private connectionUrl: string;
+
+//     constructor(connectionUrl: string) {
+//         this.connectionUrl = connectionUrl;
+//     }
+    
+//     private connect(callback: (error: AmqpConnectionError | null, connection: amqp.Connection) => void) {
+//         amqp.connect(this.connectionUrl, callback);
+//     }
+
+//     public sendMessage(queue: string, msg: string, callback: )
+// }
