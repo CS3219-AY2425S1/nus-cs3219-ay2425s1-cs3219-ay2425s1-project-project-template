30e72947aecd60dfc19fa685dff11c9cafc8c537
Keagan <keaganpzh@gmail.com>
2024-09-15 11:01:51 +0800

    Add Swagger docs

--- backend/question-service/src/controllers/question.controller.ts
+++ backend/question-service/src/controllers/question.controller.ts
@@ -1,51 +1,59 @@
-import { ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Controller, Get, Post, Body, Query, Param, Put, Delete, UsePipes, ValidationPipe } from '@nestjs/common';
 import { QuestionService } from '../services/question.service';
 import { CreateQuestionDto, UpdateQuestionDto, FilterQuestionsDto } from '../dto/question.dto';
 import { Question } from '../schemas/question.schema';
 
+@ApiTags('question')
 @Controller('question')
 export class QuestionController {
     constructor(private readonly questionService: QuestionService) {}
 
-    @ApiResponse({ status: 201, description: 'Create a new question' })
+    @ApiOperation({ summary: 'Create a new question' })
+    @ApiResponse({ status: 201, type: Question })
     @Post()
     async create(@Body() createQuestionDto: CreateQuestionDto): Promise<Question> {
         return this.questionService.create(createQuestionDto);
     }
 
-    @ApiResponse({ status: 200, description: 'Get all questions' })
+    @ApiOperation({ summary: 'Get all questions' })
+    @ApiResponse({ status: 200, type: [Question] })
     @Get()
     async findAll(): Promise<Question[]> {
         return this.questionService.findAll();
     }
 
-    @ApiResponse({ status: 200, description: 'Get a question by id' })
+    @ApiOperation({ summary: 'Get a question by id' })
+    @ApiResponse({ status: 200, type: Question })
     @Get(':id')
     async findOne(@Param('id') id: string): Promise<Question> {
         return this.questionService.findOne(id);
     }
 
-    @ApiResponse({ status: 200, description: 'Update a question by id' })
+    @ApiOperation({ summary: 'Update a question by id' })
+    @ApiResponse({ status: 200, type: Question })
     @Put(':id')
     async update(@Param('id') id: string, @Body() updateQuestionDto: UpdateQuestionDto): Promise<Question> {
         return this.questionService.update(id, updateQuestionDto);
     }
 
-    @ApiResponse({ status: 200, description: 'Delete a question by id' })
+    @ApiOperation({ summary: 'Delete a question by id' })
+    @ApiResponse({ status: 200, type: Question })
     @Delete(':id')
     async delete(@Param('id') id: string): Promise<Question> {
         return this.questionService.delete(id);
     }
 
-    @ApiResponse({ status: 200, description: 'Filter questions' })
+    @ApiOperation({ summary: 'Filter questions by difficulty and topics' })
+    @ApiResponse({ status: 200, type: [Question] })
     @Get('filter')
     @UsePipes(new ValidationPipe({ transform: true }))
     async filterQuestions(@Query() filterQuestionsDto: FilterQuestionsDto): Promise<Question[]> {
         return this.questionService.filterQuestions(filterQuestionsDto);
     }
 
-    @ApiResponse({ status: 200, description: 'Get a random question' })
+    @ApiOperation({ summary: 'Get a random question based on filter criteria' })
+    @ApiResponse({ status: 200, type: Question })
     @Get('random')
     @UsePipes(new ValidationPipe({ transform: true }))
     async getRandomQuestion(@Query() filterQuestionsDto: FilterQuestionsDto): Promise<Question | null> {
--- backend/question-service/src/dto/question.dto.ts
+++ backend/question-service/src/dto/question.dto.ts
@@ -1,37 +1,44 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { IsArray, IsString, IsNotEmpty, IsEnum, IsOptional } from 'class-validator';
 import { QuestionComplexity, QuestionTopic } from '../schemas/question.schema';
 import { Optional } from '@nestjs/common';
 
 export class CreateQuestionDto {
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     readonly title: string;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     readonly description: string;
 
+    @ApiProperty()
     @IsEnum(QuestionTopic, { each: true })
     readonly topics: QuestionTopic[];
 
+    @ApiProperty()
     @IsEnum(QuestionComplexity)
     readonly complexity: QuestionComplexity;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     readonly link: string;
 }
 
-export class UpdateQuestionDto extends PartialType(CreateQuestionDto) {}
+export class UpdateQuestionDto extends PartialType(CreateQuestionDto) { }
 
 export class FilterQuestionsDto {
+    @ApiProperty()
     @IsEnum(QuestionComplexity)
-    @IsOptional() 
-    difficulty?: QuestionComplexity;
-  
+    @IsOptional()
+    readonly difficulty?: QuestionComplexity;
+
+    @ApiProperty()
     @IsArray()
-    @Optional()  
-    @IsEnum(QuestionTopic, { each: true })  
-    topics: QuestionTopic[];
+    @Optional()
+    @IsEnum(QuestionTopic, { each: true })
+    readonly topics: QuestionTopic[];
 }
