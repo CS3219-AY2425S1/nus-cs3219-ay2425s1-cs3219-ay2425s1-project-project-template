47d0666a31afd46e879871dbba1f13d572450110
Keagan <keaganpzh@gmail.com>
2024-09-15 10:48:33 +0800

    Create Question standalone service

--- backend/question-service/src/app.module.ts
+++ backend/question-service/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { QuestionController } from './controllers/question.controller';
+import { QuestionService } from './services/question.service';
+
+import config from './config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true
+    }),
+    MongooseModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        return {
+          uri: configService.mongodb.uri,
+          dbName: configService.mongodb.dbName
+        }
+      },
+    })
+  ],
+  controllers: [QuestionController],
+  providers: [QuestionService],
 })
 export class AppModule {}
