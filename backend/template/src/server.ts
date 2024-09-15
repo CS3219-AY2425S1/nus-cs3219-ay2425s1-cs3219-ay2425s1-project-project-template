db19119f994fef886634b42bce1742273bfbaeb8
SeeuSim <seeusimong@gmail.com>
2024-09-15 15:21:18 +0800

    Add Database Service
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- backend/template/src/server.ts
+++ backend/template/src/server.ts
@@ -1,14 +1,21 @@
 import express, { json } from 'express';
 import pino from 'pino-http';
+import { db, tableName } from './lib/db';
 
 const app = express();
 app.use(pino());
 app.use(json());
 
-app.get('/', async (req, res) => {
+app.get('/', async (_req, res) => {
   res.json({
     message: 'OK',
   });
+});
+
+// Ensure DB service is up before running.
+app.get('/test-db', async (_req, res) => {
+  await db.select().from(tableName);
+  res.json({ message: 'OK ' });
 });
 
 export default app;
