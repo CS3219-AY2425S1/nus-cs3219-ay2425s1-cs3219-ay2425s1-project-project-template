b8d658f914a91cea73c14fa4a6ecc8b25379688a
XXJJXJ <e0544100@u.nus.edu>
2024-09-15 14:36:10 +0800

    Add dev files

---
+++ backend/api-gateway/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+appimage=apigateway
+containername="my-api-gateway"
+containerPort=8001
+localPort=8001
+
+# remove existing containers if found
+docker ps -a | grep $containername
+if [[ $? == 0 ]]; then
+    docker stop $containername
+    docker rm $containername
+fi
+
+stop=$1
+if [[ "x$stop" != "x" ]]; then
+    echo "Container $containername terminated" 
+    exit 0
+fi
+
+
+docker images | grep $appimage
+if [[ $? == 0 ]]; then
+    docker rmi $appimage
+fi
+
+docker build -t $appimage -f Dockerfile.dev .
+
+docker run --name $containername -p $localPort:$containerPort -v $(pwd):/app -d $appimage
