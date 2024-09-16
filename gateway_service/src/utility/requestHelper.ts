c56b329b761ebb5fbe640a1aee7e22c425cf5057
xav168 <xav.ong.jh@gmail.com>
2024-09-17 01:09:37 +0800

    fix bug

--- gateway_service/src/utility/requestHelper.ts
+++ gateway_service/src/utility/requestHelper.ts
@@ -35,7 +35,7 @@ export function sendGetRequest(path: string, service: string, id: string = "") {
 export function sendPostRequest(path: string, service: string) {
   return async (req: Request, res: Response) => {
     try {
-      let resp = await axios.post(`${service}/${path}`);
+      let resp = await axios.post(`${service}/${path}`, req.body);
       res.status(200).json(resp.data);
     } catch (error: any) {
       if (error.response) {
