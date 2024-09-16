2662a0a85e88dcf7c54891f312a9bd26931c80fc
Eugenetanwl3881 <Eugene.tan.wl@gmail.com>
2024-09-16 20:44:52 +0800

    chore: Set up tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="text-3xl font-bold underline">
+          Edit <code>src/App.js</code> and save to reload. This is using
+          Tailwind CSS
         </p>
         <a
           className="App-link"
