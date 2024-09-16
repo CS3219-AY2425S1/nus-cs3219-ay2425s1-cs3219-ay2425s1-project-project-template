8fb98394db32e29e35e8fcd867e87d3260ebad71
Jweng88 <108978684+Jweng88@users.noreply.github.com>
2024-09-16 11:52:54 +0800

    Enable searching and filtering by question fields and query by question id

--- question-service/src/controller/question-controller.ts
+++ question-service/src/controller/question-controller.ts
@@ -34,7 +34,31 @@ export const questionController = {
   // Get all questions
   getAllQuestions: async (req: Request, res: Response) => {
     try {
-      const questions = await Question.find();
+      const { question_id, title, description, category, complexity } =
+        req.query;
+
+      // Construct the filter object dynamically based on provided query params
+      const filter: any = {};
+
+      if (question_id) {
+        filter.question_id = question_id;
+      }
+      if (title) {
+        filter.title = { $regex: title, $options: "i" }; // Case-insensitive search
+      }
+      if (description) {
+        filter.description = { $regex: description, $options: "i" };
+      }
+      if (category) {
+        filter.category = {
+          $in: Array.isArray(category) ? category : [category],
+        };
+      }
+      if (complexity) {
+        filter.complexity = complexity;
+      }
+
+      const questions = await Question.find(filter);
       res.status(200).json({ questions });
     } catch (err) {
       res.status(500).json({ message: "Failed to get questions", error: err });
@@ -43,10 +67,9 @@ export const questionController = {
 
   // Get question by ID
   getQuestionById: async (req: Request, res: Response) => {
-    const { id } = req.params;
-
     try {
-      const question = await Question.findById(id);
+      const { id } = req.params;
+      const question = await Question.findOne({ question_id: id });
       if (question) {
         res.status(200).json({ question });
       } else {
@@ -63,11 +86,12 @@ export const questionController = {
     const { title, description, category, complexity } = req.body;
 
     try {
-      const updatedQuestion = await Question.findByIdAndUpdate(
-        id,
+      const updatedQuestion = await Question.findOneAndUpdate(
+        { question_id: id },
         { title, description, category, complexity },
-        { new: true }
+        { new: true } // Return the updated document
       );
+
       if (updatedQuestion) {
         res.status(200).json(updatedQuestion);
       } else {
@@ -82,10 +106,11 @@ export const questionController = {
 
   // Delete a question
   deleteQuestion: async (req: Request, res: Response) => {
-    const { id } = req.params;
-
     try {
-      const deletedQuestion = await Question.findByIdAndDelete(id);
+      const { id } = req.params;
+      const deletedQuestion = await Question.findOneAndDelete({
+        question_id: id,
+      });
       if (deletedQuestion) {
         res.status(200).json({ message: "Question deleted successfully" });
       } else {
