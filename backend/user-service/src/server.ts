3baa4dc89335065b777ff831240dbec51c7f7782
Glemen Neo <glemenneo@gmail.com>
2024-09-17 02:27:53 +0800

    [#7] feat: add logging

--- backend/user-service/src/server.ts
+++ backend/user-service/src/server.ts
@@ -1,11 +1,24 @@
 import 'dotenv/config'
 import http, { Server } from 'http'
+import Winston, { Logger } from 'winston'
 import index from './index'
+
+// Setup Logger
+export const logger: Logger = Winston.createLogger({
+    level: 'info',
+    format: Winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        new Winston.transports.Console(),
+        new Winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new Winston.transports.File({ filename: 'combined.log' }),
+    ],
+})
 
 const port: string = process.env.PORT ?? '3000'
 
 const server: Server = http.createServer(index)
 
 server.listen(port, async () => {
-    console.log(`Server is listening on port ${port}`)
+    logger.info(`[Init] Server is listening on port ${port}`)
 })
