80302de54a8f14f0a9ac3df3957586f35aac116a
wr1159 <chuweiron18@gmail.com>
2024-09-17 17:52:03 +0800

    Remove semiquote in signup page

--- frontend/app/auth/signup/page.tsx
+++ frontend/app/auth/signup/page.tsx
@@ -3,7 +3,7 @@ import { SignUpForm } from "@/components/auth/sign-up";
 export default function SignUpPage() {
   return (
     <div className="flex items-center justify-center h-[90vh]">
-      <SignUpForm />;
+      <SignUpForm />
     </div>
   );
 }
