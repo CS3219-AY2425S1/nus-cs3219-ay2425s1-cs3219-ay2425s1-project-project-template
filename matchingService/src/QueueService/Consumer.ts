16f0a4dc5a6f81b0355b607e4524a3e935292958
Donova Chan Jia Jun <donovanchanjiajun2001@gmail.com>
2024-09-17 22:21:50 +0800

    refactor: Remove duplicated and unused code that has no effect on functionality

--- matchingService/src/QueueService/Consumer.ts
+++ matchingService/src/QueueService/Consumer.ts
@@ -1,7 +1,6 @@
 import { Channel } from "amqplib";
 import QueueMessage from "../models/QueueMessage";
 import MatchRequest from "../models/MatchRequest";
-import { ConsumerMessageHeaderReq } from "../models/MessageHeaders";
 
 class Consumer {
     constructor() {
@@ -9,16 +8,7 @@ class Consumer {
     }
 
     public async receiveMessages(topic: string, difficulty: string, responseExchange: string, channel: Channel): Promise<void> {
-        // const queue = await channel.assertQueue("", { exclusive: true })
         const queueName = `queue_${topic}_${difficulty}`;
-
-        // var headers: ConsumerMessageHeaderReq = {
-        //     "x-match": "all",
-        //     "topic": topic,
-        //     "difficulty": difficulty
-        // }
-
-        // await channel.bindQueue(queueName, exchange, "", headers);
         await channel.consume(queueName, (message) => {
             this.handleMatchRequest(message, channel, responseExchange);
         }, { noAck: true });
@@ -32,9 +22,9 @@ class Consumer {
                 console.log("Consumer received match request: ", matchRequest);
                 const correlationId: string = msg?.properties.correlationId;
                 const responseQueue: string = msg?.properties.replyTo;
-                console.log(channel.publish(responseExchange, responseQueue, Buffer.from("Successfully matched"), {
+                channel.publish(responseExchange, responseQueue, Buffer.from("Successfully matched"), {
                     correlationId: correlationId,
-                  }));
+                  });
             } catch (e) {
                 if (e instanceof Error) {
                     let name = e.message;
