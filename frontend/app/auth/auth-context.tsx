e131415f4fc24709bd7f601d792da2db52d8eaf9
wr1159 <chuweiron18@gmail.com>
2024-09-18 02:40:57 +0800

    Redirect user back to landing page after logout

--- frontend/app/auth/auth-context.tsx
+++ frontend/app/auth/auth-context.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Router } from "lucide-react";
+import { useRouter } from "next/navigation";
 import {
   createContext,
   ReactNode,
@@ -28,6 +30,7 @@ const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const tokenKey = "jwtToken";
   const [user, setUser] = useState<UserAuthType | null>(null);
   const [token, setToken] = useState<string | null>(null);
+  const router = useRouter();
 
   useEffect(() => {
     setToken(localStorage.getItem(tokenKey));
@@ -96,6 +99,7 @@ const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     setUser(null);
     setToken(null);
     localStorage.removeItem("jwtToken");
+    router.push("/");
   };
 
   return (
