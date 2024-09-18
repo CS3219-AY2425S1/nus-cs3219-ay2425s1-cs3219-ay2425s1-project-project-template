3bfa665f7508455cf9f1335a3ebd1709115f40aa
Glemen Neo <glemenneo@gmail.com>
2024-09-18 20:18:04 +0800

    [#7] refactor: move mongoose connection logic into seperate util file

--- backend/user-service/src/server.ts
+++ backend/user-service/src/server.ts
@@ -1,25 +1,19 @@
 import 'dotenv/config'
 import http, { Server } from 'http'
-import { connect } from 'mongoose'
 import logger from './common/logger.util'
+import connectToDatabase from './common/mongodb.util'
 import index from './index'
 
 const port: string = process.env.PORT ?? '3000'
-const dbUrl: string | undefined = process.env.DB_URL
-if (!dbUrl) {
-    logger.error(`[Init] DB_URL is not set`)
-}
-
 const server: Server = http.createServer(index)
-
-connect(dbUrl!)
-    .then(() => {
-        logger.info(`[Init] Connected to MongoDB`)
-    })
-    .catch((error: Error) => {
-        logger.error(`[Init] ${error.message}`)
-    })
-
 server.listen(port, async () => {
     logger.info(`[Init] Server is listening on port ${port}`)
 })
+
+const connectionString: string | undefined = process.env.DB_URL
+if (!connectionString) {
+    logger.error(`[Init] DB_URL is not set`)
+    process.exit(1)
+}
+
+connectToDatabase(connectionString)
