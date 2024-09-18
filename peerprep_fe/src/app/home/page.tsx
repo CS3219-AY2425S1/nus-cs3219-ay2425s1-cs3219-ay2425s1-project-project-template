b7142112c689681ac6ab41d70bafaf1bc60883b2
zheng-ze <zhengze@u.nus.edu>
2024-09-18 20:01:35 +0800

    Update home page to redirect to login if token is not found

--- peerprep_fe/src/app/home/page.tsx
+++ peerprep_fe/src/app/home/page.tsx
@@ -1,6 +1,21 @@
-import Header from "@/components/header";
+"use client";
 
-export default function Signup() {
+import Header from "@/components/header";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    alert(token);
+
+    if (!token) {
+      router.push("/auth/login");
+    }
+  });
+
   return (
     <div className="h-screen w-screen flex flex-col max-w-6xl mx-auto py-10">
       <Header />
