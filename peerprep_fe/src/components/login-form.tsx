b1877f8eb1376650b85bcfa6b1f60091ac66363c
zheng-ze <zhengze@u.nus.edu>
2024-09-18 20:00:54 +0800

    Link fe to api gateway

--- peerprep_fe/src/components/login-form.tsx
+++ peerprep_fe/src/components/login-form.tsx
@@ -5,32 +5,45 @@ import Textfield from "@/components/text-field";
 import Button from "@/components/button";
 import TextButton from "@/components/text-button";
 import { login } from "@/app/actions/auth";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export function LoginForm() {
   const [state, action] = useFormState(login, undefined);
+  const router = useRouter();
 
+  useEffect(() => {
+    if (state?.message) {
+      localStorage.setItem("token", state.message);
+      router.push("/home");
+    }
+  }, [state]);
+
+  // TODO: Make errors look better
   return (
-    <form action={action}>
-      <Textfield name="email" secure={false} placeholder_text="Email" />
-      {state?.errors?.email && <p>{state.errors.email}</p>}
-      <Textfield name="password" secure={true} placeholder_text="Password" />
-      {state?.errors?.password && (
-        <div>
-          <p>Password must:</p>
-          <ul>
-            {state.errors.password.map((error) => (
-              <li key={error}>- {error}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-      <Button type="submit" text="Login" />
+    <div>
+      <form action={action}>
+        <Textfield name="email" secure={false} placeholder_text="Email" />
+        {state?.errors?.email && <p>{state.errors.email}</p>}
+        <Textfield name="password" secure={true} placeholder_text="Password" />
+        {state?.errors?.password && (
+          <div>
+            <p>Password must:</p>
+            <ul>
+              {state.errors.password.map((error) => (
+                <li key={error}>- {error}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <Button type="submit" text="Login" />
+      </form>
 
       <div className="mt-5">
         <p className="text-sm font-hairline">
           Need an account? <TextButton text="Sign Up" link="/auth/signup" />
         </p>
       </div>
-    </form>
+    </div>
   );
 }
--- peerprep_fe/src/components/signup-form.tsx
+++ peerprep_fe/src/components/signup-form.tsx
@@ -5,35 +5,51 @@ import Textfield from "@/components/text-field";
 import Button from "@/components/button";
 import TextButton from "@/components/text-button";
 import { signup } from "@/app/actions/auth";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export function SignupForm() {
   const [state, action] = useFormState(signup, undefined);
+  const router = useRouter();
 
+  useEffect(() => {
+    if (state?.message) {
+      localStorage.setItem("token", state.message);
+      router.push("/home");
+    }
+  }, [state]);
+
+  // TODO: Make errors look better
   return (
-    <form action={action}>
-      <div>
-        <Textfield name="username" secure={false} placeholder_text="Name" />
-        {state?.errors?.name && <p>{state.errors.name}</p>}
-      </div>
-      <div>
-        <Textfield name="email" secure={false} placeholder_text="Email" />
-        {state?.errors?.email && <p>{state.errors.email}</p>}
-      </div>
-      <div>
-        <Textfield name="password" secure={true} placeholder_text="Password" />
-        {state?.errors?.password && (
-          <div>
-            <p>Password must:</p>
-            <ul>
-              {state.errors.password.map((error) => (
-                <li key={error}>- {error}</li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
-
-      <Button type="submit" text="Sign Up" />
+    <div>
+      <form action={action}>
+        <div>
+          <Textfield name="username" secure={false} placeholder_text="Name" />
+          {state?.errors?.name && <p>{state.errors.name}</p>}
+        </div>
+        <div>
+          <Textfield name="email" secure={false} placeholder_text="Email" />
+          {state?.errors?.email && <p>{state.errors.email}</p>}
+        </div>
+        <div>
+          <Textfield
+            name="password"
+            secure={true}
+            placeholder_text="Password"
+          />
+          {state?.errors?.password && (
+            <div>
+              <p>Password must:</p>
+              <ul>
+                {state.errors.password.map((error) => (
+                  <li key={error}>- {error}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+        <Button type="submit" text="Sign Up" />
+      </form>
 
       <div className="mt-5">
         <p className="text-sm font-hairline">
@@ -41,6 +57,6 @@ export function SignupForm() {
           <TextButton text="Login" link="/auth/login" />
         </p>
       </div>
-    </form>
+    </div>
   );
 }
