edf8aeda8856500d7760db65a0505802ebba552f
zheng-ze <zhengze@u.nus.edu>
2024-09-18 16:09:32 +0800

    Add test home screen page

---
+++ peerprep_fe/src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/header";
+
+export default function Signup() {
+  return (
+    <div className="h-screen w-screen flex flex-col max-w-6xl mx-auto py-10">
+      <Header />
+      Home Page
+    </div>
+
+    
+  );
+}
