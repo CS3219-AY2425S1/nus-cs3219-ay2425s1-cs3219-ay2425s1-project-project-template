cc63f4c3386e2cf774bb0fe38f985b71e1bd6fce
zheng-ze <zhengze@u.nus.edu>
2024-09-18 16:01:27 +0800

    Update button and textfields to work with forms

--- peerprep_fe/src/components/button.tsx
+++ peerprep_fe/src/components/button.tsx
@@ -1,14 +1,28 @@
 import Link from "next/link";
 
-export default function Button({ text="Button", link="", onClick }:
-  { text?: string, link?: string, onClick?: () => void }) {
-  return (
-    <Link href={link}>
-      <button
-          className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-3 my-3 w-full rounded-lg"
-          onClick={onClick}>
-        {text}
-      </button>
-    </Link>
+export default function Button({
+  type = "button",
+  text = "Button",
+  link = "",
+  onClick,
+}: {
+  type?: "button" | "submit" | "reset" | undefined;
+  text?: string;
+  link?: string;
+  onClick?: () => void;
+}) {
+  const button = (
+    <button
+      type={type}
+      className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-3 my-3 w-full rounded-lg"
+      onClick={onClick}
+    >
+      {text}
+    </button>
   );
+
+  if (link != "") {
+    return <Link href={link}>{button}</Link>;
+  }
+  return button;
 }
--- peerprep_fe/src/components/text-field.tsx
+++ peerprep_fe/src/components/text-field.tsx
@@ -1,7 +1,8 @@
-export default function Textfield({ secure=false, placeholder_text="Enter your text" }:
-  { secure?: boolean, placeholder_text?: string }) {
+export default function Textfield({ name, secure=false, placeholder_text="Enter your text" }:
+  { name?: string, secure?: boolean, placeholder_text?: string }) {
   return (
     <input
+      name = { name }
       type = { secure ? "password" : "text" }
       className = "bg-slate-200 dark:bg-slate-700 rounded-lg w-full p-4 my-3 focus:outline-none"
       placeholder = { placeholder_text }
