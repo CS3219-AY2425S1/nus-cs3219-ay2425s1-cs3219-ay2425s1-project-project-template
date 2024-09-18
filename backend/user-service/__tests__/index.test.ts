60e92beee80cf6cb2dfbe7b22bde5d9977835163
Glemen Neo <glemenneo@gmail.com>
2024-09-18 16:22:02 +0800

    [#7] feat: add mongodb test container to test

--- backend/user-service/__tests__/index.test.ts
+++ backend/user-service/__tests__/index.test.ts
@@ -1,9 +1,24 @@
+import { MongoDBContainer, StartedMongoDBContainer } from '@testcontainers/mongodb'
 import request from 'supertest'
 import app from '../src/index'
 
-describe('GET /', () => {
-    it('should return 200 OK', async () => {
-        const response = await request(app).get('/')
-        expect(response.status).toBe(200)
+describe('Index', () => {
+    let startedContainer: StartedMongoDBContainer
+
+    beforeAll(async () => {
+        const container: MongoDBContainer = new MongoDBContainer().withExposedPorts(27017)
+        startedContainer = await container.start()
+        process.env.DB_URL = `mongodb://${startedContainer.getHost()}:${startedContainer.getMappedPort(27017)}/user-service`
+    })
+
+    afterAll(async () => {
+        await startedContainer.stop()
+    })
+
+    describe('GET /', () => {
+        it('should return 200 OK', async () => {
+            const response = await request(app).get('/')
+            expect(response.status).toBe(200)
+        })
     })
 })
