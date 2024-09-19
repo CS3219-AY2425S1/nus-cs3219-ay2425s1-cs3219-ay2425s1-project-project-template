830322d0614de328bc7f0f2d2f006498330f459a
ldinghan <u.dinghan@gmail.com>
2024-09-20 07:59:02 +0800

    Link navbar to pages

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,9 +1,18 @@
+'use client';
 // components/navbar.tsx
 import Link from 'next/link';
+import classnames from "classnames";
+import { usePathname } from 'next/navigation';
 
 const Navbar: React.FC = () => {
+  const currentPath = usePathname();
+  const routes = [
+    { label: "Explore", href:"/explore" },
+    { label: "Codespace", href:"/codespace" },
+    { label: "Dashboard", href:"/dashboard" }
+  ]
   return (
-    <nav className="bg-violet-700 p-4">
+    <nav className="bg-violet-800 p-3 w-screen h-18 fixed">
       <div className="mx-auto flex justify-between items-center">
         
         <div className="text-white flex text-3xl font-extrabold">
@@ -11,19 +20,21 @@ const Navbar: React.FC = () => {
             <Link href="/">peerprep.</Link>
         </div>
         <ul className="flex justify-end w-4/5 text-lg">
-          <li className='mx-4 font-bold'>
-            <Link href="/">Explore</Link>
-          </li>
-          <li className='mx-4'>
-            <Link href="/about">Codespace</Link>
-          </li>
-          <li className='mx-4'>
-            <Link href="/contact">Dashboard</Link>
-          </li>
+          {routes.map(route => 
+              <Link
+                    key={route.label} 
+                    className={classnames({
+                        'font-bold': route.href === currentPath,
+                        'font-medium': route.href !== currentPath,
+                        'hover:text-zinc-400 text-white transition-colors mx-4': true
+                    })} 
+                    href={route.href}>
+                        {route.label}
+              </Link>)}
         </ul>
         <div className='flex w-auto justify-between'>
             <button className='font-black bg-yellow-500 mt-1 h-10 px-4 rounded-lg mx-4'>
-                Practice
+                Practice {/* button to call API and brings out matching component*/}
             </button>
             <div className='w-12 h-12 rounded-full bg-white mx-4'></div>
         </div>
