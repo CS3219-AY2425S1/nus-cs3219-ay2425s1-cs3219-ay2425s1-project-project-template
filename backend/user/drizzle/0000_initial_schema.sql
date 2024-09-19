be89f4e6cd5190fe0093e48c4c4c5419a46ec85d
SeeuSim <seeusimong@gmail.com>
2024-09-19 20:38:11 +0800

    PEER-208 Add rate limit for auth router
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- backend/user/drizzle/0000_initial_schema.sql
+++ backend/user/drizzle/0000_initial_schema.sql
@@ -1,4 +1,10 @@
-CREATE TABLE IF NOT EXISTS "tableName" (
+CREATE TABLE IF NOT EXISTS "users" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	CONSTRAINT "tableName_id_unique" UNIQUE("id")
+	"email" varchar(255) NOT NULL,
+	"username" varchar(255) NOT NULL,
+	"first_name" varchar(255) NOT NULL,
+	"last_name" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email"),
+	CONSTRAINT "users_username_unique" UNIQUE("username")
 );
