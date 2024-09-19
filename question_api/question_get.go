20d91eb4e1fe11ee6aaf97f066dce2f4ea588aa3
Yuheng <guoyuheng1year@gmail.com>
2024-09-19 22:07:02 +0800

    Add functionality to pull data from database
    Instead of hardcoded cases, we retrieve entries from the mondoDB
    "questions" collection.

--- question_api/question_get.go
+++ question_api/question_get.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	//"go.mongodb.org/mongo-driver/mongo"
+	"context"
 )
 
-func GetAllQuestions(context *gin.Context) {
-	context.JSON(200, questions)
+func (db *QuestionDB) GetAllQuestions(ctx *gin.Context) {
+	// get all questions from the database
+	questions_cursor, err := db.collection.Find(context.Background(), bson.D{})
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error retrieving questions": err.Error()})
+	}
+
+	// create a slice to store the questions
+	var questions []Question
+
+	// decode all the questions from the cursor. If there is an error, return the error
+	if err = questions_cursor.All(context.Background(), &questions); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error decoding questions": err.Error()})
+	}
+
+	if len(questions) == 0 {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "No questions found"})
+	}
+
+	ctx.JSON(200, questions)
 }
--- question_api/question_server.go
+++ question_api/question_server.go
@@ -9,53 +9,68 @@ import (
 )
 
 // for initial API testing
+/*
 var questions = []Question{
 	{
-		ID:         1,
-		Difficulty: Medium,
-		Title: "Two Sum",
+		ID:          1,
+		Difficulty:  Medium,
+		Title:       "Two Sum",
 		Description: "Given an array of integers, return indices of the two numbers such that they add up to a specific target.",
 		TestCases: map[string]string{
 			"[2, 7, 11, 15], 9": "[0, 1]",
-			"[3, 2, 4], 6": "[1, 2]",
-			"[3, 3], 6": "[0, 1]",
+			"[3, 2, 4], 6":      "[1, 2]",
+			"[3, 3], 6":         "[0, 1]",
 		},
 	},
 	{
-		ID:         2,
-		Difficulty: Easy,
-		Title: "Reverse Integer",
+		ID:          2,
+		Difficulty:  Easy,
+		Title:       "Reverse Integer",
 		Description: "Given a 32-bit signed integer, reverse digits of an integer.",
 		TestCases: map[string]string{
 			"123": "321",
-			"1": "1",
-			"22": "22",
+			"1":   "1",
+			"22":  "22",
 		},
 	},
 	{
-		ID:         3,
-		Difficulty: Hard,
-		Title: "Median of Two Sorted Arrays",
+		ID:          3,
+		Difficulty:  Hard,
+		Title:       "Median of Two Sorted Arrays",
 		Description: "There are two sorted arrays nums1 and nums2 of size m and n respectively. Find the median of the two sorted arrays.",
 		TestCases: map[string]string{
-			"[1, 3], [2]": "2.0",
+			"[1, 3], [2]":    "2.0",
 			"[1, 2], [3, 4]": "2.5",
 			"[0, 0], [0, 0]": "0.0",
 		},
 	},
 }
-var Server *mongo.Client = nil
+*/
+// QuestionDB is a struct that contains a pointer to a mongo client
+type QuestionDB struct {
+	collection *mongo.Collection
+}
+// returns a pointer to an instance of the Question collection
+func NewQuestionDB(client *mongo.Client) *QuestionDB {
+
+	collection := client.Database("questions").Collection("questions")
+	return &QuestionDB{collection: collection}
+}
+
+
 
 func main() {
 	router := gin.Default()
-	//initialise the database and handle error
-	var err error
-	Server, err = initialiseDB()
-	
+	//initialise the database and handle errors
+	server, err := initialiseDB()
+
 	if err != nil {
 		panic(err)
 	}
 
-	router.GET("/questions", GetAllQuestions)
-	router.Run(":9090")  //currently local hosted
+	//create a new instance of the questionDB
+	questionDB := NewQuestionDB(server)
+
+	router.GET("/questions", questionDB.GetAllQuestions)
+	router.Run(":9090") //currently local hosted
 }
