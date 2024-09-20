6a39416ce5bf036249021f6147151b2082d82eb9
linuschancs <mrlinuschan@gmail.com>
2024-09-20 13:45:27 +0800

    Reinitialise frontend to use typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-3xl font-bold underline">
-          Edit <code>src/App.js</code> and save to reload. This is using
-          Tailwind CSS
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
