8a5e36a20740e178e52a1d64c2cce4bcabde2e77
AustinHuang1203 <austinhuang1203@gmail.com>
2024-09-20 15:41:43 +0800

    init question page

---
+++ frontend/src/routes/QuestionPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './QuestionPage.css'
+
+function NewPage() {
+  return (
+    <div>
+      <div class = "Header">Question page goes here</div>
+    </div>
+  );
+}
+
+export default NewPage;
