6181c1b4d1ac1b66cad7676e25e8dd3dacd53f9e
ldinghan <u.dinghan@gmail.com>
2024-09-20 08:02:01 +0800

    Realign hero buttons

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,12 +11,13 @@ export default function Home() {
           Collaborate and learn together with your peers, enhance your skills<br/>
           and prepare for technical interviews - all in one place.
         </p>
-        <button className="mt-6 px-6 py-3 mx-4 bg-yellow-500 text-white font-bold rounded-lg">
+        <div className="flex justify-start">
+        <button className="mt-6 px-8 py-3 mr-4 bg-yellow-500 text-white font-bold rounded-lg">
           Get Started
         </button>
-        <button className="mt-6 px-6 py-3 mx-4 bg-violet-800 text-yellow-500 border border-yellow-500 font-bold rounded-lg">
+        <button className="mt-6 px-8 py-3 bg-violet-800 text-yellow-500 border border-yellow-500 font-bold rounded-lg">
           Find out more
-        </button>
+        </button></div>
       </div>
     </section>
   );
