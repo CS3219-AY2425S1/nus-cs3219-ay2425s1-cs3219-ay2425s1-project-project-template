9d17a93eaabca609af2fbd5b8f4677fe2ea35f46
Glemen Neo <glemenneo@gmail.com>
2024-09-20 17:18:37 +0800

    [#12] feat: add auth and login handlers

--- backend/user-service/src/controllers/auth.controller.ts
+++ backend/user-service/src/controllers/auth.controller.ts
@@ -1,4 +1,34 @@
 import { compare, hash } from 'bcrypt'
+import { Request, Response } from 'express'
+import { IVerifyOptions } from 'passport-local'
+import { findOneUserByEmail, findOneUserByUsername } from '../models/user.repository'
+import { UserDto } from '../types/UserDto'
+
+export async function handleAuthentication(
+    usernameOrEmail: string,
+    password: string,
+    done: (error: Error | null, user: UserDto | false, options?: IVerifyOptions) => void
+): Promise<void> {
+    const user = (await findOneUserByUsername(usernameOrEmail)) || (await findOneUserByEmail(usernameOrEmail))
+    if (!user) {
+        done(null, false)
+        return
+    }
+
+    const isPasswordMatching = await compare(password, user.password)
+    if (!isPasswordMatching) {
+        done(null, false)
+        return
+    }
+
+    const dto = UserDto.fromModel(user)
+
+    done(null, dto)
+}
+
+export async function handleLogin({ user }: Request, response: Response): Promise<void> {
+    response.status(201).json(user).send()
+}
 
 export async function comparePasswords(plaintextPassword: string, hashedPassword: string): Promise<boolean> {
     return compare(plaintextPassword, hashedPassword)
