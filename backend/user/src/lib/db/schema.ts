29c72f6fa4742a2aad958a9828bbf651af0f12f3
SeeuSim <seeusimong@gmail.com>
2024-09-20 13:57:05 +0800

    PEER-208 Add Lockout Schema
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- backend/user/src/lib/db/schema.ts
+++ backend/user/src/lib/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, uuid, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, smallint, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
 
 // Define the user table
 export const users = pgTable('users', {
@@ -8,4 +8,6 @@ export const users = pgTable('users', {
   firstName: varchar('first_name', { length: 255 }).notNull(), // First name field, required
   lastName: varchar('last_name', { length: 255 }).notNull(), // Last name field, required
   password: varchar('password', { length: 255 }).notNull(), // Password field, required (hashed password)
+  failedAttempts: smallint('failed_attempts').default(0), // Failed counts
+  unlockTime: timestamp('unlock_time', { precision: 6, withTimezone: true }), // If failed counts > limit, block all attempts until this time.
 });
