9d3182eb6f1a11c8f2103f0301b747491f7b03ef
jasonqiu212 <jason_qiu@hotmail.com>
2024-09-20 23:00:54 +0800

    feat: add login modal

---
+++ frontend/src/components/LoginModal.tsx
@@ -0,0 +1,51 @@
+import {
+  Button,
+  Modal,
+  PasswordInput,
+  Stack,
+  Text,
+  TextInput,
+  Title,
+} from '@mantine/core';
+import { useState } from 'react';
+
+interface LoginModalProps {
+  isLoginModalOpened: boolean;
+  closeLoginModal: () => void;
+}
+
+function LoginModal({ isLoginModalOpened, closeLoginModal }: LoginModalProps) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <Modal
+      opened={isLoginModalOpened}
+      onClose={closeLoginModal}
+      withCloseButton={false}
+      centered
+    >
+      <form onSubmit={() => console.log(password, email)}>
+        <Stack ta="center" p="16px">
+          <Title order={3}>Login</Title>
+          <TextInput
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+          <PasswordInput
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <Button type="submit">Log in</Button>
+          <Text>
+            Don't have an account yet? <b>Sign up now</b>
+          </Text>
+        </Stack>
+      </form>
+    </Modal>
+  );
+}
+
+export default LoginModal;
--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -3,47 +3,61 @@ import {
   Button,
   Center,
   Group,
+  Modal,
   Stack,
   Text,
   Title,
 } from '@mantine/core';
 import '@mantine/core/styles.css';
+import { useDisclosure } from '@mantine/hooks';
+
+import LoginModal from '../components/LoginModal';
 
 function Landing() {
-  return (
-    <AppShell withBorder={false} header={{ height: 80 }}>
-      <AppShell.Header px="40px" py="16px" bg="slate.8">
-        <Group justify="space-between">
-          <a href="." className="logo">
-            <Title c="white">PeerPrep</Title>
-          </a>
-          <Button>Log in</Button>
-        </Group>
-      </AppShell.Header>
+  const [isLoginModalOpened, { open: openLoginModal, close: closeLoginModal }] =
+    useDisclosure(false);
 
-      <AppShell.Main h="calc(100vh - 80px)" w="100%" bg="slate.9">
-        <Center h="100%" p="40px">
-          <Stack
-            maw="720px"
-            align="center"
-            justify="center"
-            ta="center"
-            gap="24px"
-          >
-            <Title size="60px" fw="800">
-              Ace your tech interviews today.
-            </Title>
-            <Text size="xl" maw="480px">
-              Match with others and practice LeetCode questions through mock
-              interviews.
-            </Text>
-            <Button size="lg" mt="16px">
-              Get started
-            </Button>
-          </Stack>
-        </Center>
-      </AppShell.Main>
-    </AppShell>
+  return (
+    <>
+      <AppShell withBorder={false} header={{ height: 80 }}>
+        <AppShell.Header px="40px" py="16px" bg="slate.8">
+          <Group justify="space-between">
+            <a href="." className="logo">
+              <Title c="white">PeerPrep</Title>
+            </a>
+            <Button onClick={openLoginModal}>Log in</Button>
+          </Group>
+        </AppShell.Header>
+
+        <AppShell.Main h="calc(100vh - 80px)" w="100%" bg="slate.9">
+          <Center h="100%" p="40px">
+            <Stack
+              maw="720px"
+              align="center"
+              justify="center"
+              ta="center"
+              gap="24px"
+            >
+              <Title size="60px" fw="800">
+                Ace your tech interviews today.
+              </Title>
+              <Text size="xl" maw="480px">
+                Match with others and practice LeetCode questions through mock
+                interviews.
+              </Text>
+              <Button size="lg" mt="16px">
+                Get started
+              </Button>
+            </Stack>
+          </Center>
+        </AppShell.Main>
+      </AppShell>
+
+      <LoginModal
+        isLoginModalOpened={isLoginModalOpened}
+        closeLoginModal={closeLoginModal}
+      />
+    </>
   );
 }
 
