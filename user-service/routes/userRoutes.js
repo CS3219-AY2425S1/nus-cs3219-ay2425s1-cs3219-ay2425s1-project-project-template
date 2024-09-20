1a90ce41b5fcf7c8e9450d0a862884966b79549b
HoJunHao2000 <kennethhojunhao@gmail.com>
2024-09-21 00:29:46 +0800

    Change api endpoint name

--- user-service/routes/userRoutes.js
+++ user-service/routes/userRoutes.js
@@ -25,12 +25,12 @@ router.put('/profile', authMiddleware, updateProfile);
 router.get('/profile', authMiddleware, viewProfile);
 
 // Get all users route (only for admin)
-router.get('/users', authMiddleware, verifyIsAdmin, getAllUsers);
+router.get('/profiles', authMiddleware, verifyIsAdmin, getAllUsers);
 
 // Delete user route (only for admin or the user itself)
-router.delete('/users/:id', authMiddleware, verifyIsOwnerOrAdmin, deleteUserById);
+router.delete('/profile/:id', authMiddleware, verifyIsOwnerOrAdmin, deleteUserById);
 
 // Delete all users route (only for admin)
-router.delete('/users', authMiddleware, verifyIsAdmin, deleteUsers);
+router.delete('/profiles', authMiddleware, verifyIsAdmin, deleteUsers);
 
 module.exports = router;
