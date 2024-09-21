96959f6e2a5366548727be21d440d0a381b34f4d
jeraldkiew <jeraldkiew007@gmail.com>
2024-09-22 00:09:39 +0800

    add getNextAvailId

--- question-service/controller/create.js
+++ question-service/controller/create.js
@@ -1,18 +1,21 @@
 import Question from '../model/Question.js';
+import { getNextAvailIdHelper } from './read.js';
 
 export const createNewQuestion = async (req, res) => {
     try {
-      const { title, description, topics, difficulty } = req.params.body;
+      const { title, description, topics, difficulty } = req.body;
+      const id = await getNextAvailIdHelper();
       const question = new Question({
+        id: id,
         title: title,
         description: description,
         topics: topics,
         difficulty: difficulty,
-  
       });
-      return res.status(200).json(questions);;
+      await question.save();
+      return res.status(200).json(question);;
     } catch (err) {
       console.log(err);
-      return res.status(500).json({ message: "Unknown error when finding all questions!" });
+      return res.status(500).json({ message: "Unknown error when creating new question!" });
     }
 };
