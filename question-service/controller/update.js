630d38846a27d5794d1a723785727c9110b26fc5
jeraldkiew <jeraldkiew007@gmail.com>
2024-09-22 00:10:56 +0800

    add delete and update

---
+++ question-service/controller/update.js
@@ -0,0 +1,16 @@
+import Question from '../model/Question.js';
+
+export const updateQuestion = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const filter = {
+        id: id
+      }
+      const update = req.body;
+      const question = await Question.findOneAndUpdate(filter, update);
+      return res.status(200).json(question);;
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: `Unknown error when updating question with ID ${id}!` });
+    }
+};
--- question-service/routes/routes.js
+++ question-service/routes/routes.js
@@ -1,6 +1,19 @@
 import { Router } from 'express';
-import { getAllQuestions, getQuestionById, getQuestionByDifficulty, getQuestionByTopic } from '../controller/controller.js';
+import { getAllQuestions, getQuestionById, getQuestionByDifficulty, getQuestionByTopic, getNextAvailId } from '../controller/read.js';
+import { createNewQuestion } from '../controller/create.js';
+import { updateQuestion } from '../controller/update.js';
+import { deleteQuestion } from '../controller/delete.js';
 const router = Router();
+
+/**
+ * CREATE
+ */
+
+router.post('/', createNewQuestion);
+
+/**
+ * READ
+ */
 
 router.get('/all', getAllQuestions);
 
@@ -9,5 +22,19 @@ router.get('/id/:id', getQuestionById);
 router.get('/difficulty/:difficulty', getQuestionByDifficulty);
 
 router.get('/topic/:topic', getQuestionByTopic);
+
+router.get('/nextid', getNextAvailId);
+
+/**
+ * UPDATE
+ */
+
+router.put('/:id', updateQuestion);
+
+/**
+ * DELETE
+ */
+
+router.delete('/:id', deleteQuestion);
 
 export default router;
