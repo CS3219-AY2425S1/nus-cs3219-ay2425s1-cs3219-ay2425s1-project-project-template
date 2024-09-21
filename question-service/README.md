2cabc624a7249c162ecb857277a40d2a0deb4fdf
dylkaw <37286651+dylkaw@users.noreply.github.com>
2024-09-21 12:08:32 +0800

    Restructure project for scalability
    Project split into 3 separate directories:
    - crud
    - models
    - routers

--- question-service/README.md
+++ question-service/README.md
@@ -5,6 +5,6 @@ Dependencies: Python 12
 2. Run `source .venv/bin/activate` for mac or `.venv/bin.activate` for windows to activate the virtual environment.
 3. Run `pip install -r requirements.txt` to install all necessary dependencies.
 4. Ensure you have a `.env` file in `question-service` with the question service mongoDB uri. 
-5. To start the service, run `fastapi dev app.py`
+5. To start the service, run `fastapi dev app/main.py`
 
 Note: For VSCode to properly detect the venv, open the project with `question-service` as the root directory. If not, VSCode will not detect the venv properly and will scream at you for everything. 
---
+++ question-service/app/crud/questions.py
@@ -0,0 +1,25 @@
+from app.models.questions import QuestionModel
+from dotenv import load_dotenv
+import motor.motor_asyncio
+import os
+
+# Load env variables
+load_dotenv()
+
+DB_CLOUD_URI = os.getenv("DB_CLOUD_URI")
+
+# Setup MongoDB client
+client = motor.motor_asyncio.AsyncIOMotorClient(DB_CLOUD_URI)
+db = client.get_database("question_service")
+question_collection = db.get_collection("questions")
+
+async def create_question(question: QuestionModel):
+    existing_question = await question_collection.find_one({"title": question.title})
+    if existing_question:
+        return None
+    new_question = await question_collection.insert_one(question.model_dump())
+    return await question_collection.find_one({"_id": new_question.inserted_id})
+
+async def get_all_questions():
+    questions = await question_collection.find().to_list(1000)
+    return {"questions": questions}
