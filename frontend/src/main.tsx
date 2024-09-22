b0ec12875f8afe62bc7e66a424f6f3050dd74962
zsheng <zsheng.dev@gmail.com>
2024-09-23 06:26:36 +0700

    feat: create 404 error page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+import ErrorPage from '@/error-page';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
