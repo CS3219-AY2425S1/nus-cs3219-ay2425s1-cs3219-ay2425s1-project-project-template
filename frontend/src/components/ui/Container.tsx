85997be4cac9d2b4f1804c05df7c85943c7be476
Mai Ting Kai <wheatgrass.mc@gmail.com>
2024-09-22 16:00:44 +0800

    Add Container

--- frontend/src/components/ui/Container.tsx
+++ frontend/src/components/ui/Container.tsx
@@ -1,13 +1,19 @@
 import Header from "@/components/ui/Header";
+import { cn } from "@/lib/utils";
 
 interface ContainerProps {
   withHeader?: boolean; // Lazy way to handle adding of headers to pages that need them
+  className: string;
   children: React.ReactNode;
 }
 
-const Container = ({ withHeader = true, children }: ContainerProps) => {
+const Container = ({
+  withHeader = true,
+  children,
+  className,
+}: ContainerProps) => {
   return (
-    <div className="p-10">
+    <div className={cn(`m-10 w-full`, className)}>
       {withHeader && <Header />}
       {children}
     </div>
