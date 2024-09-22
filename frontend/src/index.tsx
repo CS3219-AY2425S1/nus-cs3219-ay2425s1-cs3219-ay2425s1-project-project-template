d53fd200d8cc37201980c9863f4acdc394c046f0
tamago55 <e0968379@u.nus.edu>
2024-09-22 09:23:20 +0800

    Dev Environment for frontend setup
    use `npm run start` to start the vite dev server to see the html output.
    use `npm run build` to produce a build of the frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-// Create a root element to render your app into
-const rootElement = document.getElementById('root') as HTMLElement;
-
-// Create a root with ReactDOM's new API for React 18+
-const root = ReactDOM.createRoot(rootElement);
-
-// Render the app inside the root element
-root.render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: '/src/index.tsx' // This is the main entry file for the build process
-    }
-  }
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 });
