33a5273f4d625967e2e4ea0db1b90437e0e7b689
Chen-Kuei <rubychen0427@gmail.com>
2024-09-22 19:30:59 +0800

    HomePage and ReadPage UI designs

---
+++ frontend/src/ReadPage.js
@@ -0,0 +1,42 @@
+import React from "react";
+//import { useParams } from "react-router-dom";
+import "./ReadPage.css";
+
+function ReadPage() {
+  // Insert the logic and API to get the question
+
+  return (
+    <div className="question-details">
+      <div className="category">
+        <p>Data Structures, Algorithms</p>
+      </div>
+      {/* Container for title and complexity */}
+      <div className="title-complexity">
+        <div className="title">
+          <h2>Linked List Cycle Detection</h2>
+        </div>
+        <div className="complexity">
+          <p>Easy</p>
+        </div>
+
+      </div>
+
+      <div className="divider"></div>   
+
+      <div className="description">
+        <p>Implement a function to detect if a linked list contains a cycle.</p>
+      </div>
+
+      <div className="edit-delete-btn">
+        <button className="edit-btn">
+          Edit
+        </button>
+        <button className="delete-btn">
+          Delete
+        </button>
+      </div>   
+    </div>
+  );
+}
+
+export default ReadPage;
