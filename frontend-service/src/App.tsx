8cb945d5dece341487f1cc6a4423f5907598f1e1
techjay-c <ferrarichewporshe@gmail.com>
2024-09-22 22:38:08 +0800

    Create nav bar component

--- frontend-service/src/App.tsx
+++ frontend-service/src/App.tsx
@@ -1,16 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css'
-import Login from './login'
+import Login from './login';
+import Navbar from './navbar';
 
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <div>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
-}
+};
+
 
 export default App
