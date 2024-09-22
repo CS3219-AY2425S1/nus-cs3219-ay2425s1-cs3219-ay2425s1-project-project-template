d2086b8cc00b1f5f64925b3a8b6c1250269cea92
zsheng <zsheng.dev@gmail.com>
2024-09-23 06:21:33 +0700

    feat: setup navbar with peerprep logo

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,32 +1,35 @@
-import reactLogo from '@/assets/react.svg';
 import { Button } from '@/components/ui/button';
+import { CodeXml } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav className='col-span-12 bg-white shadow-md'>
+    <nav className='col-span-12 bg-background shadow-sm'>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
         <div className='flex justify-between h-16'>
           <div className='flex'>
             <div className='flex-shrink-0 flex items-center'>
-              <img className='h-8 w-auto' src={reactLogo} alt='Logo' />
+              <Link to='/'>
+                <CodeXml className='h-8 w-auto text-primary' />
+              </Link>
             </div>
             <div className='hidden sm:ml-6 sm:flex sm:space-x-8'>
-              <a
-                href='#'
-                className='border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+              <Link
+                to='/problems'
+                className='border-primary text-foreground font-bold inline-flex items-center px-1 pt-1 border-b-2 text-sm'
               >
                 Problems
-              </a>
-              <a
-                href='#'
-                className='border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+              </Link>
+              <Link
+                to='/discuss'
+                className='border-transparent text-muted-foreground hover:border-muted-foreground hover:text-foreground inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium transition'
               >
                 Discuss
-              </a>
+              </Link>
             </div>
           </div>
           <div className='hidden sm:ml-6 sm:flex sm:items-center'>
-            <Button variant={"secondary"}>Sign In</Button>
+            <Button variant={'secondary'}>Sign In</Button>
           </div>
         </div>
       </div>
