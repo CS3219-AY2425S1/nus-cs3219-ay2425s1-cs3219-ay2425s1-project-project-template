2b47a25b97ef9c297eed401b12501fca33e47f96
Mai Ting Kai <wheatgrass.mc@gmail.com>
2024-09-22 16:14:13 +0800

    Add boilerplate for DataTable component

--- frontend/src/app/(authenticatedPages)/dashboard/page.tsx
+++ frontend/src/app/(authenticatedPages)/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { getUserInterviewMetadata } from "@/api/dashboard";
 import DashboardCard from "@/app/(authenticatedPages)/dashboard/components/DashboardCard";
 import { Button } from "@/components/ui/button";
 import Container from "@/components/ui/Container";
+import { DataTableDemo } from "@/components/ui/DataTable";
 import { UserInterviewMetadata } from "@/types/dashboard";
 import { useEffect, useState } from "react";
 import { IoMdSearch } from "react-icons/io";
@@ -23,31 +24,30 @@ const Dashboard = () => {
   };
 
   return (
-    <Container className="flex flex-col">
-      <div className="w-full">
-        <div className="flex flex-row w-full gap-8">
-          <DashboardCard
-            cardTitleLabel="Completed This Week"
-            cardBodyLabel={`${userInterviewMetadata?.completedThisWeek.interviewCount} Interviews`}
-            cardFooterLabel={`${userInterviewMetadata?.completedThisWeek.dateRangeStart.toLocaleDateString(
-              "en-US",
-              dateFormattingOption
-            )} - ${userInterviewMetadata?.completedThisWeek.dateRangeEnd.toLocaleDateString(
-              "en-US",
-              dateFormattingOption
-            )}`}
-          />
-          <DashboardCard
-            cardTitleLabel="Latest Interview With"
-            cardBodyLabel={`${userInterviewMetadata?.latestInterview.latestInterviewPartnerName}`}
-            cardFooterLabel={`You collaborated for ${userInterviewMetadata?.latestInterview.timeTakenFormatted}`}
-          />
-          <Button className="w-full self-end flex gap-3" size={"lg"}>
-            <IoMdSearch />
-            <span>Find Interview Peer</span>
-          </Button>
-        </div>
+    <Container className="flex flex-col w-full">
+      <div className="flex flex-row w-full gap-8">
+        <DashboardCard
+          cardTitleLabel="Completed This Week"
+          cardBodyLabel={`${userInterviewMetadata?.completedThisWeek.interviewCount} Interviews`}
+          cardFooterLabel={`${userInterviewMetadata?.completedThisWeek.dateRangeStart.toLocaleDateString(
+            "en-US",
+            dateFormattingOption
+          )} - ${userInterviewMetadata?.completedThisWeek.dateRangeEnd.toLocaleDateString(
+            "en-US",
+            dateFormattingOption
+          )}`}
+        />
+        <DashboardCard
+          cardTitleLabel="Latest Interview With"
+          cardBodyLabel={`${userInterviewMetadata?.latestInterview.latestInterviewPartnerName}`}
+          cardFooterLabel={`You collaborated for ${userInterviewMetadata?.latestInterview.timeTakenFormatted}`}
+        />
+        <Button className="w-full self-end flex gap-3" size={"lg"}>
+          <IoMdSearch />
+          <span>Find Interview Peer</span>
+        </Button>
       </div>
+      <DataTableDemo />
     </Container>
   );
 };
