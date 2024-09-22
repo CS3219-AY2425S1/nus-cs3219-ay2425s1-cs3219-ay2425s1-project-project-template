b847560ba92cc56842ae89f766a9c76458b0f02c
Mai Ting Kai <wheatgrass.mc@gmail.com>
2024-09-22 16:00:27 +0800

    Add Button and Card from ShadCN UI

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,32 +1,90 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          900: "#171C28",
-          800: "#1D2433",
-          700: "#2F3B54",
-          600: "#6679A4",
-          500: "#8695B7",
-          400: "#A2AABC",
-          300: "#D7DCE2"
-        },
-        secondary: "#FFCC66",
-        red: "#EF6B73",
-        
-      },
-      fontFamily: {
-        sans: ["Inter", "sans-serif"]
-      }
-    },
+	fontSize: {
+		xs: "14px",
+		sm: "16px",
+		md: "18px",
+		lg: "20px",
+		h3: "32px",
+		h2: "40px",
+		h1: "48px"
+	},
+	colors: {
+		white: "#FFFFFF"
+	},
+  	extend: {
+  		colors: {
+  			primary: {
+  				'300': '#D7DCE2',
+  				'400': '#A2AABC',
+  				'500': '#8695B7',
+  				'600': '#6679A4',
+  				'700': '#2F3B54',
+  				'800': '#1D2433',
+  				'900': '#171C28',
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			red: '#EF6B73',
+			yellow: {
+				"300": "#FFD280",
+				"500": "#FFCC66",
+			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			sans: ["Inter", "sans-serif"]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
