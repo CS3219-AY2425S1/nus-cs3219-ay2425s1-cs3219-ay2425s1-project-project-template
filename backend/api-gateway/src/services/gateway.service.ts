ea15c8ce861dc0bca1a0c3ed686ed6809d2938a6
XXJJXJ <e0544100@u.nus.edu>
2024-09-23 12:10:28 +0800

    Update header conversion

--- backend/api-gateway/src/services/gateway.service.ts
+++ backend/api-gateway/src/services/gateway.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import axios from 'axios';
+import { IncomingHttpHeaders } from 'http';
 
 @Injectable()
 export class GatewayService {
@@ -8,11 +9,14 @@ export class GatewayService {
   async handleRedirectRequest(@Req() req: Request, @Res() res: Response, serviceDomain: string): Promise<void> {
     const url = `${serviceDomain}${req.originalUrl.replace("/api", "")}`;
     console.log(url)
+    console.log(req.method)
+    // NOTE: axios headers can't just take the req.headers as it is
+    const headers = getHeaders(req.headers)
     try {
       const response = await axios({
         method: req.method,
         url: url,
-        headers: req.headers,
+        headers: headers,
         data: req.body,
       });
       res.status(response.status).json(response.data);
@@ -25,4 +29,15 @@ export class GatewayService {
       });
     }
   }
+}
+
+function getHeaders(headers: IncomingHttpHeaders) {
+  const axiosHeader = {}
+  for (let key in headers) {
+    const parts = key.split('-');
+    const capitalizedParts = parts.map(part => part.charAt(0).toUpperCase() + part.slice(1));
+    const newKey = capitalizedParts.join('');
+    axiosHeader[newKey] = headers[key];
+  }
+  return axiosHeader
 }
