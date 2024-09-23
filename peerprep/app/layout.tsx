8203b1e2245cad76cd305a481e92d7262643828c
WZWren <115881359+WZWren@users.noreply.github.com>
2024-09-23 15:18:38 +0800

    Add basic title-bar element

--- peerprep/app/layout.tsx
+++ peerprep/app/layout.tsx
@@ -1,3 +1,4 @@
+import TitleBar from '@/components/shared/TitleBar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -17,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <h1>NavBar</h1>
+        <TitleBar />
         {children}
       </body>
     </html>
