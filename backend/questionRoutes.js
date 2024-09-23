8a924ce6f8f8981a65cae95b8e0537be8304dc51
Jishnu <e0725046@u.nus.edu>
2024-09-23 23:34:05 +0800

    feat: add API endpoints for CRUD ops

---
+++ backend/questionRoutes.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const Question = require('./models/Question');
+const router = new express.Router();
+
+// POST endpoint to create a new question
+app.post('/questions', async (req, res) => {
+    try {
+        const newQuestion = new Question(req.body);
+        await newQuestion.save();
+        res.status(201).send(newQuestion);
+    } catch (error) {
+          res.status(400).send(error);
+    }
+});
+
+// GET endpoint to retrieve all questions
+app.get('/questions', async (req, res) => {
+    try {
+        const questions = await Question.find();
+        res.send(questions);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// GET endpoint to retrieve a question by ID
+app.get('/questions/:id', async (req, res) => {
+    try {
+        const question = await Question.findById(req.params.id);
+        if (!question) {
+            return res.status(404).send();
+        }
+        res.send(question);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// PATCH endpoint to update a question by ID
+app.patch('/questions/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['title', 'description', 'category', 'complexity', 'web_link'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' });
+    }
+    try {
+        const question = await Question.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!question) {
+            return res.status(404).send();
+        }
+        res.send(question);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// DELETE endpoint to delete a question by ID
+app.delete('/questions/:id', async (req, res) => {
+    try {
+        const question = await Question.findByIdAndDelete(req.params.id);
+        if (!question) {
+            return res.status(404).send();
+        }
+        res.send(question);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const userRouter = require('./userRoutes');
 const app = express();
 
 app.use(express.json());
+app.use(userRouter);
 
 const port = process.env.PORT || 3001;
 
