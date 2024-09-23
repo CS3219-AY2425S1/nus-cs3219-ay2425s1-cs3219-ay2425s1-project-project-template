158fb4d84b82047d475da66600d5d2f9364246f9
LHeng1 <limhengyi1@gmail.com>
2024-09-23 22:16:27 +0800

    feat: setup backend boilerplate structure

---
+++ backend/src/main/java/g55/cs3219/backend/sampleService/model/HelloUser.java
@@ -0,0 +1,19 @@
+package g55.cs3219.backend.sampleService.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class HelloUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    // Constructors, Getters, Setters
+}
