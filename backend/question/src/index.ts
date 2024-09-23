d22c3aea6e8367f43147eddb07fe9ed656c29eeb
SeeuSim <seeusimong@gmail.com>
2024-09-23 11:27:02 +0800

    chore(workspace): update workspace API codes
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- backend/question/src/index.ts
+++ backend/question/src/index.ts
@@ -1,9 +1,10 @@
-import app from './server';
-import { logger } from './lib/utils';
+import { logger } from '@/lib/utils';
+import app, { dbHealthCheck } from '@/server';
 
 const port = process.env.PORT || 8001;
 
 const listenMessage = `App listening on port: ${port}`;
 app.listen(port, () => {
+  void dbHealthCheck();
   logger.info(listenMessage);
 });
