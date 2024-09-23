10aa4c0ecec737bb6421de2bfe3f01a3688e53df
shishirbychapur <shishirbychapur@gmail.com>
2024-09-23 19:18:27 +0800

    fix: update config file to validate env

--- backend/user-service/src/types/Config.ts
+++ backend/user-service/src/types/Config.ts
@@ -1,4 +1,4 @@
-import { IsBase64, IsEnum, IsNotEmpty, IsNumberString, IsString, validateOrReject } from 'class-validator'
+import { IsBase64, IsEmail, IsEnum, IsNotEmpty, IsNumberString, IsString, validateOrReject } from 'class-validator'
 
 export class Config {
     @IsEnum(['development', 'production', 'test'])
@@ -18,18 +18,29 @@ export class Config {
     @IsBase64()
     ACCESS_TOKEN_PUBLIC_KEY: string
 
+    @IsEmail()
+    NODEMAILER_EMAIL: string
+
+    @IsString()
+    @IsNotEmpty()
+    NODEMAILER_PASSWORD: string
+
     constructor(
         NODE_ENV: string,
         PORT: string,
         DB_URL: string,
         ACCESS_TOKEN_PRIVATE_KEY: string,
-        ACCESS_TOKEN_PUBLIC_KEY: string
+        ACCESS_TOKEN_PUBLIC_KEY: string,
+        NODEMAILER_EMAIL: string,
+        NODEMAILER_PASSWORD: string
     ) {
         this.NODE_ENV = NODE_ENV ?? 'development'
         this.PORT = PORT ?? '3002'
         this.DB_URL = DB_URL
         this.ACCESS_TOKEN_PRIVATE_KEY = ACCESS_TOKEN_PRIVATE_KEY
         this.ACCESS_TOKEN_PUBLIC_KEY = ACCESS_TOKEN_PUBLIC_KEY
+        this.NODEMAILER_EMAIL = NODEMAILER_EMAIL
+        this.NODEMAILER_PASSWORD = NODEMAILER_PASSWORD
     }
 
     static fromEnv(env: { [key: string]: string | undefined }): Config {
@@ -38,7 +49,9 @@ export class Config {
             env.PORT!,
             env.DB_URL!,
             env.ACCESS_TOKEN_PRIVATE_KEY!,
-            env.ACCESS_TOKEN_PUBLIC_KEY!
+            env.ACCESS_TOKEN_PUBLIC_KEY!,
+            env.NODEMAILER_EMAIL!,
+            env.NODEMAILER_PASSWORD!
         )
     }
 
