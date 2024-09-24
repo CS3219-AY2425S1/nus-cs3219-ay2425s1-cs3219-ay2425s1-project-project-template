1221f2764f5115c873d3aca89a04590cf1b4cbe7
yunruu <yunruuchin@gmail.com>
2024-09-24 16:47:10 +0800

    feat: add language mode icons

---
+++ frontend/assets/icons/typescript.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+function TypescriptIcon(props) {
+    return (
+        <svg width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <path fill="none" d="M0 0H24V24H0z" />
+            <path d="M3 3h18v18H3V3m10.71 14.86a3.26 3.26 0 003.09 1.73c1.6 0 2.8-.83 2.8-2.36s-.81-2-2.25-2.66l-.42-.18c-.73-.31-1-.52-1-1a.74.74 0 01.81-.73 1.16 1.16 0 011.09.73l1.31-.87a2.5 2.5 0 00-2.4-1.33 2.26 2.26 0 00-2.48 2.23c0 1.38.81 2 2 2.55l.42.18c.78.34 1.24.55 1.24 1.13s-.45.83-1.15.83a1.83 1.83 0 01-1.67-1l-1.38.8M13 11.25H8v1.5h1.5V20h1.75v-7.25H13z" />
+        </svg>
+    )
+}
+
+export default TypescriptIcon
