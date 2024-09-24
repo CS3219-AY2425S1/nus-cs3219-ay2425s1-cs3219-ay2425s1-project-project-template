08f00d99d2bb9aa16bf636a337a4b2d7833292d8
tamago55 <e0968379@u.nus.edu>
2024-09-24 12:03:20 +0800

    Add tailwindcss
    Tailwindcss allows us to design ui components with a faster development time as it comes with prebuilt CSS classes.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1 className="text-5xl font-bold underline">Hello world!</h1>
+      </div>
+    </BrowserRouter>
   );
 };
 
