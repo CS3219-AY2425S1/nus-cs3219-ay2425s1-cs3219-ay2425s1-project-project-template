55c9e8eb3817bfe251e286e099dc0dbe89b76004
yunruu <yunruuchin@gmail.com>
2024-09-24 17:08:37 +0800

    feat: create testcases tab component

---
+++ frontend/pages/code/testcase-tab.tsx
@@ -0,0 +1,33 @@
+import CustomTabs from '@/components/customs/custom-tabs'
+import { ITestcase } from '@/types'
+import { useState } from 'react'
+
+export default function TestcasesTab({ testcases }: { testcases: ITestcase[] }) {
+    const [activeTestcaseIdx, setActiveTestcaseIdx] = useState(testcases[0].idx)
+
+    const activeTestcase = (idx: number) => {
+        return testcases.find((testcase) => testcase.idx === idx)
+    }
+    return (
+        <div>
+            <CustomTabs
+                tabs={testcases.map((testcase) => `Case ${testcase.idx}`)}
+                handleActiveTabChange={(tab) => setActiveTestcaseIdx(parseInt(tab.split(' ')[1]))}
+                type="label"
+                size="label"
+            />
+            {testcases.map((testcase) => (
+                <div className="text-sm mx-2 mt-4">
+                    {activeTestcaseIdx === testcase.idx && (
+                        <div key={activeTestcaseIdx} className="flex flex-col gap-2">
+                            <span>{activeTestcase(activeTestcaseIdx)?.inputVar} =</span>
+                            <span className="px-2 py-1 rounded-lg bg-slate-100">
+                                {activeTestcase(activeTestcaseIdx)?.input}
+                            </span>
+                        </div>
+                    )}
+                </div>
+            ))}
+        </div>
+    )
+}
