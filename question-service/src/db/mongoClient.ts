4a1a7a7b593c877689f801bce010aae779e80d9d
leesiyuan <lee.si.yuan.20@gmail.com>
2024-09-24 23:44:54 +0800

    Update mongoClient.ts
    Removed unused statement.

--- question-service/src/db/mongoClient.ts
+++ question-service/src/db/mongoClient.ts
@@ -4,7 +4,6 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: '.env.dev' });
 
 const MONGODB_URI = process.env.MONGODB_CONNECTION_STRING || '';
-// console.log(MONGODB_URI);
 
 let db: Db;
 
