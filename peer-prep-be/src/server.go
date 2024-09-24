316d3940bc56fa4e4efd4d6c45e41ae1ed52dd69
Kervyn <73232587+kervyntan@users.noreply.github.com>
2024-09-24 11:32:52 +0800

    Added godotenv dependency, sample connection to MongoDB,

--- peer-prep-be/src/server.go
+++ peer-prep-be/src/server.go
@@ -1,12 +1,48 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	key := "MONGODB_URI"
+	err := godotenv.Load("../.env")
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	uri := os.Getenv(key)
+	fmt.Println(uri)
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	// Send a ping to confirm a successful connection
+	var result bson.M
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
