c4d6cd2850f569ad2752eecc66bd9cba87f0c75e
blaukc <brandonlau1@hotmail.com>
2024-09-24 18:24:06 +0800

    Add list questions

--- peerprep-questions/controller/question.go
+++ peerprep-questions/controller/question.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/CS3219-AY2425S1/cs3219-ay2425s1-project-g32/peerprep-questions/model"
@@ -31,6 +32,7 @@ func (qc QuestionController) CreateQuestion(w http.ResponseWriter, r *http.Reque
 	// Set to DB
 	result, err := qc.questionRepository.CreateQuestion(question)
 	if err != nil {
+		log.Fatal("Error creating question")
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -41,18 +43,16 @@ func (qc QuestionController) CreateQuestion(w http.ResponseWriter, r *http.Reque
 }
 
 func (qc QuestionController) ListQuestions(w http.ResponseWriter, r *http.Request) {
-	// var question model.Question
-	// if err := json.NewDecoder(r.Body).Decode(&question); err != nil {
-	// 	http.Error(w, "Invalid input", http.StatusBadRequest)
-	// 	return
-	// }
+	// Get from DB
+	questions, err := qc.questionRepository.ListQuestions()
+	if err != nil {
+		log.Fatal("Error getting all question")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-	// // Validation
-
-	// // Set to DB
-
-	// w.WriteHeader(http.StatusCreated)
-	// json.NewEncoder(w).Encode(question)
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(questions)
 }
 
 func (qc QuestionController) UpdateQuestion(w http.ResponseWriter, r *http.Request) {
--- peerprep-questions/respository/question.go
+++ peerprep-questions/respository/question.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/CS3219-AY2425S1/cs3219-ay2425s1-project-g32/peerprep-questions/db"
 	"github.com/CS3219-AY2425S1/cs3219-ay2425s1-project-g32/peerprep-questions/model"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -29,4 +30,39 @@ func (qr QuestionRepository) CreateQuestion(question model.Question) (*mongo.Ins
 		return nil, err
 	}
 	return result, nil
+}
+
+func (qr QuestionRepository) ListQuestions() ([]model.Question, error) {
+	collection := db.GetCollection(qr.mongoClient, "questions")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// // Set up options for pagination
+	// opts := options.Find()
+	// opts.SetSkip(int64((page - 1) * limit)) // Calculate the number of documents to skip
+	// opts.SetLimit(int64(limit))              // Limit the number of documents returned
+
+	// Find the questions with the given filter
+	cursor, err := collection.Find(ctx, bson.M{})
+	// cursor, err := collection.Find(ctx, filter, opts)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	// Iterate through the cursor and collect results
+	var questions []model.Question
+	for cursor.Next(ctx) {
+		var question model.Question
+		if err := cursor.Decode(&question); err != nil {
+			return nil, err
+		}
+		questions = append(questions, question)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	return questions, nil
 }
