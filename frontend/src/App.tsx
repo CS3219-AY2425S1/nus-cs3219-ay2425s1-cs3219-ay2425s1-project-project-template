1b0cb506ff9423eaeef9a0320444d901bf4283cc
Hmuu Myat Moe <hmuumyatmoe@gmail.com>
2024-09-25 00:42:17 +0800

    Setup tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,20 @@ import React from "react";
 const App: React.FC = () => {
   return (
     <div>
+      <header className="bg-white p-2">
+      <div className="flex lg:flex-1">
+        <a href="#" className="-m-1.5 p-1.5">
+          <span className="sr-only">Your Company</span>
+          <text className="text-amber-600">Peer</text>
+          <text>Prep</text>
+          </a>
+      </div>
+      
+      </header>
+
       <h1>Hello, World!</h1>
+      
+      <h1 className="text-4xl">TEST</h1>
     </div>
   );
 };
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./*.html', "./src/*.{js,ts,jsx,tsx}"], // tell tailwind where our html is
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
