aff62624132a0805fb42d0c635ba565657ba7d1a
blaukc <brandonlau1@hotmail.com>
2024-09-24 18:17:05 +0800

    Add .env

--- peerprep-questions/db/db.go
+++ peerprep-questions/db/db.go
@@ -3,15 +3,23 @@ package db
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func ConnectDB() *mongo.Client {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	mongoURI := os.Getenv("MONGO_URI")
+
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI("").SetServerAPIOptions(serverAPI)
+	opts := options.Client().ApplyURI(mongoURI).SetServerAPIOptions(serverAPI)
 
 	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
