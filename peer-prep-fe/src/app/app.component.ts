40a8ac20161fd63d45c03c1116a15dd08cf11d66
smolegz <zhuohui.koo@gmail.com>
2024-09-26 02:31:03 +0800

    Add front-end to Peerprep

---
+++ peer-prep-fe/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HomeComponent } from './home/home.component';
+
+const MODULES = [
+  CommonModule,
+  RouterOutlet,
+  HomeComponent,
+  LoginComponent,
+  DashboardComponent,
+  RouterLinkActive,
+  RouterLink,
+  MatSlideToggleModule
+];
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: MODULES,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'peer-prep-fe'
+}
