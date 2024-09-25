49216fbbfea50818d6761f23ec7b30eaf637cf48
jehousoh <e0694467@u.nus.edu>
2024-09-25 15:54:05 +0800

    Add basic test cases

--- backend/question/src/routes/questionRoutes.ts
+++ backend/question/src/routes/questionRoutes.ts
@@ -1,7 +1,11 @@
-import express, { Request, Response } from 'express';
-import { validationResult } from 'express-validator';
+import express, { Request, Response } from "express";
+import { validationResult } from "express-validator";
 import Question, { TQuestion } from "../models/Question";
-import { createQuestionValidators, idValidators, updateQuestionValidators } from './validators';
+import {
+  createQuestionValidators,
+  idValidators,
+  updateQuestionValidators,
+} from "./validators";
 
 /**
  * Router for the question service.
@@ -9,134 +13,127 @@ import { createQuestionValidators, idValidators, updateQuestionValidators } from
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello from question service!');
+router.get("/", (req, res) => {
+  res.send("Hello from question service!");
 });
 
 // Create a new question
-router.post('/create', [
-    ...createQuestionValidators,
-], async (req: Request, res: Response) => {
+router.post(
+  "/create",
+  [...createQuestionValidators],
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
     try {
-        const { title, description, category, complexity } = req.body;
-        const question = { title, description, category, complexity };
-        const newQuestion = new Question(question);
-        await newQuestion.save();
-        return res.status(200).json({ message: 'Question created succesfully', question: newQuestion });
+      const { title, description, category, complexity } = req.body;
+      const question = { title, description, category, complexity };
+      const newQuestion = new Question(question);
+      await newQuestion.save();
+      return res.status(200).json({
+        message: "Question created successfully",
+        question: newQuestion,
+      });
     } catch (error) {
-        return res.status(500).send('Internal server error');
+      return res.status(500).send("Internal server error");
     }
-});
+  }
+);
 // Retrieve all questions
-router.get('/all', async (req: Request, res: Response) => {
-    try {
-        const questions = await Question
-            .find()
-            .lean()
-            .sort({ questionid: 'ascending' })
-            .exec();
-        console.log(questions);
-        return res.json(questions);
-    }
-    catch (error) {
-        return res.status(500).send('Internal server error');
-    }
+router.get("/all", async (req: Request, res: Response) => {
+  try {
+    const questions = await Question.find()
+      .lean()
+      .sort({ questionid: "ascending" })
+      .exec();
+    console.log(questions);
+    return res.json(questions);
+  } catch (error) {
+    return res.status(500).send("Internal server error");
+  }
 });
 
 // Retrieve a specific question by id
-router.get('/:id', [
-    ...idValidators,
-], async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    const questionId = parseInt(req.params.id, 10);
-    try {
-        const question = await Question
-            .findOne({ questionid: questionId })
-            .exec();
-        return res.json(question);
-    }
-    catch (error) {
-        res.status(500).send('Internal server error');
-    }
+router.get("/:id", [...idValidators], async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const questionId = parseInt(req.params.id, 10);
+  try {
+    const question = await Question.findOne({ questionid: questionId }).exec();
+    return res.json(question);
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
 });
 
 // Update a specific question by id
-router.post('/:id/update', 
-    [
-        ...updateQuestionValidators,
-    ],
-    async (req: Request, res: Response) => {
+router.post(
+  "/:id/update",
+  [...updateQuestionValidators],
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const questionId = parseInt(req.params.id);
     const updateData: Partial<TQuestion> = {};
     if (req.body.title) {
-        updateData.title = req.body.title;
+      updateData.title = req.body.title;
     }
     if (req.body.description) {
-        updateData.description = req.body.description;
+      updateData.description = req.body.description;
     }
     if (req.body.category) {
-        updateData.category = req.body.category;
+      updateData.category = req.body.category;
     }
     if (req.body.complexity) {
-        updateData.complexity = req.body.complexity;
+      updateData.complexity = req.body.complexity;
     }
 
     try {
-        const updatedQuestion = await Question
-            .findOneAndUpdate(
-                { questionid: questionId },
-                { $set: updateData },
-            );
-        return res.json(updatedQuestion);
+      const updatedQuestion = await Question.findOneAndUpdate(
+        { questionid: questionId },
+        { $set: updateData }
+      );
+      return res.json(updatedQuestion);
+    } catch (error) {
+      //to catch pre-middleware defined error
+      if (error instanceof Error) {
+        return res.status(400).json({ message: "Question not found" });
+      }
+      return res.status(500).send("Internal server error");
     }
-    catch (error) {
-        //to catch pre-middleware defined error
-        if (error instanceof Error) {
-            return res.status(400).json({ message: 'Question not found' });
-        }
-        return res.status(500).send('Internal server error');
-    }
-});
+  }
+);
 
 // Delete a specific question by id
-router.post('/:id/delete', [
-    ...idValidators,
-], async (req: Request, res: Response) => {
+router.post(
+  "/:id/delete",
+  [...idValidators],
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const questionId = parseInt(req.params.id);
     try {
-        const deletedQuestion = await Question
-            .findOneAndDelete({ questionid: questionId })
-            .exec();
-        if (!deletedQuestion) {
-            return res.status(404).json({ message: 'Question not found' });
-        }
-        return res.json(deletedQuestion);
+      const deletedQuestion = await Question.findOneAndDelete({
+        questionid: questionId,
+      }).exec();
+      return res.json(deletedQuestion);
+    } catch (error) {
+      //to catch pre-middleware defined error
+      if (error instanceof Error) {
+        return res.status(400).json({ message: "Question not found" });
+      }
+      return res.status(500).send("Internal server error");
     }
-    catch (error) {
-        //to catch pre-middleware defined error
-        if (error instanceof Error) {
-            return res.status(400).json({ message: 'Question not found' });
-        }
-        return res.status(500).send('Internal server error');
-    }
-
-});
+  }
+);
 
 export default router;
--- backend/question/src/tests/app.spec.ts
+++ backend/question/src/tests/app.spec.ts
@@ -2,22 +2,233 @@ import app from "../server";
 import * as db from "./db";
 import supertest from "supertest";
 const request = supertest(app);
-describe("Test request with mongoose", () => {
+
+// Connect to DB and test /api/
+describe("Connect DB", () => {
   beforeAll(async () => {
     await db.connect();
   });
-  afterEach(async () => {
-    await db.clearDatabase();
-  });
-  afterAll(async () => {
-    await db.closeDatabase();
-  });
 
-  test("GET - /", async () => {
+  test("GET - /api/", async () => {
     const res = await request.get("/api/").send();
     const body = res.body;
     const message = body.message;
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe("Hello from question service!");
+  });
+});
+
+// Test /api/create
+describe("Test Question API", () => {
+  // Valid create
+  test("POST /api/create - should create a new question", async () => {
+    const newQuestion = {
+      title: "Sample Question",
+      description: "This is a sample question",
+      category: "General",
+      complexity: "Easy",
+    };
+
+    const res = await request.post("/api/create").send(newQuestion);
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe("Question created successfully");
+    expect(res.body.question).toHaveProperty("title", "Sample Question");
+    expect(res.body.question).toHaveProperty(
+      "description",
+      "This is a sample question"
+    );
+    expect(res.body.question).toHaveProperty("category", "General");
+    expect(res.body.question).toHaveProperty("complexity", "Easy");
+  });
+
+  // Empty title
+  test("POST /api/create - empty title", async () => {
+    const newQuestion = {
+      title: "",
+      description: "This is a sample question",
+      category: "General",
+      complexity: "Easy",
+    };
+
+    const res = await request.post("/api/create").send(newQuestion);
+    expect(res.statusCode).toBe(400);
+    expect(res.body.errors[0].msg).toBe("Invalid value");
+    expect(res.body.errors[0].path).toBe("title");
+  });
+
+  // Empty description
+  test("POST /api/create - empty description", async () => {
+    const newQuestion = {
+      title: "Sample Question",
+      description: "",
+      category: "General",
+      complexity: "Easy",
+    };
+
+    const res = await request.post("/api/create").send(newQuestion);
+    expect(res.statusCode).toBe(400);
+    expect(res.body.errors[0].msg).toBe("Invalid value");
+    expect(res.body.errors[0].path).toBe("description");
+  });
+
+  // Empty category
+  test("POST /api/create - empty category", async () => {
+    const newQuestion = {
+      title: "Sample Question",
+      description: "This is a sample question",
+      category: "",
+      complexity: "Easy",
+    };
+
+    const res = await request.post("/api/create").send(newQuestion);
+    expect(res.statusCode).toBe(400);
+    expect(res.body.errors[0].msg).toBe("Invalid value");
+    expect(res.body.errors[0].path).toBe("category");
+  });
+
+  // Empty complexity
+  test("POST /api/create - empty complexity", async () => {
+    const newQuestion = {
+      title: "Sample Question",
+      description: "This is a sample question",
+      category: "General",
+      complexity: "",
+    };
+
+    const res = await request.post("/api/create").send(newQuestion);
+    expect(res.statusCode).toBe(400);
+    expect(res.body.errors[0].msg).toBe("Invalid value");
+    expect(res.body.errors[0].path).toBe("complexity");
+  });
+});
+
+// Test /api/all
+describe("Test Get All", () => {
+  // Get all
+  test("GET /api/all - should retrieve all questions", async () => {
+    const res = await request.get("/api/all").send();
+    const sampleQuestion = res.body[0];
+    expect(res.statusCode).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+    expect(res.body.length).toBe(1);
+    expect(sampleQuestion).toHaveProperty("title", "Sample Question");
+    expect(sampleQuestion).toHaveProperty(
+      "description",
+      "This is a sample question"
+    );
+    expect(sampleQuestion).toHaveProperty("category", "General");
+    expect(sampleQuestion).toHaveProperty("complexity", "Easy");
+  });
+});
+
+// Test /api/{id}
+describe("Test Get by Id", () => {
+  // Valid id
+  test("GET /api/:id - valid id", async () => {
+    const questionId = 1090; // We start with id 1090
+    const res = await request.get(`/api/${questionId}`).send();
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty("questionid", questionId);
+    expect(res.body).toHaveProperty("title", "Sample Question");
+    expect(res.body).toHaveProperty("description", "This is a sample question");
+    expect(res.body).toHaveProperty("category", "General");
+    expect(res.body).toHaveProperty("complexity", "Easy");
+  });
+
+  // Negative id
+  test("GET /api/:id - negative id", async () => {
+    const questionId = -1;
+    const res = await request.get(`/api/${questionId}`).send();
+    expect(res.statusCode).toBe(400);
+    expect(res.body.errors[0].msg).toBe("Invalid value");
+  });
+
+  // Non-existent id
+  // test("GET /api/:id - non existent id", async () => {
+  //   const questionId = 999999;
+  //   const res = await request.get(`/api/${questionId}`).send();
+  //   expect(res.statusCode).toBe(500);
+  // });
+});
+
+// Test /api/{id}/update
+describe("Test Update", () => {
+  // Valid update
+  test("POST - valid update", async () => {
+    const updateQuestion = {
+      title: "Update Title",
+      description: "Update Description",
+      category: "Update Category",
+      complexity: "Update Complexity",
+    };
+    const questionId = 1090;
+    const res = await request
+      .post(`/api/${questionId}/update`)
+      .send(updateQuestion);
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty("questionid", questionId);
+  });
+
+  // Empty update
+  test("POST - valid update", async () => {
+    const updateQuestion = {
+      title: "",
+      description: "",
+      category: "",
+      complexity: "",
+    };
+    const questionId = 1090;
+    const res = await request
+      .post(`/api/${questionId}/update`)
+      .send(updateQuestion);
+    expect(res.statusCode).toBe(400);
+    expect(res.body.errors[0].msg).toBe("At least one field must be provided");
+  });
+
+  // Non-existent id
+  test("POST - valid update", async () => {
+    const updateQuestion = {
+      title: "Update Title",
+      description: "Update Description",
+      category: "Update Category",
+      complexity: "Update Complexity",
+    };
+    const questionId = 999999;
+    const res = await request
+      .post(`/api/${questionId}/update`)
+      .send(updateQuestion);
+    expect(res.statusCode).toBe(400);
+    expect(res.body.message).toBe("Question not found");
+  });
+});
+
+// Test /api/{id}/delete
+describe("Test Delete", () => {
+  afterAll(async () => {
+    await db.clearDatabase();
+    await db.closeDatabase();
+  });
+
+  // Valid delete
+  test("POST - valid delete", async () => {
+    const questionId = 1090;
+    const res = await request.post(`/api/${questionId}/delete`).send();
+    expect(res.statusCode).toBe(200);
+  });
+
+  // Negative id
+  test("POST - valid delete", async () => {
+    const questionId = -1;
+    const res = await request.post(`/api/${questionId}/delete`).send();
+    expect(res.statusCode).toBe(400);
+    expect(res.body.errors[0].msg).toBe("Invalid value");
+  });
+
+  // Non-existent id
+  test("POST - valid delete", async () => {
+    const questionId = 999999;
+    const res = await request.post(`/api/${questionId}/delete`).send();
+    expect(res.statusCode).toBe(400);
+    expect(res.body.message).toBe("Question not found");
   });
 });
