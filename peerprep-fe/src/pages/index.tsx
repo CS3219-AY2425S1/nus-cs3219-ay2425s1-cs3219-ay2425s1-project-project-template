81ed96893564e5bb1b69ea32082f13276c3abfc5
Tan Lip Wei <53000252+lipwei1808@users.noreply.github.com>
2024-09-26 21:51:09 +0800

    add login page

--- peerprep-fe/src/pages/index.tsx
+++ peerprep-fe/src/pages/index.tsx
@@ -1,3 +1,74 @@
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import Input from '@/components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+const FormSchema = z.object({
+  username: z.string().min(1, 'Please enter username'),
+  password: z.string().min(1, 'Enter your password nig'),
+});
+
 export default function Home() {
-  return <>Check components/ui for components</>;
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = () => {
+    console.log(form.getValues('username'));
+    console.log(form.getValues('password'));
+  };
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-y-8">
+      <div className="text-4xl font-bold">PeerPrep</div>
+      <div className="w-72 rounded-lg bg-gray-100 p-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              name="username"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="mb-4">
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="mt-6 w-full" type="submit">
+              Login
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 }
