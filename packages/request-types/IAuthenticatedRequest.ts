6c2c8b981d1f6849a8c2b92df476ac6311825586
Glemen Neo <glemenneo@gmail.com>
2024-09-27 03:05:00 +0800

    [#36] [#37] fix: resolve passport express user namespace type merge error

--- packages/request-types/IAuthenticatedRequest.ts
+++ packages/request-types/IAuthenticatedRequest.ts
@@ -4,3 +4,13 @@ import { Request } from 'express'
 export interface IAuthenticatedRequest extends Request {
     user?: IUserDto
 }
+
+// We need to redeclare the global namespace to include the IUserDto interface in the Express.User interface.
+// Passport declares a global namespace for Express.User, so we need to merge our IUserDto interface with it.
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace Express {
+        // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+        interface User extends IUserDto {}
+    }
+}
