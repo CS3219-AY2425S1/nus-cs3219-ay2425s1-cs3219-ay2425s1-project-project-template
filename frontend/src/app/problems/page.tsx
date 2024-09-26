08ce5b5e739a55dca525e488b04e7b909461fd19
wesho1107 <wesho1107@gmail.com>
2024-09-26 22:35:45 +0800

    Implement await function to get questions

--- frontend/src/app/problems/page.tsx
+++ frontend/src/app/problems/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Button } from "@/components/ui/button"
 import {
     Table,
@@ -16,6 +17,7 @@ import {
 } from "@/components/ui/tooltip"
 import { Badge } from "@/components/ui/badge"
 import CompletedIcon from '@mui/icons-material/TaskAlt';
+import { useEffect, useState } from "react";
 
 interface Problem {
     id: number
@@ -27,6 +29,25 @@ interface Problem {
 }
 
 export default function Problems(props: { problems: any }) {
+    const [questions, setQuestions] = useState([])
+  
+    const fetchQuestions = async () => {
+      try {
+        const response = await fetch('http://localhost:5001/get-questions')
+        if (!response.ok) {
+          throw new Error('Failed to fetch questions')
+        }
+        const data = await response.json()
+        setQuestions(data)
+      } catch (err) {
+        console.log("Error", err)
+      }
+    }
+  
+    useEffect(() => {
+      fetchQuestions()
+    }, [])
+
     // const { problems } = props
     let problems: Problem[] = [
         { id: 1, title: "Two Sum", description: "Find two numbers that add up to a target value.", category: ["Array", "Hash Table"], difficulty: "Easy", status: true },
