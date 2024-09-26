dcf104282fa6519c95ce54a79cf73e1f31fee0d9
anunayajoshi <anunayajoshi@gmail.com>
2024-09-26 10:20:53 +0800

    insert questions schema

---
+++ backend/question/drizzle/0001_new_questions_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS "questions" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"difficulty" varchar(50) NOT NULL,
+	"topic" varchar(100) NOT NULL,
+	"description" text NOT NULL,
+	"created_at" timestamp (6) with time zone DEFAULT now(),
+	"updated_at" timestamp (6) with time zone DEFAULT now()
+);
+--> statement-breakpoint
+DROP TABLE "tableName";
--- backend/question/src/lib/db/schema.ts
+++ backend/question/src/lib/db/schema.ts
@@ -1,5 +1,11 @@
-import { pgTable, uuid } from 'drizzle-orm/pg-core';
+import { pgTable, uuid, varchar, text, timestamp } from 'drizzle-orm/pg-core';
 
-export const tableName = pgTable('tableName', {
-  id: uuid('id').unique().primaryKey().defaultRandom(),
+export const questions = pgTable('questions', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  title: varchar('title', { length: 255 }).notNull(),
+  difficulty: varchar('difficulty', { length: 50 }).notNull(),
+  topic: varchar('topic', { length: 100 }).notNull(),
+  description: text('description').notNull(),
+  createdAt: timestamp('created_at', { precision: 6, withTimezone: true }).defaultNow(),
+  updatedAt: timestamp('updated_at', { precision: 6, withTimezone: true }).defaultNow(),
 });
