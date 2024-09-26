d31fde0d4c685446551567c7c637784c1aabdc07
Nam Do Hyun <brandonnam2020@gmail.com>
2024-09-26 16:04:23 +0800

    Add getQuestion, updateQuestion, and deleteQuestion

--- backend/controllers/questionController.js
+++ backend/controllers/questionController.js
@@ -11,9 +11,20 @@ exports.getQuestions = async (req, res, next) => {
     }
 }
 
+// returns a question with the specified id
+exports.getQuestion = async (req, res, next) => {
+    try {
+        const question = await Question.findById(req.params.id)
+        res.status(200).json(question)
+    } catch(error) {
+        res.status(400).json({ message: 'Cannot find quesiton.' })
+    }
+
+}
+
 // adds new question
 exports.addQuestion = async (req, res, next) => {
-    const { title, description, category, complexity } = req.body;
+    const { title, description, category, complexity } = req.body
     if (!title || !description || !category || !complexity) {
         return res.status(400).json({ message: 'Please enter all fields.'})
     }
@@ -35,5 +46,32 @@ exports.addQuestion = async (req, res, next) => {
         })
     } catch(error) {
         return res.status(400).json({ message: 'Invalid question data.' })
+    }
+}
+
+// updates a question with the specified id
+exports.updateQuestion = async (req, res, next) => {
+    const { title, description, category, complexity } = req.body
+    try {
+        const question = await Question.findById(req.params.id)
+        question.title = title
+        question.description = description
+        question.category = category
+        question.complexity = complexity
+        await question.save()
+
+        res.status(200).json(question)
+    } catch(error) {
+        return res.status(400).json({ message: 'Invalid question data.' })
+    }
+}
+
+// deletes a question with the specified id
+exports.deleteQuestion = async (req, res, next) => {
+    try {
+        const question = await Question.deleteOne({ _id: req.params.id })
+        res.status(200).json({ message: 'Question removed' })
+    } catch(error) {
+        return res.status(400).json({ message: 'Question not found' })
     }
 }
--- backend/routes/questionsRoutes.js
+++ backend/routes/questionsRoutes.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const { getQuestions, addQuestion } = require('../controllers/questionController');
+const { getQuestion, getQuestions, addQuestion, updateQuestion, deleteQuestion } = require('../controllers/questionController');
 
+// get a question with the specified id
+router.route('/:id').get(getQuestion)
+
+// get all questions
 router.route('/').get(getQuestions)
+
+// add new question
 router.route('/').post(addQuestion)
+
+// update a question with the specified id
+router.route('/:id').put(updateQuestion)
+
+// delete a question with the specified id
+router.route('/:id').delete(deleteQuestion) 
 
 module.exports = router;
