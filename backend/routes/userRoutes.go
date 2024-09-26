9e1a26c0ea303a9d232d1333f91019da5ba9452e
Hwww23 <122247552+Hwww23@users.noreply.github.com>
2024-09-26 22:10:13 +0800

    remove comments for logout

--- backend/routes/userRoutes.go
+++ backend/routes/userRoutes.go
@@ -12,6 +12,5 @@ func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("/v1/signup", controller.SignUp())
 	incomingRoutes.POST("/v1/login", controller.Login())
 	incomingRoutes.POST("/v1/refresh", controller.RefreshToken)
-	//incomingRoutes.POST("/v1/logout", controller.Logout())
 	incomingRoutes.POST("/v1/logout", middleware.Authentication(), controller.Logout())
 }
