8101f03e5fc799297a673a91b27c88f3838ee6ef
TresaAnnThomas <teeesa728@gmail.com>
2024-09-27 14:54:41 +0800

    Create List of Questions page with pop-up functionality
    - Implemented the list of questions with a pop-up box that triggers upon clicking a question.
    - TODO: Add a scroll bar for better navigation through long question lists.
    - TODO: Implement a dropdown menu for filtering questions by topic and difficulty.

--- peer-prep-fe/src/app/app.component.ts
+++ peer-prep-fe/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { QuestionListComponent } from '../components/question-list/question-list.component';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QuestionListComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ peer-prep-fe/src/components/question-explanation-box/question-explanation-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question-explanation-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './question-explanation-box.component.html',
+  styleUrl: './question-explanation-box.component.css'
+})
+export class QuestionExplanationBoxComponent {
+
+}
