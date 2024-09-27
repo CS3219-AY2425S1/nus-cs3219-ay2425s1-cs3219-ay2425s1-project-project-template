9fa83902c59a54e594a2928c53d36d060a19c743
Cyrus Krispin <cyruskrispin@gmail.com>
2024-09-27 18:49:01 +0800

    titleSlug

--- backend/question-service/question_service/service/question_service.py
+++ backend/question-service/question_service/service/question_service.py
@@ -16,16 +16,16 @@ async def get_questions() -> List[Question]:
 
 
 async def get_question(title_slug: str) -> Question:
-    question = await question_collection.find_one({"title_slug": title_slug})
+    question = await question_collection.find_one({"titleSlug": title_slug})
     return question
 
 
 async def update_question(title_slug: str, updated_question: Question) -> Question:
     question = await question_collection.find_one_and_update(
-        {"title_slug": title_slug}, {"$set": updated_question.model_dump()}, return_document=True
+        {"titleSlug": title_slug}, {"$set": updated_question.model_dump()}, return_document=True
     )
     return question
 
 
 async def delete_question(title_slug: str) -> None:
-    await question_collection.delete_one({"title_slug": title_slug})
+    await question_collection.delete_one({"titleSlug": title_slug})
