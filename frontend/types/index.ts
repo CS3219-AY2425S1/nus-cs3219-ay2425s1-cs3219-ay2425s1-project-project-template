c11ebbec0c38e817a6f80ed8f519300bbeee5672
revdrag <wanghelin67626343@gmail.com>
2024-09-27 16:44:03 +0800

    Add QuestionForm

--- frontend/types/index.ts
+++ frontend/types/index.ts
@@ -3,3 +3,10 @@ import { SVGProps } from "react";
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+
+export interface Question {
+  questionTitle: string;
+  questionDifficulty: string;
+  questionTopics: string[];
+  questionDescription: string;
+}
