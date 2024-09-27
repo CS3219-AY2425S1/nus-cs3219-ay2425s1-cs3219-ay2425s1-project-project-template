46c5ec15c8d4edeeb72b17e5189f03f6c2186210
arkarsg <alxarkar@gmail.com>
2024-09-27 17:31:27 +0800

    export question service as module

---
+++ backend/question-service/question_service/service/__init__.py
@@ -0,0 +1,3 @@
+from .question_service import create_question, delete_question, get_question, get_questions, update_question
+
+__all__ = ["create_question", "get_questions", "get_question", "update_question", "delete_question"]
