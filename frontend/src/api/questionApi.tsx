a4b79962e52883783b6e7cd55a294a6096dbb81e
JunHao247 <e0726760@u.nus.edu>
2024-09-27 13:54:05 +0800

    implement debounce function on search

--- frontend/src/api/questionApi.tsx
+++ frontend/src/api/questionApi.tsx
@@ -14,17 +14,23 @@ export const getAllQuestions = async ({
   sort: string;
   order: string;
   search?: string;
-}) => {
+}, 
+signal: AbortSignal
+) => {
   try {
     let url = `${API_URL}/api/questions?page=${page}&limit=${limit}&sort=${sort}&order=${order}`;
     if (search) {
       url += `&search=${encodeURIComponent(search)}`;
     }
-    const response = await axios.get(url);
+    const response = await axios.get(url, {signal});
     console.log(response);
     return response.data;
-  } catch (error) {
+  } catch (error: any) {
+    if (axios.isCancel(error)) {
+      console.log("Request canceled:", error.message);
+    } else {
     console.error("Error fetching questions:", error);
     throw error;
+    }
   }
 };
