5a9c6f763363aa71a14d484091e2ff37a66c427b
arkarsg <alxarkar@gmail.com>
2024-09-27 17:44:43 +0800

    create settings object

--- backend/question-service/question_service/config.py
+++ backend/question-service/question_service/config.py
@@ -3,9 +3,20 @@ Global config object
     ie DB Connection strings etc
 """
 
-import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Config:
-    origins = ["http://localhost","http://localhost:3000"]
-    db = os.getenv("QUESTION_MONGO_URL", "http://localhost:27017")
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file="../.env",
+        env_ignore_empty=True,
+        extra="ignore",
+    )
+
+    QUESTION_DB_URL: str = "mongodb://localhost:27017"
+    DB_NAME: str = "questions_db"
+    COLLECTION_NAME: str = "questions"
+    ORIGINS: list[str] = ["http://localhost", "http://localhost:3000"]
+
+
+settings = Settings()
