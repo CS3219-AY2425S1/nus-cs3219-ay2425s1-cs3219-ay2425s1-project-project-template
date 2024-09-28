15699c980079c05b764215b381b52a22db87ae97
Mai Ting Kai <wheatgrass.mc@gmail.com>
2024-09-28 13:11:41 +0800

    Implement find match page

---
+++ frontend/src/types/find-match.tsx
@@ -0,0 +1,15 @@
+export enum QuestionDifficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+}
+
+export enum QuestionLanguages {
+  PYTHON = "python",
+  JAVASCRIPT = "javascript",
+}
+
+export enum QuestionTopics {
+  GRAPHS = "graphs",
+  TREES = "trees",
+}
