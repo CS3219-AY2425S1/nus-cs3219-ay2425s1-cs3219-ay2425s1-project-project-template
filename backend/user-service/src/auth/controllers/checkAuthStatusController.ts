418685711375777b0eaffd70290614bbd6d283db
oeggy03 <hathu2516@gmail.com>
2024-09-28 19:35:05 +0800

    Add logger msg to auth status checking

--- backend/user-service/src/auth/controllers/checkAuthStatusController.ts
+++ backend/user-service/src/auth/controllers/checkAuthStatusController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { config } from '../../config/envConfig';
+import logger from '../../utils/logger';
 
 const authStatusController = (req: Request, res: Response) => {
     const token = req.cookies.token;
@@ -11,8 +12,10 @@ const authStatusController = (req: Request, res: Response) => {
 
     try {
         jwt.verify(token, config.jwtSecret);
+        logger.info('Token verified successfully!')
         return res.status(200).json({ isAuthenticated: true });
     } catch (error) {
+        logger.warn('Token verification failed.')
         console.error('JWT verification failed:', error);
         return res.status(200).json({ isAuthenticated: false });
     }
