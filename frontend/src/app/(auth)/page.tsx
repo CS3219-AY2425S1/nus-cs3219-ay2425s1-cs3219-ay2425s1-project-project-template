cfd8d2e15ebe3af504726a48dc4c7291ea580e2e
Mai Ting Kai <wheatgrass.mc@gmail.com>
2024-09-28 12:21:13 +0800

    Add new ShadCN components

--- frontend/src/app/(auth)/page.tsx
+++ frontend/src/app/(auth)/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useAuth } from "@/components/auth/AuthContext";
-import LandingPage from "./components/landingPage/LandingPage";
+import LandingPage from "./components/landing-page/LandingPage";
 import Dashboard from "./components/dashboard/Dashboard";
 
 const Home = () => {
