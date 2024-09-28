4d059b83f26fde8dd6e24972c11cd31a3ab1f1e3
yihfei <lim.yih.fei@gmail.com>
2024-09-28 16:20:25 +0800

    Style modal form

--- frontend/src/components/QuestionTable.jsx
+++ frontend/src/components/QuestionTable.jsx
@@ -12,10 +12,26 @@ const QuestionTable = ({ questions, handleDelete, handleCreate, handleEdit }) =>
     questionDifficulty: 'Easy', // Default value
   });
 
+  const predefinedTopics = [
+    "Arrays", 
+    "Strings", 
+    "Trees", 
+    "Graphs", 
+    "Sorting", 
+    "Dynamic Programming", 
+    "Algorithms", 
+    "Data Structures", 
+    "Bit Manipulation", 
+    "Recursion", 
+    "Databases", 
+    "Brainteaser"
+  ];
+
   const [isEditing, setIsEditing] = useState(false); // New state for editing
 
   const toggleForm = () => {
     setShowForm(prevForm => !prevForm); //ensures latest state value
+    
   }
 
   // updates dynamically
@@ -32,8 +48,8 @@ const QuestionTable = ({ questions, handleDelete, handleCreate, handleEdit }) =>
     setFormData({
       questionId: question["Question ID"],
       questionName: question["Question Title"],
-      questionDescription: question["Question Description"], // Adjust as necessary
-      questionTopics: question["Question Categories"], // Adjust as necessary
+      questionDescription: question["Question Description"], 
+      questionTopics: question["Question Categories"], 
       link: question["Link"],
       questionDifficulty: question["Question Complexity"],
     });
@@ -42,18 +58,22 @@ const QuestionTable = ({ questions, handleDelete, handleCreate, handleEdit }) =>
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const topicsArray = Array.isArray(formData.questionTopics) 
-        ? formData.questionTopics 
-        : JSON.parse(formData.questionTopics);
-    console.log(formData)
     const { questionId, questionName, questionDescription, questionTopics, link, questionDifficulty } = formData;
     if (isEditing) {
-      handleEdit(questionId, questionName, questionDescription, topicsArray, link, questionDifficulty);
+      handleEdit(questionId, questionName, questionDescription, questionTopics, link, questionDifficulty);
       setIsEditing(false); 
     } else {
-      handleCreate(questionId, questionName, questionDescription, topicsArray, link, questionDifficulty);
+      handleCreate(questionId, questionName, questionDescription, questionTopics, link, questionDifficulty);
     }
     toggleForm();
+    setFormData({
+      questionId: '',
+      questionName: '',
+      questionDescription: '',
+      questionTopics: '',
+      link: '',
+      questionDifficulty: 'Easy', // Default value
+    })
 
   }
 
@@ -71,15 +91,14 @@ const QuestionTable = ({ questions, handleDelete, handleCreate, handleEdit }) =>
   };
 
   const formatString = (input) => {
-    // Check if the input is an array
     if (Array.isArray(input)) {
         input = input.join(', '); 
+        console.log(input);
     } else if (typeof input !== 'string') {
         console.error('Expected a string or an array but received:', input);
-        return ''; // or some default value
+        return ''; 
     }
-
-    // Perform the replacement if it's a string
+    
     return input.replace(/[\[\]"]/g, "");
 };
   
@@ -87,46 +106,54 @@ const QuestionTable = ({ questions, handleDelete, handleCreate, handleEdit }) =>
   return (
     
     <div className={styles.questionTable}>
-      <section className={styles.tableHeader}>
+      <section className={showForm ? `${styles.tableHeader} ${styles.blurred}` :styles.tableHeader}>
         <h1>Question List</h1>
         <button onClick={toggleForm}>Add Question</button>
       </section>
+      
       {showForm && (
-        <section className={styles.formSection}>
-          <form className={styles.form} onSubmit={handleSubmit}>
-            <div>
-              <label>ID:</label>
-              <input type="text" name="questionId" value={formData.questionId} onChange={handleChange} required />
+        <>
+          <div className={styles.overlay}>
+            <div className={styles.modal}>
+              <button className={styles.closeButton} onClick={toggleForm}>&times;</button>
+              <form className={styles.form} onSubmit={handleSubmit}>
+                <h3>{isEditing? 'Edit question' : 'Add question'}</h3>
+                <div>
+                  <label>ID:</label>
+                  <input type="number" name="questionId" value={formData.questionId} onChange={handleChange} required />
+                </div>
+                <div>
+                  <label>Title:</label>
+                  <input type="text" name="questionName" value={formData.questionName} onChange={handleChange} required />
+                </div>
+                <div>
+                  <label>Description:</label>
+                  <textarea name="questionDescription" value={formData.questionDescription} onChange={handleChange} required />
+                </div>
+                <div>
+                  <label>Topics:</label>
+                  <input type="text" name="questionTopics" value={formData.questionTopics} onChange={handleChange} required />
+                </div>
+                <div>
+                  <label>Link:</label>
+                  <input type="url" name="link" value={formData.link} onChange={handleChange} required />
+                </div>
+                <div>
+                  <label>Difficulty:</label>
+                  <select name="questionDifficulty" value={formData.questionDifficulty} onChange={handleChange}>
+                    <option value="Easy">Easy</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Hard">Hard</option>
+                  </select>
+                </div>
+                <button type="submit">Submit</button>
+              </form>
             </div>
-            <div>
-              <label>Title:</label>
-              <input type="text" name="questionName" value={formData.questionName} onChange={handleChange} required />
-            </div>
-            <div>
-              <label>Description:</label>
-              <textarea name="questionDescription" value={formData.questionDescription} onChange={handleChange} required />
-            </div>
-            <div>
-              <label>Topics:</label>
-              <input type="text" name="questionTopics" value={formData.questionTopics} onChange={handleChange} required />
-            </div>
-            <div>
-              <label>Link:</label>
-              <input type="url" name="link" value={formData.link} onChange={handleChange} required />
-            </div>
-            <div>
-              <label>Difficulty:</label>
-              <select name="questionDifficulty" value={formData.questionDifficulty} onChange={handleChange}>
-                <option value="Easy">Easy</option>
-                <option value="Medium">Medium</option>
-                <option value="Hard">Hard</option>
-              </select>
-            </div>
-            <button type="submit">Submit</button>
-          </form>
-        </section>
+          </div>
+        </>
       )}
-      <section className={styles.tableSection}>
+
+      <section className={showForm ? `${styles.tableSection} ${styles.blurred}` : styles.tableSection} >
         <table className={styles.table}>
           <thead>
             <tr>
@@ -135,6 +162,7 @@ const QuestionTable = ({ questions, handleDelete, handleCreate, handleEdit }) =>
               <th>Categories</th>
               <th>Difficulty</th>
               <th>Link</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
