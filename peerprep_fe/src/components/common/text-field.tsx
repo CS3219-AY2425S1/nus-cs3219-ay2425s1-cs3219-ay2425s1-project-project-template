d0b3fd64bd154c14e689a1e37344b34475f8f077
zheng-ze <zhengze@u.nus.edu>
2024-09-28 21:32:54 +0800

    Add more fields into textfield

--- peerprep_fe/src/components/common/text-field.tsx
+++ peerprep_fe/src/components/common/text-field.tsx
@@ -2,10 +2,16 @@ export default function Textfield({
   name,
   secure = false,
   placeholder_text = "Enter your text",
+  required = false,
+  minLength = 0,
+  maxLength = 100,
 }: {
   name?: string;
   secure?: boolean;
   placeholder_text?: string;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
 }) {
   return (
     <input
@@ -13,6 +19,9 @@ export default function Textfield({
       type={secure ? "password" : "text"}
       className="bg-slate-200 dark:bg-slate-700 rounded-lg w-full p-4 my-3 focus:outline-none"
       placeholder={placeholder_text}
+      required={required}
+      minLength={minLength}
+      maxLength={maxLength}
     />
   );
 }
