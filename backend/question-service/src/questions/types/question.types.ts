99e49c4c664128d54bd01dabb203f1885bd958fd
richavm14 <richavm14@gmail.com>
2024-09-28 22:57:16 +0800

    Add option other to QuestionCategory enum

--- backend/question-service/src/questions/types/question.types.ts
+++ backend/question-service/src/questions/types/question.types.ts
@@ -7,6 +7,7 @@ export enum QuestionCategory {
     DATABASES = 'Databases',
     ARRAYS = 'Arrays',
     BRAINTEASER = 'Brainteaser',
+    OTHER = 'Other',
   }
   
   export enum QuestionComplexity {
