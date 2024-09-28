cdbf3e933a7da29ba27ccb2524df70844de0480c
Zoe Ang <za.zoeang@gmail.com>
2024-09-28 21:04:47 +0800

    Add user service nginx config

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+envsubst '\$USER_SERVICE_ADDR' < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
