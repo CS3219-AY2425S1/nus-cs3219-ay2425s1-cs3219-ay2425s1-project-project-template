58fe701781b93a5ff591b1f7fc9b996ebf1361d7
leesiyuan <lee.si.yuan.20@gmail.com>
2024-09-28 14:58:19 +0800

    Update types.ts

--- question-service/src/models/types.ts
+++ question-service/src/models/types.ts
@@ -3,6 +3,27 @@ import { ObjectId } from 'mongodb';
 
 // object id schema
 const objectIdSchema = z.instanceof(ObjectId);
+// image file schema
+const MAX_FILE_SIZE = 5000000
+const ACCEPTED_IMAGE_TYPES = [
+ 'image/jpeg',
+ 'image/jpg',
+ 'image/png',
+ 'image/webp',
+]
+
+const imageSchema = z
+  .any()
+  // To not allow empty files
+  .refine((files) => files?.length >= 1, { message: 'Image is required.' })
+  // To not allow files other than images
+  .refine((files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type), {
+    message: '.jpg, .jpeg, .png and .webp files are accepted.',
+   })
+  // To not allow files larger than 5MB
+  .refine((files) => files?.[0]?.size <= MAX_FILE_SIZE, {
+    message: `Max file size is 5MB.`,
+   })
 
 export const QuestionsSchema = z.object({
   _id: objectIdSchema,
@@ -13,7 +34,7 @@ export const QuestionsSchema = z.object({
   tags: z.array(z.string()),
   title_slug: z.string(),
   title: z.string(),
-  pictures: z.array(z.instanceof(File)).optional(),
+  pictures: z.array(imageSchema).optional(),
 });
 
 export const UserQuestionsSchema = z.object({
