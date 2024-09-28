d8221bef94e803585cea1fa70d6024fb3fb1c667
Kervyn <73232587+kervyntan@users.noreply.github.com>
2024-09-28 23:47:13 +0800

    Update

--- peer-prep-fe/src/components/search-and-filter/search-and-filter.component.ts
+++ peer-prep-fe/src/components/search-and-filter/search-and-filter.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, Output} from '@angular/core';
 import {EditPageComponent} from "../../edit-page/edit-page.component";
 import {MatDialog} from "@angular/material/dialog";
 import {AddPageComponent} from "../../add-page/add-page.component";
+import { QuestionService } from '../../services/question.service';
 
 @Component({
   selector: 'app-search-and-filter',
@@ -13,7 +14,7 @@ import {AddPageComponent} from "../../add-page/add-page.component";
 export class SearchAndFilterComponent {
   @Output() refresh = new EventEmitter<void>();
 
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog, questionService : QuestionService) {}
   openAddModal() {
     const dialogRef = this.dialog.open(AddPageComponent, {
       panelClass: 'custom-modalbox',
@@ -28,5 +29,9 @@ export class SearchAndFilterComponent {
     dialogRef.componentInstance.addComplete.subscribe(() => {
       this.refresh.emit();
     });
+  }
+
+  searchQuestions() {
+    this.qu
   }
 }
