956de9ec53f94f4456f35bd82073abd9e0ebe595
yunruu <yunruuchin@gmail.com>
2024-09-28 11:28:59 +0800

    feat: add custom label styling

--- frontend/components/ui/label.tsx
+++ frontend/components/ui/label.tsx
@@ -6,7 +6,9 @@ interface CustomLabelProps {
 
 export default function CustomLabel({ title, textColor, bgColor }: CustomLabelProps) {
     return (
-        <span className={`${textColor} ${bgColor} px-3 rounded-xl text-center flex items-center margin-auto`}>
+        <span
+            className={`${textColor} ${bgColor} px-3 rounded-xl text-center flex items-center justify-center margin-auto capitalize`}
+        >
             {title}
         </span>
     )
