114cf1ca562a4833538a6cff843845597beffa07
yunruu <yunruuchin@gmail.com>
2024-09-29 17:21:44 +0800

    feat: add more session datatable columns and column property

--- frontend/pages/sessions/columns.tsx
+++ frontend/pages/sessions/columns.tsx
@@ -1,4 +1,17 @@
-import { IDatatableColumn } from '@/types'
+import { TickIcon, ExclamationIcon } from '@/assets/icons'
+import { DifficultyLabel } from '@/components/customs/difficulty-label'
+import { IDatatableColumn, QuestionStatus } from '@/types'
+
+const convertTimestamp = (millis: number) => {
+    const date = new Date(millis)
+    return date.toLocaleDateString('en-US', {
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+    })
+}
 
 export const columns: IDatatableColumn[] = [
     {
@@ -6,6 +19,38 @@ export const columns: IDatatableColumn[] = [
     },
     {
         key: 'question',
+    },
+    {
+        key: 'description',
+        maxWidth: '40%',
+        offAutoCapitalize: true,
+    },
+    {
+        key: 'status',
+        formatter: (value) => {
+            return (
+                <div className="flex items-center justify-center">
+                    {value === QuestionStatus.COMPLETED ? (
+                        <TickIcon />
+                    ) : value === QuestionStatus.FAILED ? (
+                        <ExclamationIcon />
+                    ) : null}
+                </div>
+            )
+        },
+    },
+    {
+        key: 'difficulty',
+        isSortable: true,
+        formatter: (value) => {
+            return <DifficultyLabel difficulty={value} />
+        },
+    },
+    {
+        key: 'time',
+        formatter: (value) => {
+            return <span>{convertTimestamp(value)}</span>
+        },
     },
 ]
 
