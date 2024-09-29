81586d7b24f9108650911fe312ebf68b29d9aa1d
arkarsg <alxarkar@gmail.com>
2024-09-29 19:51:45 +0800

    remove dependencies for submission

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,7 @@ import { Slide, ToastContainer } from 'react-toastify';
 import './App.css'; // Import your CSS file here
 
 import 'react-toastify/dist/ReactToastify.css';
-import EditorPage from './routes/EditorPage';
 import QuestionPage from './routes/QuestionPage';
-import RegisterPage from './routes/RegisterPage';
 
 
 function App() {
@@ -15,8 +13,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<QuestionPage />} />
-        <Route path="/code" element={<EditorPage />} />
-        <Route path="/register" element={<RegisterPage />} />
         <Route path="/question" element={<QuestionPage />} />
       </Routes>
     </Router>
