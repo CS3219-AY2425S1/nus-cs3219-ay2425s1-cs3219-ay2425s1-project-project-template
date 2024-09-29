3afe5cbe7a5f2f19b5496c936aa2c82bda23fae3
Nam Do Hyun <brandonnam2020@gmail.com>
2024-09-29 21:08:55 +0800

    Add missing dependencies in frontend and set up axios for logging in

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,86 +1,89 @@
 import { Container, CssBaseline, Box, Avatar, Typography, TextField, Button, Grid } from "@mui/material";
-import LoginIcon from '@mui/icons-material/Login';
+import LoginIcon from "@mui/icons-material/Login";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Helmet } from 'react-helmet-async';
+import { Helmet } from "react-helmet-async";
+import axios from "../api.js";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-  const handleLogin = () => {};
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post("/api/users/login", { username, password });
+            console.log(response);
+        } catch (err) {
+            console.log(err)
+        }
+    };
 
-  return (
-    <>
-      <Helmet>
-        <title>Login</title>
-      </Helmet>
-      <Container maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            mt: 20,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "primary.light" }}>
-            <LoginIcon/>
-          </Avatar>
-          <Typography variant="h5">Login</Typography>
-          <Box sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="Username"
-              name="username"
-              autoFocus
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
+    return (
+        <>
+            <Helmet>
+                <title>Login</title>
+            </Helmet>
+            <Container maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        mt: 20,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: "primary.light" }}>
+                        <LoginIcon />
+                    </Avatar>
+                    <Typography variant="h5">Login</Typography>
+                    <Box sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="Username"
+                            name="username"
+                            autoFocus
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
 
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="password"
-              name="password"
-              label="Password"
-              type="password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="password"
+                            name="password"
+                            label="Password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => {
+                                setPassword(e.target.value);
+                            }}
+                        />
 
-            <Grid container justifyContent="flex-end">
-              <Grid item xs>
-                <Link to="/forgot-password">Forgot Password?</Link>
-              </Grid>
-            </Grid>
+                        <Grid container justifyContent="flex-end">
+                            <Grid item xs>
+                                <Link to="/forgot-password">Forgot Password?</Link>
+                            </Grid>
+                        </Grid>
 
-            <Button
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={handleLogin}
-            >
-              Login
-            </Button>
+                        <Button fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} onClick={handleLogin}>
+                            Login
+                        </Button>
 
-            <Grid container justifyContent={"flex-end"}>
-              <Grid item>
-                <Link to="/register">Don't have an account? Register Here.</Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </>
-  );
+                        <Grid container justifyContent={"flex-end"}>
+                            <Grid item>
+                                <Link to="/register">Don't have an account? Register Here.</Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </>
+    );
 };
 
 export default Login;
