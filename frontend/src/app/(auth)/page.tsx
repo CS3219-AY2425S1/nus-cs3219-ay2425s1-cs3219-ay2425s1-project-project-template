7f3dd9ffe19b8b1a09cba8bf47694eb095bad6d7
Izz Hafeez <izzhafeez@gmail.com>
2024-09-29 17:56:54 +0800

    Re-add the unauth page

--- frontend/src/app/(auth)/page.tsx
+++ frontend/src/app/(auth)/page.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-// import { useAuth } from "@/components/auth/AuthContext";
-// import LandingPage from "./components/landing-page/LandingPage";
+import { useAuth } from "@/components/auth/AuthContext";
+import LandingPage from "./components/landing-page/LandingPage";
 // import Dashboard from "./components/dashboard/Dashboard";
 import LeetcodeDashboard from "./leetcode-dashboard/LeetcodeDashboard";
 
 const Home = () => {
-  // const { token } = useAuth();
+  const { token } = useAuth();
 
-  // return (
-  //   token ? <LeetcodeDashboard/> : <LandingPage/>
-  // );
-
-  return <LeetcodeDashboard />;
+  return (
+    token ? <LeetcodeDashboard/> : <LandingPage/>
+  );
 };
 
 export default Home;
