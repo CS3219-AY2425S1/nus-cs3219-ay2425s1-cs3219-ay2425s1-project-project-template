62a7a46f5c5d8c991084e0bd32192046c218a7dd
delishad21 <soojohan@gmail.com>
2024-09-29 19:28:59 +0800

    Remove irrelevant services

--- matching-service/src/routes/socket-routes.ts
+++
@@ -1,9 +0,0 @@
-import { Server, Socket } from "socket.io";
-import { handleDeregisterForMatching, handleDisconnect, handleRegisterForMatching } from "../controller/socket-controller";
-
-export function registerEventHandlers(socket: Socket, io: Server) {
-    handleRegisterForMatching(socket, io);
-    handleDeregisterForMatching(socket);
-    handleDisconnect(socket);
-}
-
