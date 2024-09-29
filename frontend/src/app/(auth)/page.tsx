c0eaf24d6f720c91ea0886750b15aab52e1467db
Izz Hafeez <izzhafeez@gmail.com>
2024-09-29 17:50:10 +0800

    Only show leetcode dashboard, and no landing page and no login

--- frontend/src/app/(auth)/page.tsx
+++ frontend/src/app/(auth)/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 
-import { useAuth } from "@/components/auth/AuthContext";
-import LandingPage from "./components/landing-page/LandingPage";
+// import { useAuth } from "@/components/auth/AuthContext";
+// import LandingPage from "./components/landing-page/LandingPage";
 // import Dashboard from "./components/dashboard/Dashboard";
 import LeetcodeDashboard from "./leetcode-dashboard/LeetcodeDashboard";
 
 const Home = () => {
-  const { token } = useAuth();
+  // const { token } = useAuth();
 
-  return (
-    token ? <LeetcodeDashboard/> : <LandingPage/>
-  );
+  // return (
+  //   token ? <LeetcodeDashboard/> : <LandingPage/>
+  // );
+
+  return <LeetcodeDashboard />;
 };
 
 export default Home;
