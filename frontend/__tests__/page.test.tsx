3f444e88e94d4939d9d72737d9bd91d087d8330b
Davian Kho <daviankho1@gmail.com>
2024-09-30 02:07:11 +0800

    fix: CI issues

--- frontend/__tests__/page.test.tsx
+++ frontend/__tests__/page.test.tsx
@@ -3,10 +3,11 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 
 import Layout from '../components/layout/layout'
+import { NavBar } from '@/components/layout/navbar'
 
 describe('Layout', () => {
     it('renders the logo', () => {
-        render(<Layout children={undefined} />)
+        render(<NavBar />)
 
         const logo = screen.getByRole('img')
         expect(logo).toBeInTheDocument()
