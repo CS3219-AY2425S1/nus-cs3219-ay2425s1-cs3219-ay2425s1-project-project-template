369d49ccb0a4831c519857c8632d82dd7a910e9c
Sean Tin <tinsean@hotmail.com>
2024-09-29 22:15:57 +0800

    Finish code editor backend

--- apps/question-service/src/app/code_editor/page.tsx
+++ apps/question-service/src/app/code_editor/page.tsx
@@ -4,28 +4,43 @@ import {
   Button,
   Col,
   Dropdown,
+  Input,
   Layout,
   message,
   Menu,
+  MenuProps,
+  Pagination,
+  PaginationProps,
   Row,
+  Select,
+  Space,
+  Table,
   TableProps,
+  Tabs,
   Tag,
 } from "antd";
 import { Content } from "antd/es/layout/layout";
 import {
   DeleteOutlined,
+  DownOutlined,
   EditOutlined,
   PlusCircleOutlined,
+  SearchOutlined,
   LeftOutlined,
   RightOutlined,
   CaretRightOutlined,
+  ClockCircleTwoTone,
+  CloseCircleFilled,
+  ClockCircleFilled,
   ClockCircleOutlined,
   CommentOutlined,
+  CheckOutlined,
+  CheckCircleFilled,
   CheckCircleOutlined,
 } from "@ant-design/icons";
 import "./styles.scss";
 import { useEffect, useState } from "react";
-import { Question, GetSingleQuestion } from "../../services/question";
+import { GetQuestions, Question, GetSingleQuestion } from "../services/question";
 import {
   CategoriesOption,
   DifficultyOption,
@@ -33,15 +48,22 @@ import {
 } from "../utils/SelectOptions";
 import React from "react";
 import TextArea from "antd/es/input/TextArea";
-import { useSearchParams } from "next/navigation";
 
 export default function Home() {
   // Table States
+  const [questions, setQuestions] = useState<Question[] | undefined>(undefined); // Store the questions
+  const [totalCount, setTotalCount] = useState<number | undefined>(undefined); // Store the total count of questions
+  const [totalPages, setTotalPages] = useState<number | undefined>(undefined); // Store the total number of pages
   const [currentPage, setCurrentPage] = useState<number | undefined>(1); // Store the current page
+  const [limit, setLimit] = useState<number | undefined>(10); // Store the quantity of questions to be displayed
   const [isLoading, setIsLoading] = useState<boolean>(true); // Store the states related to table's loading
 
   // Filtering States
   const [search, setSearch] = useState<string | undefined>(undefined); // Store the search
+  const [delayedSearch, setDelayedSearch] = useState<string | undefined>(
+    undefined
+  ); // Store the delayed search value
+  // const [categories, setCategories] = useState<string[]>([]); // Store the selected filter categories
   const [difficulty, setDifficulty] = useState<string[]>([]); // Store the selected difficulty level
   const [sortBy, setSortBy] = useState<string | undefined>(undefined); // Store the selected sorting parameter
 
@@ -76,8 +98,25 @@ export default function Home() {
   };
 
   // Include States for Create/Edit Modal (TODO: Sean)
-  const searchParams = useSearchParams();
-  const docRefId = searchParams.get("data")?.toString();
+
+  // When the page is initialised, fetch all the questions and display in table
+  // When the dependencies/states change, the useEffect hook will trigger to re-fetch the questions
+  // useEffect(() => {
+  //   if (!isLoading) {
+  //     setIsLoading(true);
+  //   }
+
+  //   GetQuestions(currentPage, limit, sortBy, difficulty, delayedSearch).then(
+  //     (data) => {
+  //       setQuestions(data.questions);
+  //       setTotalCount(data.totalCount);
+  //       setTotalPages(data.totalPages);
+  //       setCurrentPage(data.currentPage);
+  //       setLimit(data.limit);
+  //       setIsLoading(false);
+  //     }
+  //   );
+  // }, [limit, currentPage, sortBy, difficulty, delayedSearch]); // TODO: (Ryan) Add dependencies for categories and edit the GetQuestion service function
 
   // When code editor page is initialised, fetch the particular question, and display in code editor
   useEffect(() => {
@@ -85,15 +124,113 @@ export default function Home() {
       setIsLoading(true);
     }
 
-    GetSingleQuestion(docRefId).then(
-      (data: any) => {
+    GetSingleQuestion(docRef).then(
+      (data) => {
         setQuestionTitle(data.title);
         setComplexity(data.complexity);
         setCategories(data.categories);
         setDescription(data.description);
       }
     );
-  }, [docRefId])
+  })
+
+  // Delay the fetching of data only after user stops typing for awhile
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDelayedSearch(search);
+      setCurrentPage(1); // Reset the current page
+    }, 800);
+    return () => clearTimeout(timeout);
+  }, [search]);
+
+  // Table column specification
+  const columns: TableProps<Question>["columns"] = [
+    {
+      title: "Id",
+      dataIndex: "id",
+      key: "id",
+      // render: (id: number) => <div>{id}</div>,
+    },
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+      render: (text: string) => <Button type="link">{text}</Button>, // TODO (Sean): Onclick links to the individual question page
+    },
+    {
+      title: "Categories",
+      dataIndex: "categories",
+      key: "categories",
+      render: (categories: string[]) =>
+        categories.map((category) => <Tag>{category}</Tag>),
+    },
+    {
+      title: "Difficulty",
+      dataIndex: "complexity",
+      key: "complexity",
+      render: (difficulty: string) => {
+        let color = "";
+        if (difficulty === "Easy") {
+          color = "#2DB55D";
+        } else if (difficulty === "Medium") {
+          color = "orange";
+        } else if (difficulty === "Hard") {
+          color = "red";
+        }
+        return <div style={{ color }}>{difficulty}</div>;
+      },
+    },
+    {
+      title: "Actions",
+      key: "actions",
+      dataIndex: "id",
+      render: (id: number) => (
+        <div>
+          {/* TODO (Sean): Include Logic to handle retrieving of editable data here and display in a modal component */}
+          <Button className="edit-button" icon={<EditOutlined />}></Button>
+          {/* TODO (Ryan): Include Pop-up confirmation for delete when clicked and link to delete API --> can also explore success notification or look into react-toast*/}
+          <Button
+            className="delete-button"
+            danger
+            icon={<DeleteOutlined />}
+          ></Button>
+        </div>
+      ),
+    },
+  ];
+
+  // Handler for change in multi-select categories option
+  const handleCategoriesChange = (value: string[]) => {
+    setCategories(value);
+    setCurrentPage(1); // Reset the current page
+  };
+
+  // Handler for clearing the filtering options
+  const handleClear = () => {
+    setCategories([]);
+    setDifficulty([]);
+    setSearch(undefined);
+    setSortBy(undefined);
+  };
+
+  // Handler for filtering (TODO)
+  // const handleFilter = async () => {
+  //   success("Filtered Successfully!");
+  // };
+
+  // Handler for show size change for pagination
+  // const onShowSizeChange: PaginationProps["onShowSizeChange"] = (
+  //   current,
+  //   pageSize
+  // ) => {
+  //   setCurrentPage(current);
+  //   setLimit(pageSize);
+  // };
+
+  // // Handler for change in page jumper
+  // const onPageJump: PaginationProps["onChange"] = (pageNumber) => {
+  //   setCurrentPage(pageNumber);
+  // };
 
   const menuItems = [
     {
@@ -141,17 +278,23 @@ export default function Home() {
                     <Row className="problem-description boxes">
                       <text className="problem-description-info">
                         <div className="problem-description-top">
-                          <h3 className="problem-description-title">{questionTitle}</h3>
+                          <h3 className="problem-description-title">Problem Description</h3>
                           <text className="problem-solve-status">Solved&nbsp;<CheckCircleOutlined/></text>
                         </div>
-                        <Tag>{complexity}</Tag>
-                        <div id="tag-container">
-                          <text>Topics: </text>
-                          {categories.map(category => (
-                              <Tag key={category}>{category}</Tag>
-                          ))}
-                        </div>
-                        <text>{description}</text>
+                        <br></br>
+                        <Tag>Easy</Tag>
+                        <br></br>
+                        <text>Topics: </text><Tag>Array</Tag><Tag>Algorithms</Tag>
+                        <br></br>
+                        <text>You are given two integers m and n, which represent the dimensions of a matrix.
+You are also given the head of a linked list of integers.
+Generate an m x n matrix that contains the integers in the linked list presented in spiral order (clockwise), starting from the top-left of the matrix. If there are remaining empty spaces, fill them with -1.
+Return the generated matrix.
+ 
+Example 1:
+Input: m = 3, n = 5, head = [3,0,2,6,8,1,7,9,4,2,5,5,0] Output: [[3,0,2,6,8],[5,0,-1,-1,1],[5,2,4,9,7]] Explanation: The diagram above shows how the values are printed in the matrix. Note that the remaining spaces in the matrix are filled with -1.
+Example 2:
+Input: m = 1, n = 4, head = [0,1,2] Output: [[0,1,2,-1]] Explanation: The diagram above shows how the values are printed from left to right in the matrix. The last space in the matrix is set to -1.</text>
                       </text>
                     </Row>
                     <Row className="test-cases boxes">
