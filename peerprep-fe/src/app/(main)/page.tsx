c8efb1fd7efb2a10213685c1b3ecd9cbb3a8a8d9
ZD292 <ongzhendong@gmail.com>
2024-09-30 00:49:02 +0800

    Edit LoginPage

--- peerprep-fe/src/app/(main)/page.tsx
+++ peerprep-fe/src/app/(main)/page.tsx
@@ -3,7 +3,7 @@ import LandingPage from './components/LandingPage';
 
 // TODO: Replace with actual authentication logic
 // change this to false to see landing page
-const isLoggedIn = false;
+const isLoggedIn = true;
 
 export default function Home() {
   return isLoggedIn ? <LoggedIn /> : <LandingPage />;
