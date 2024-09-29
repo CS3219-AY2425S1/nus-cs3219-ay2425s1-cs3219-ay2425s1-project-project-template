097952bd0b52a655f30b8fc761e84a3cdd87335c
zsheng <zsheng.dev@gmail.com>
2024-09-29 20:20:25 +0800

    fix: handle CORS for post requests
    POST requests have content-type header.
    Preflight requests are OPTIONS request.
    
    We must allow both to use our POST method.

--- backend/service-question/src/main/java/g55/cs3219/backend/questionservice/config/SecurityConfig.java
+++ backend/service-question/src/main/java/g55/cs3219/backend/questionservice/config/SecurityConfig.java
@@ -23,7 +23,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000, http://localhost:5173"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
