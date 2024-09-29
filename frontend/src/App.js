bfe2933de25be73f3624d4a1f7a14102fc5c93ba
Chan How <73763719+ChillinRage@users.noreply.github.com>
2024-09-29 22:42:01 +0800

    Remove user service

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import logo from './styles/logo.svg';
 import './styles/App.css';
-import { Home, Login, Questions, Signup } from './pages';
+import { Home, Questions } from './pages';
 
 function App() {
   return (
     <div className="App">
 
       <Routes>
-        <Route path="/" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/questions" element={<Questions />} />
       </Routes>
     </div>
--- frontend/src/pages/Questions.jsx
+++ frontend/src/pages/Questions.jsx
@@ -1,4 +1,4 @@
-import GeneralNavbar from "../components/GeneralNavbar";
+import Navbar from "../components/Navbar";
 import QuestionTable from '../components/QuestionTable'
 import AddQuestionButton from '../components/AddQuestionButtons'
 import '../styles/questions.css';
@@ -6,7 +6,7 @@ import '../styles/questions.css';
 const Questions = () => {
     return (
         <div>
-            <GeneralNavbar />
+            <Navbar />
             <h1>Questions</h1>
             <p className="description">View all the questions stored in database.</p>
             <div className="question-table-container">
