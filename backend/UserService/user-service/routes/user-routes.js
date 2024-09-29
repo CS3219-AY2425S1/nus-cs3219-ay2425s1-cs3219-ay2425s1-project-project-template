274bc902cde0885ed081c83e065419bb2c32236b
Eugenetanwl3881 <Eugene.tan.wl@gmail.com>
2024-09-29 23:09:47 +0800

    Add comments for routes

--- backend/UserService/user-service/routes/user-routes.js
+++ backend/UserService/user-service/routes/user-routes.js
@@ -8,18 +8,29 @@ import {
   updateUser,
   updateUserPrivilege,
 } from "../controller/user-controller.js";
-import { verifyAccessToken, verifyIsAdmin, verifyIsOwnerOrAdmin } from "../middleware/basic-access-control.js";
+import {
+  verifyAccessToken,
+  verifyIsAdmin,
+  verifyIsOwnerOrAdmin,
+} from "../middleware/basic-access-control.js";
 
 const router = express.Router();
 
 router.get("/", verifyAccessToken, verifyIsAdmin, getAllUsers);
 
-router.patch("/:id/privilege", verifyAccessToken, verifyIsAdmin, updateUserPrivilege);
+//Change user to isAdmin. During creation, default is false
+router.patch(
+  "/:id/privilege",
+  verifyAccessToken,
+  verifyIsAdmin,
+  updateUserPrivilege
+);
 
 router.post("/", createUser);
 
 router.get("/:id", verifyAccessToken, verifyIsOwnerOrAdmin, getUser);
 
+//Update User password only
 router.patch("/:id", verifyAccessToken, verifyIsOwnerOrAdmin, updateUser);
 
 router.delete("/:id", verifyAccessToken, verifyIsOwnerOrAdmin, deleteUser);
