d408dabd530c7685397c912799c0a90856388319
delishad21 <soojohan@gmail.com>
2024-09-30 21:52:18 +0800

    dockerise user and question service

--- question-service/src/server.ts
+++ question-service/src/server.ts
@@ -1,6 +1,6 @@
 import app from "./index";
 
-const PORT = process.env.PORT || 8003;
+const PORT = process.env.QUESTION_PORT || 8003;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
