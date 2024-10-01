ee00e5ccd956d41ef23d9751860357e3b4dfdb17
jeff <jefferyng2000@gmail.com>
2024-10-01 17:01:50 +0800

    feat: Add question dto

---
+++ backend/service-question/src/main/java/g55/cs3219/backend/questionservice/dto/QuestionDto.java
@@ -0,0 +1,36 @@
+package g55.cs3219.backend.questionservice.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+@Setter
+public class QuestionDto implements Serializable {
+
+    private Integer id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private String difficulty;
+
+    @NotNull
+    private List<String> categories;
+
+    @NotNull
+    private List<String> examples;
+
+    @NotNull
+    private List<String> constraints;
+
+    @NotNull
+    private String link;
+}
