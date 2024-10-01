0f7f5ee93907df0b8be68eb01f7060d4e05b58dd
Chenglong <e0958791@u.nus.edu>
2024-10-01 17:28:11 +0800

    Add validation to login and signup fields

--- frontend/src/pages/Login/login.tsx
+++ frontend/src/pages/Login/login.tsx
@@ -7,22 +7,22 @@ import axios from "axios";
 
 export default function LoginPage() {
 
-  const{register, handleSubmit, watch, formState: {errors}} = useForm({
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({
     defaultValues: {
-      username: "",
+      email: "",
       password: "",
     }
   });
 
-  const {mutate, isPending} = useMutation({
-    mutationFn: (data: Record<string, string>)=>{
+  const { mutate, isPending } = useMutation({
+    mutationFn: (data: Record<string, string>) => {
       console.log(data)
-      return axios.post("localhost:4001/login", data)
+      return axios.post("Replace with login API", data)
     },
     onSuccess: (data) => {
       console.log(data);
     },
-    onError: (error) =>{
+    onError: (error) => {
       console.log(error);
     }
   });
@@ -36,22 +36,27 @@ export default function LoginPage() {
   return <div className="flex flex-row min-w-full min-h-[calc(100vh-64px)]">
     <div className="flex-1 bg-black"></div>
     <div className="flex-1 flex flex-col gap-10 bg-white text-black justify-center items-center text-lg">
-    <img className="w-1/4" src="/logo-with-text.svg" />
-    <form className="flex w-3/5 flex-col gap-y-8" onSubmit={handleSubmit(onSubmit)}>
-      <div className="relative flex flex-col">
-        <input className={textBoxStyle} type="text" placeholder="Email" {...register("username", {required: true})}/>
-        <PersonIcon fontSize="medium" className="absolute top-1/2 -translate-y-1/2 translate-x-1/3"/>
-        {errors.username && <span className="absolute -bottom-6 right-0 text-base text-red-500">This field is required</span>}
-      </div>
-      <div className="relative flex flex-col">
-        <input className={textBoxStyle} type="password" placeholder="Password" {...register("password", {required: true})}/>
-        <VpnKeyIcon fontSize="medium" className="absolute top-1/2 -translate-y-1/2 translate-x-1/3"/>
-        {errors.password && <span className="absolute -bottom-6 right-0 text-base text-red-500">This field is required</span>}
-      </div>
-      <div className="flex justify-between items-center">
-        <Link className="hover:underline" to="/forgotpassword">Forgot Password?</Link>
-        <button disabled={isPending} className="bg-buttonColour px-8 py-2 rounded-lg opacity-80 hover:opacity-100 disabled:cursor-not-allowed disabled:opacity-40">Login</button>
-      </div>
+      <img className="w-1/4" src="/logo-with-text.svg" />
+      <form className="flex w-3/5 flex-col gap-y-8" onSubmit={handleSubmit(onSubmit)}>
+        <div className="relative flex flex-col">
+          <input className={textBoxStyle} type="text" placeholder="Email"
+            {...register("email", {
+              required: { value: true, message: "Email is required" },
+              pattern: { value: /[a-z0-9]+@[a-z0-9]+\.[a-z]{2,3}/, message: "Email is invalid" },
+            })} />
+          <PersonIcon fontSize="medium" className="absolute top-1/2 -translate-y-1/2 translate-x-1/3" />
+          <span className="absolute bottom-0 translate-y-full right-0 text-base text-red-500">{errors.email?.message}</span>
+        </div>
+        <div className="relative flex flex-col">
+          <input className={textBoxStyle} type="password" placeholder="Password"
+            {...register("password", { required: { value: true, message: "Password is required." } })} />
+          <VpnKeyIcon fontSize="medium" className="absolute top-1/2 -translate-y-1/2 translate-x-1/3" />
+          <span className="absolute bottom-0 translate-y-full right-0 text-base text-red-500">{errors.password?.message}</span>
+        </div>
+        <div className="flex justify-between items-center">
+          <Link className="hover:underline" to="/forgotpassword">Forgot Password?</Link>
+          <button disabled={isPending} className="bg-buttonColour px-8 py-2 rounded-lg opacity-80 hover:opacity-100 transition-all delay-600 disabled:cursor-not-allowed disabled:opacity-40">Login</button>
+        </div>
       </form>
       <span>Not Registered? <Link className="text-buttonColour hover:underline" to="/signup">Sign up</Link> now!</span>
     </div>
--- frontend/src/pages/Signup/signup.tsx
+++ frontend/src/pages/Signup/signup.tsx
@@ -7,23 +7,25 @@ import axios from "axios";
 
 export default function SignupPage() {
 
-  const{register, handleSubmit, watch, formState: {errors}} = useForm({
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({
     defaultValues: {
-      username: "",
+      email: "",
       password: "",
-      cfmPassword:"",
+      cfmPassword: "",
     }
   });
 
-  const {mutate, isPending} = useMutation({
-    mutationFn: (data: Record<string, string>)=>{
+  const password = watch("password");
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: (data: Record<string, string>) => {
       console.log(data)
-      return axios.post("localhost:4001/signup", data)
+      return axios.post("Replace with signup API", data)
     },
     onSuccess: (data) => {
       console.log(data);
     },
-    onError: (error) =>{
+    onError: (error) => {
       console.log(error);
     }
   });
@@ -37,26 +39,41 @@ export default function SignupPage() {
   return <div className="flex flex-row min-w-full min-h-[calc(100vh-64px)]">
     <div className="flex-1 bg-black"></div>
     <div className="flex-1 flex flex-col gap-10 bg-white text-black justify-center items-center text-lg">
-    <img className="w-1/4" src="/logo-with-text.svg" />
-    <form className="flex w-3/5 flex-col gap-y-8" onSubmit={handleSubmit(onSubmit)}>
-      <div className="relative flex flex-col">
-        <input className={textBoxStyle} type="text" placeholder="Enter email" {...register("username", {required: true})}/>
-        <PersonIcon fontSize="medium" className="absolute top-1/2 -translate-y-1/2 translate-x-1/3"/>
-        {errors.username && <span className="absolute -bottom-6 right-0 text-base text-red-500">This field is required</span>}
-      </div>
-      <div className="relative flex flex-col">
-        <input className={textBoxStyle} type="password" placeholder="Enter password" {...register("password", {required: true})}/>
-        <VpnKeyIcon fontSize="medium" className="absolute top-1/2 -translate-y-1/2 translate-x-1/3"/>
-        {errors.password && <span className="absolute -bottom-6 right-0 text-base text-red-500">This field is required</span>}
-      </div>
-      <div className="relative flex flex-col">
-        <input className={textBoxStyle} type="password" placeholder="Enter password again" {...register("cfmPassword", {required: true})}/>
-        <VpnKeyIcon fontSize="medium" className="absolute top-1/2 -translate-y-1/2 translate-x-1/3"/>
-        {errors.password && <span className="absolute -bottom-6 right-0 text-base text-red-500">This field is required</span>}
-      </div>
-      <div className="flex justify-center items-center">
-        <button disabled={isPending} className="bg-buttonColour px-12 py-2 rounded-lg opacity-80 hover:opacity-100 disabled:cursor-not-allowed disabled:opacity-40">Sign Up</button>
-      </div>
+      <img className="w-1/4" src="/logo-with-text.svg" />
+      <form className="flex w-3/5 flex-col gap-y-8" onSubmit={handleSubmit(onSubmit)}>
+        <div className="relative flex flex-col">
+          <input className={textBoxStyle} type="text" placeholder="Enter email"
+            {...register("email", {
+              required: { value: true, message: "Email is required" },
+              pattern: { value: /[a-z0-9]+@[a-z0-9]+\.[a-z]{2,3}/, message: "Email is invalid" },
+            })} />
+          <PersonIcon fontSize="medium" className="absolute top-1/2 -translate-y-1/2 translate-x-1/3" />
+          <span className="absolute bottom-0 translate-y-full right-0 text-base text-red-500">{errors.email?.message}</span>
+        </div>
+        <div className="relative flex flex-col">
+          <input className={textBoxStyle} type="password" placeholder="Enter password"
+            {...register("password", {
+              required: { value: true, message: "Password is required." },
+              minLength: { value: 6, message: "Password must be at least 6 characters." }
+            })} />
+          <VpnKeyIcon fontSize="medium" className="absolute top-1/2 -translate-y-1/2 translate-x-1/3" />
+          <span className="absolute bottom-0 translate-y-full right-0 text-base text-red-500">{errors.password?.message}</span>
+        </div>
+        <div className="relative flex flex-col">
+          <input className={textBoxStyle} type="password" placeholder="Enter password again"
+            {...register("cfmPassword",
+              { required: { value: true, message: "Please enter password again." },
+              validate: (value) => {
+                if (value != password) {
+                  return "Password do not match."
+                }
+              }})} />
+          <VpnKeyIcon fontSize="medium" className="absolute top-1/2 -translate-y-1/2 translate-x-1/3" />
+          <span className="absolute bottom-0 translate-y-full right-0 text-base text-red-500">{errors.cfmPassword?.message}</span>
+        </div>
+        <div className="flex justify-center items-center">
+          <button disabled={isPending} className="bg-buttonColour px-12 py-2 rounded-lg opacity-80 hover:opacity-100 transition-all delay-600 disabled:cursor-not-allowed disabled:opacity-40">Sign Up</button>
+        </div>
       </form>
       <span>Already have an account? <Link className="text-buttonColour hover:underline" to="/login">Login</Link> now!</span>
     </div>
