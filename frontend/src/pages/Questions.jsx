02272384009b63a1b0b3f8584396bd2fe723b2a9
Chan How <73763719+ChillinRage@users.noreply.github.com>
2024-10-03 00:23:48 +0800

    Add refresh button to question table

--- frontend/src/pages/Questions.jsx
+++ frontend/src/pages/Questions.jsx
@@ -1,20 +1,16 @@
+import { useState } from "react";
+
 import GeneralNavbar from "../components/navbar/GeneralNavbar";
-import QuestionTable from '../components/questions/QuestionTable'
-import AddQuestionButton from '../components/questions/AddQuestionButtons'
+import QuestionTable from '../components/questions/QuestionTable';
+import AddQuestionButton from '../components/questions/AddQuestionButtons';
+import RefreshTableButton from '../components/questions/refreshTableButton';
+
 import '../styles/questions.css';
-import { useNavigate } from "react-router-dom";
-import useAuth from "../hooks/useAuth";
 import 'react-toastify/dist/ReactToastify.css';
 
 const Questions = () => {
-    
-  const navigate = useNavigate();
-  const { username, removeCookie } = useAuth();
-
-  const Logout = () => {
-    removeCookie("token");
-    navigate("/login");
-  };
+    const [refresh, setRefresh] = useState(true);
+    const toggle = () => setRefresh(!refresh);
 
     return (
         <div>
@@ -23,10 +19,13 @@ const Questions = () => {
                 <h1>Questions</h1>
                 <p className="description">View all the questions stored in database.</p>
                 <div className="question-table-container">
-                    <div className="admin-button">
-                        <AddQuestionButton/>
+                    <div className="table-buttons">
+                        <RefreshTableButton trigger={toggle}/>
+                        <div className="admin-button">
+                            <AddQuestionButton />
+                        </div>
                     </div>
-                    <QuestionTable />
+                    <QuestionTable mountTrigger={refresh} />
                 </div>
             </div>
         </div>
