cfc9c4913769a6f9b5cb4bfb9f358416ad9a56dc
Chan How <73763719+ChillinRage@users.noreply.github.com>
2024-10-02 23:10:41 +0800

    Add authentication to question service

--- backend/question-backend/routes/question-route.js
+++ backend/question-backend/routes/question-route.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { createQuestion, deleteQuestion, updateQuestion, getAllQuestions, getQuestionById } from "../controllers/question-controller.js";
-import { verifyAccessToken, verifyIsAdmin } from "../../user-backend/middleware/basic-access-control.js";   
+import { verifyAccessToken, verifyAdmin } from "../middleware/basic-access-control.js";
 
 const router = express.Router();
 
-router.post("/", verifyAccessToken, verifyIsAdmin, createQuestion);
-router.delete("/:id", verifyAccessToken, verifyIsAdmin, deleteQuestion);
-router.put("/:id", verifyAccessToken, verifyIsAdmin, updateQuestion);
+router.post("/", verifyAccessToken, verifyAdmin, createQuestion);
+router.delete("/:id", verifyAccessToken, verifyAdmin, deleteQuestion);
+router.put("/:id", verifyAccessToken, verifyAdmin, updateQuestion);
 router.get("/", verifyAccessToken, getAllQuestions);
 router.get("/:id", verifyAccessToken, getQuestionById);
 
