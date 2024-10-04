756d470d79d5754b76ef0d2fdfc0dd6a927782f7
Ryan Chia <ryan_chia8@hotmail.com>
2024-10-04 19:23:22 +0800

    Add functionality to delete JWT token

--- apps/frontend/src/app/services/login-store.ts
+++ apps/frontend/src/app/services/login-store.ts
@@ -7,3 +7,9 @@ export function setToken(token: string): void {
 export function getToken(): string {
     return KEY in window.localStorage ? window.localStorage[KEY] : ""
 }
+
+export function deleteToken() {
+    if (KEY in window.localStorage) {
+        window.localStorage.removeItem(KEY);
+    }
+}
