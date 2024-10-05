ac5da2ac890350665dfb228d30242c768df4f079
bensohh <lok200910@gmail.com>
2024-10-06 00:59:55 +0800

    Update the Header component new props

--- apps/frontend/src/app/login/page.tsx
+++ apps/frontend/src/app/login/page.tsx
@@ -1,32 +1,25 @@
 "use client";
 import Header from "@/components/Header/header";
-import {
-  Button,
-  Input,
-  Layout,
-  message,
-  Form,
-} from "antd";
+import { Button, Input, Layout, message, Form } from "antd";
 import { Content } from "antd/es/layout/layout";
 import "./styles.scss";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import TextArea from "antd/es/input/TextArea";
-import {loginUser} from '@/app/services/user'
-import {setToken} from '@/app/services/login-store'
+import { loginUser } from "@/app/services/user";
+import { setToken } from "@/app/services/login-store";
 import { useRouter } from "next/navigation";
 
 type InputFields = {
-  email: string
-  password: string
-}
+  email: string;
+  password: string;
+};
 
 export default function Home() {
-  
   const [isLoginFailed, setIsLoginFailed] = useState(false);
   const router = useRouter();
   const [messageApi, contextHolder] = message.useMessage();
-  
+
   const successMessage = (message: string) => {
     messageApi.open({
       type: "success",
@@ -34,60 +27,57 @@ export default function Home() {
     });
   };
 
-  function submitDetails({email, password}: InputFields): void {
-    loginUser(email, password).then(jwt => {
-      successMessage("Login successful")
-      setToken(jwt);
-      router.push("/");
-
-    }).catch(err => {
-      console.error(err);
-      setIsLoginFailed(true);
-    })
+  function submitDetails({ email, password }: InputFields): void {
+    loginUser(email, password)
+      .then((jwt) => {
+        successMessage("Login successful");
+        setToken(jwt);
+        router.push("/");
+      })
+      .catch((err) => {
+        console.error(err);
+        setIsLoginFailed(true);
+      });
   }
 
   return (
     <>
       {contextHolder}
       <Layout>
-        <Header/>
+        <Header selectedKey={undefined} />
         <Content>
           <div className="login-card">
             <h1>Login</h1>
-            <Form
-              name="basic"
-              onFinish={submitDetails}
-            >
+            <Form name="basic" onFinish={submitDetails}>
               <Form.Item<InputFields>
-                name="email"  
+                name="email"
                 rules={[
                   {
-                    required: true, 
-                    message: "Please provide your email."
+                    required: true,
+                    message: "Please provide your email.",
                   },
                 ]}
               >
-                <Input
-                  type="email"
-                  placeholder="Email"
-                />
+                <Input type="email" placeholder="Email" />
               </Form.Item>
 
               <Form.Item<InputFields>
-                name="password"  
-                rules={[{
-                  required: true, 
-                  message: "Please enter your password."
-                }]}
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please enter your password.",
+                  },
+                ]}
               >
-                <Input.Password
-                  placeholder="Password"
-                />
+                <Input.Password placeholder="Password" />
               </Form.Item>
 
-              <div 
-                style={{visibility: isLoginFailed ? "visible" : "hidden"}} 
-                className="registration-failed-text">This email/username is not valid
+              <div
+                style={{ visibility: isLoginFailed ? "visible" : "hidden" }}
+                className="registration-failed-text"
+              >
+                This email/username is not valid
               </div>
 
               <Form.Item>
@@ -97,9 +87,7 @@ export default function Home() {
               </Form.Item>
             </Form>
             <p>
-              Let me <Link
-                href="/register"
-              >register</Link>
+              Let me <Link href="/register">register</Link>
             </p>
           </div>
         </Content>
