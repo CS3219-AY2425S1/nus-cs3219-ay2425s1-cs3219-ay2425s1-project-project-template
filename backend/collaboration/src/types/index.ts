da2c96c9e590934b61c13c5104d7b0b5692dcdc1
anunayajoshi <anunayajoshi@gmail.com>
2024-10-07 15:29:07 +0800

    first collaboration wow

--- backend/collaboration/src/types/index.ts
+++ backend/collaboration/src/types/index.ts
@@ -1,1 +1,9 @@
 export * from './utility';
+export type Difficulty = 'Easy' | 'Medium' | 'Hard';
+
+export interface MatchRequest {
+    userId: string;
+    topic: string;
+    difficulty: Difficulty;
+    timestamp: number;
+  }
