042547419cb9ee03efcc4233511c04b983a0cffc
zheng-ze <zhengze@u.nus.edu>
2024-10-08 16:44:17 +0800

    Add basic skeleton of matching service

---
+++ matching_service/src/routes/matchingRoutes.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { Queue, IQueue } from "../services/queue";
+
+const router = express.Router();
+const queue: IQueue = new Queue();
+
+router.post("/match", async (req, res) => {
+  const { userId, topic, difficulty } = req.body;
+  // TODO: Add request into queue
+  // Possibly return a matching id
+  // Possibly set up socket connection so that we can update the user when a match is found
+  queue.add({ userId, topic, difficulty });
+  res.json({ success: true });
+});
+
+router.post("/cancelMatch", async (req, res) => {
+  const { userId, matchingId } = req.body;
+  // TODO: Remove request from queue
+  // Possiblt remove socket connection
+  // Possibly return a success message
+  queue.cancel({ userId, matchingId });
+});
+
+export default router;
