06c848d2344f4e42f7b997080fb5ab836ffc9d88
mounilsankar <mounil974@gmail.com>
2024-10-08 23:02:19 +0800

    Add logout route to user-service

--- backend/user-service/routes/auth-routes.js
+++ backend/user-service/routes/auth-routes.js
@@ -1,12 +1,16 @@
 import express from "express";
 
-import { handleLogin, handleVerifyToken } from "../controller/auth-controller.js";
+import { handleLogin,  handleLogout, handleRefreshToken, handleVerifyToken } from "../controller/auth-controller.js";
 import { verifyAccessToken } from "../middleware/basic-access-control.js";
 
 const router = express.Router();
 
 router.post("/login", handleLogin);
 
+router.post("/logout",  handleLogout);
+
 router.get("/verify-token", verifyAccessToken, handleVerifyToken);
+
+router.get("/refresh-token", handleRefreshToken, handleVerifyToken);
 
 export default router;
