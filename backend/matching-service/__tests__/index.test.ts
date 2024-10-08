e69831ccfedf37a3f49632da4db6e5550b038103
Low Han <lowhanlynn@Lows-MacBook-Air-2.local>
2024-10-08 18:38:26 +0800

    Resolve comments

---
+++ backend/matching-service/__tests__/index.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest'
+import app from '../src/index'
+
+describe('Index', () => {
+    describe('GET /', () => {
+        it('should return 200 OK', async () => {
+            const response = await request(app).get('/')
+            expect(response.status).toBe(200)
+        })
+    })
+})
--- backend/matching-service/src/index.ts
+++ backend/matching-service/src/index.ts
@@ -1,5 +1,5 @@
 import cors from 'cors'
-import express, { Express, NextFunction, Request, Response } from 'express'
+import express, { Express, Request, Response } from 'express'
 import 'express-async-errors'
 import helmet from 'helmet'
 import passport from 'passport'
@@ -14,26 +14,7 @@ app.use(express.json())
 app.use(cors()) // config cors so that front-end can use
 app.options('*', cors())
 app.use(helmet())
-
 app.use(passport.initialize())
-
-// To handle CORS Errors
-app.use((request: Request, response: Response, next: NextFunction) => {
-    response.header('Access-Control-Allow-Origin', '*') // "*" -> Allow all links to access
-
-    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-
-    // Browsers usually send this before PUT or POST Requests
-    if (request.method === 'OPTIONS') {
-        response.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH')
-        response.status(200).send()
-        return
-    }
-
-    // Continue Route Processing
-    next()
-})
-
 app.use('/matching', matchingRouter)
 
 // Health Check Route
