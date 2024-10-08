ffd5c9ebb9dc3719c5828961a5b2b17c36ed31cb
Donova Chan Jia Jun <donovanchanjiajun2001@gmail.com>
2024-10-08 23:33:13 +0800

    refactor: Moved controller and service instantiation logic to the index main function rather than routes

--- matchingService/src/index.ts
+++ matchingService/src/index.ts
@@ -2,22 +2,33 @@ import express, { Application, Request, Response } from "express";
 import Service from "./QueueService/Service";
 import MatchRequest from "./models/MatchRequest";
 import createRouter from "./routes";
+import MatchController from "./controllers/MatchController";
+import MatchService from "./services/MatchService";
 
 export interface QueueService {
     sendMessage(matchRequest: MatchRequest): Promise<boolean>;
     cancelMatchRequest(matchId: string): Promise<boolean>;
 }
 
-async function main() {
-    var amqpService: QueueService = await Service.of(process.env.RABBITMQ_URL || "amqp://localhost:5672", "gateway", "responseGateway");
+var amqpService: QueueService;
+var matchService: MatchService;
+var matchController: MatchController;
 
+async function main() {
     const app: Application = express();
+    await instantiateControllerServices();
     app.use(express.json());
-    app.use('/match', createRouter(amqpService));
+    app.use('/match', createRouter(matchController));
 
     app.listen(3000, () => {
         console.log("Server is running on port 3000");
     });
+}
+
+async function instantiateControllerServices() {
+    amqpService = await Service.of(process.env.RABBITMQ_URL || "amqp://localhost:5672", "gateway", "responseGateway");
+    matchService = new MatchService(amqpService);
+    matchController = new MatchController(matchService);
 }
 
 main();
--- matchingService/src/routes.ts
+++ matchingService/src/routes.ts
@@ -1,15 +1,11 @@
 import express, { Router } from "express";
 import MatchController from "./controllers/MatchController";
-import { QueueService } from ".";
-import MatchService from "./services/MatchService";
 
-export default function createRouter(amqpService: QueueService): Router {
+export default function createRouter(controller: MatchController): Router {
     const router: Router = express.Router();
-    const matchService = new MatchService(amqpService);
-    const matchController = new MatchController(matchService);
 
-    router.post("/findMatch", (req, res) => matchController.findMatch(req, res));
-    router.delete("/cancelMatch", (req, res) => matchController.cancelMatch(req, res));
+    router.post("/findMatch", (req, res) => controller.findMatch(req, res));
+    router.delete("/cancelMatch", (req, res) => controller.cancelMatch(req, res));
 
     return router;
 }
