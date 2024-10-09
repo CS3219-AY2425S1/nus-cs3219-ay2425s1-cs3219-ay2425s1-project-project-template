3b9aa51ec1420dc188a308f65fa7207298c3efc6
SeeuSim <seeusimong@gmail.com>
2024-10-09 17:02:14 +0800

    PEER-220 Fix memory leak
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- backend/matching/src/workers/matcher.ts
+++ backend/matching/src/workers/matcher.ts
@@ -1,4 +1,3 @@
-import async from 'async';
 import { client } from '@/lib/db';
 import { POOL_INDEX, STREAM_GROUP, STREAM_NAME, STREAM_WORKER } from '@/lib/db/constants';
 import { io } from '@/server';
@@ -16,69 +15,55 @@ process.on('SIGTERM', () => {
 });
 
 async function match() {
-  try {
-    const redisClient = client.isReady || client.isOpen ? client : await client.connect();
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-      logger.info('Iterating Worker');
-      const stream = await redisClient.xReadGroup(
-        STREAM_GROUP,
-        STREAM_WORKER,
-        {
-          key: STREAM_NAME,
-          id: '>',
-        },
-        {
-          COUNT: 1,
-          BLOCK: 2000,
-        }
+  const redisClient = client.isReady || client.isOpen ? client : await client.connect();
+  const stream = await redisClient.xReadGroup(
+    STREAM_GROUP,
+    STREAM_WORKER,
+    {
+      key: STREAM_NAME,
+      id: '>',
+    },
+    {
+      COUNT: 1,
+      BLOCK: 2000,
+    }
+  );
+  if (!stream || stream.length === 0) {
+    return;
+  }
+  for (const group of stream) {
+    // Perform matching
+    for (const _message of group.messages) {
+      // Query the pool
+      const matches = await redisClient.ft.search(
+        POOL_INDEX,
+        `@topic{} @difficulty{} -@id(<userId>)`,
+        { LIMIT: { from: 0, size: 1 }, SORTBY: { BY: 'timestamp', DIRECTION: 'ASC' } }
       );
-      if (stream && stream.length > 0) {
-        for (const group of stream) {
-          // Perform matching
-          for (const _message of group.messages) {
-            // Query the pool
-            const matches = await redisClient.ft.search(
-              POOL_INDEX,
-              `@topic{} @difficulty{} -@id(<userId>)`,
-              { LIMIT: { from: 0, size: 1 }, SORTBY: { BY: 'timestamp', DIRECTION: 'ASC' } }
-            );
 
-            // IF Found:
-            if (matches.total > 0) {
-              await Promise.all([
-                // Remove other from pool
-                redisClient.del(['key1', 'key2']),
-                // Remove other from queue
-                redisClient.xAck(STREAM_NAME, STREAM_GROUP, [`id1`, `id2`]),
-              ]);
-              // Notify both sockets
-              io.sockets.in(['userId1', 'userId2']).emit(' ROOMNUMBER | QUESTION??? ');
-            }
-          }
-        }
-      } else {
-        await new Promise((resolve, reject) =>
-          setTimeout(() => {
-            resolve({});
-          }, 1000)
-        );
+      // IF Found:
+      if (matches.total > 0) {
+        await Promise.all([
+          // Remove other from pool
+          redisClient.del(['key1', 'key2']),
+          // Remove other from queue
+          redisClient.xAck(STREAM_NAME, STREAM_GROUP, [`id1`, `id2`]),
+        ]);
+        // Notify both sockets
+        io.sockets.in(['userId1', 'userId2']).emit(' ROOMNUMBER | QUESTION??? ');
       }
     }
-  } catch (error) {
-    const { message, cause, name } = error as unknown as Error;
-    logger.error('::match: ' + JSON.stringify({ message, cause, name }));
-    process.exit(1);
   }
 }
 
-async.forever(
-  (next) => {
-    void match();
-    next();
-  },
-  (err) => {
-    logger.error(err);
-    process.exit(1);
-  }
-);
+(function loop() {
+  Promise.resolve()
+    .then(async () => await match())
+    .catch((error) => {
+      if (error !== null) {
+        const { message, name, cause } = error as Error;
+        logger.error({ message, name, cause });
+      }
+    })
+    .then(() => process.nextTick(loop));
+})();
