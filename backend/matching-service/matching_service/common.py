9410b61c18445b03801afbf6cb5fa19041721184
arkarsg <alxarkar@gmail.com>
2024-10-09 20:51:53 +0800

    init matching-service

---
+++ backend/matching-service/matching_service/common.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Difficulty(Enum):
+    Easy = "Easy"
+    Medium = "Medium"
+    Hard = "Hard"
---
+++ backend/matching-service/matching_service/config.py
@@ -0,0 +1,34 @@
+"""
+Loading env vars from .env files for FastAPI
+https://fastapi.tiangolo.com/advanced/settings/
+
+If env vars are not supplied by a .env file,
+default values defined in the class are used
+
+Defining env vars with `export` in terminal overrides
+env file values
+"""
+
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+this_file = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(this_file, os.pardir))
+ENV_PATH = Path(PROJECT_ROOT) / ".env"
+
+
+class Settings(BaseSettings):
+    REDIS_URL: str = "redis://localhost:6379"
+    ORIGINS: list[str] = ["http://localhost", "http://localhost:3000"]
+
+    class Config:
+        env_file = ENV_PATH
+        env_ignore_empty = (True,)
+        extra = "ignore"
+
+
+settings = Settings()
