6d742c0e2cf40ed4b9b7ffdf1874b21cce4aa470
Jun Jie <62635032+TJun-Jie@users.noreply.github.com>
2024-10-09 21:03:39 +0800

    add get random question route

--- matching_service/src/app.ts
+++ matching_service/src/app.ts
@@ -4,6 +4,7 @@ import { Server, Socket } from "socket.io";
 import cors from "cors";
 import jwt from "jsonwebtoken";
 import { joinRoomWhenReady } from "./services/socket";
+import { DifficultyLevel } from "./services/room";
 
 const app = express();
 const server = http.createServer(app);
@@ -46,9 +47,15 @@ io.on("connection", (socket) => {
   console.log("A user connected. Socket ID:", socket.id);
   console.log(socket.handshake.auth.token);
 
-  socket.on("match", ({ selectedDifficulty, selectedTopic }) => {
+  socket.on("match", async ({ selectedDifficulty, selectedTopic }) => {
     console.log(selectedDifficulty);
     console.log(selectedTopic);
+
+    const testquestion = await generateQuestion(socket.handshake.auth.token, {
+      topic: "Hash Table",
+      difficultyLevel: "Easy",
+    });
+    console.log(testquestion);
     // todo queue processing logic which will create a room upon successful match
     // const socket2 = null;
     // joinRoomWhenReady(socket, null, selectedDifficulty, selectedTopic);
@@ -73,3 +80,46 @@ io.on("connection", (socket) => {
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+interface QuestionParams {
+  topic: string | string[];
+  difficultyLevel: "Easy" | "Medium" | "Hard";
+}
+
+async function generateQuestion(token: string, params?: QuestionParams) {
+  try {
+    let url = new URL(
+      "http://gateway-service:5003/api/questions/questions/random"
+    );
+
+    if (params) {
+      if (params.topic) {
+        if (Array.isArray(params.topic)) {
+          params.topic.forEach((t) => url.searchParams.append("topic", t));
+        } else {
+          url.searchParams.append("topic", params.topic);
+        }
+      }
+      if (params.difficultyLevel) {
+        url.searchParams.append("difficultyLevel", params.difficultyLevel);
+      }
+    }
+
+    const response = await fetch(url.toString(), {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch question. Status: ${response.status}`);
+    }
+
+    const responseData = await response.json();
+    return responseData;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+}
