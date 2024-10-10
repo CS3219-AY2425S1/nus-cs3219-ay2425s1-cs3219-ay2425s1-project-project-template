219c6bfa213d5e837ffdd84c718d42fc255c1b0c
SeeuSim <seeusimong@gmail.com>
2024-10-10 09:53:10 +0800

    PEER-220: Add comments for logic
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- backend/matching/src/services/queue.ts
+++ backend/matching/src/services/queue.ts
@@ -2,11 +2,10 @@ import { MATCH_PREFIX, STREAM_NAME } from '@/lib/db/constants';
 import { getRedisPayload } from '@/lib/utils';
 import type { IQueueRequest, IRedisClient } from '@/types';
 
-let queueId = 1;
-
 export const queueingService = async (client: IRedisClient, payload: IQueueRequest) => {
   const formattedPayload = getRedisPayload(payload);
+  // Add to queue
   await client.xAdd(STREAM_NAME, formattedPayload.timestamp, formattedPayload);
+  // Add to matching pool
   await client.hSet(`${MATCH_PREFIX}${payload.userId}`, formattedPayload);
-  queueId += 1;
 };
