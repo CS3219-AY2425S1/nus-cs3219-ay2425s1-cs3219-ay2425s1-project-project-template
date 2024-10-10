814758ab73a9a69d011ac41f43f1a3ceb76d2d35
TabrizPlv <tabrizpahlavi@gmail.com>
2024-10-10 19:03:09 +0800

    add docker config

--- backend/collaboration/src/config.ts
+++ backend/collaboration/src/config.ts
@@ -3,5 +3,3 @@ import 'dotenv/config';
 export const UI_HOST = process.env.PEERPREP_UI_HOST!;
 
 export const EXPRESS_PORT = process.env.EXPRESS_PORT;
-
-export const WEBSOCKET_PORT = process.env.WEBSOCKET_PORT;
--- backend/collaboration/src/index.ts
+++ backend/collaboration/src/index.ts
@@ -4,11 +4,9 @@ import { dbHealthCheck } from '@/server';
 import server from '@/websocket';
 
 const port = Number.parseInt(EXPRESS_PORT || '8001');
-const wsPort = Number.parseInt(WEBSOCKET_PORT || '8002');
 
 const listenMessage = `App listening on port: ${port}`;
 server.listen(port, () => {
   void dbHealthCheck();
   logger.info(listenMessage);
 });
-server.listen(wsPort, () => logger.info(`WebSocket listening on port: ${wsPort}`));
