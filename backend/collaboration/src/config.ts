7e3817269c5df3cd02ca35eae85ef69cb5285c67
TabrizPlv <tabrizpahlavi@gmail.com>
2024-10-10 13:59:26 +0800

    setup y-websocket server with y-postgresql persistence layer

--- backend/collaboration/src/config.ts
+++ backend/collaboration/src/config.ts
@@ -3,3 +3,5 @@ import 'dotenv/config';
 export const UI_HOST = process.env.PEERPREP_UI_HOST!;
 
 export const EXPRESS_PORT = process.env.EXPRESS_PORT;
+
+export const WEBSOCKET_PORT = process.env.WEBSOCKET_PORT;
