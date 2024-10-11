5e2449416a3298af0dd3ae874b007cfb4f9cac1a
shishirbychapur <shishirbychapur@gmail.com>
2024-10-11 23:00:34 +0800

    refactor: change difficulty to complexity

--- frontend/components/dashboard/progress-card.tsx
+++ frontend/components/dashboard/progress-card.tsx
@@ -3,17 +3,17 @@
 import { Progress } from '@/components/ui/progress'
 
 interface ProgressCardProps {
-    difficulty: string
+    complexity: string
     score: string
     progress: number
     indicatorColor: string
     backgroundColor: string
 }
 
-export const ProgressCard = ({ difficulty, score, progress, indicatorColor, backgroundColor }: ProgressCardProps) => {
+export const ProgressCard = ({ complexity, score, progress, indicatorColor, backgroundColor }: ProgressCardProps) => {
     return (
         <div className="border-solid border-2 border-gray-200 rounded flex flex-col w-2/6 mx-2 p-6">
-            <p className="text-medium font-medium">{difficulty}</p>
+            <p className="text-medium font-medium">{complexity}</p>
             <h4 className="text-4xl font-extrabold mb-4">{score}</h4>
             <Progress value={progress} indicatorColor={indicatorColor} backgroundColor={backgroundColor} />
         </div>
