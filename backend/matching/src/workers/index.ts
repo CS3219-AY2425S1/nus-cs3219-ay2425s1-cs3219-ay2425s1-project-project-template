216076a67fec305e6006f6690c07a8b15babbf24
SeeuSim <seeusimong@gmail.com>
2024-10-11 10:57:41 +0800

    PEER-222: Add IPC for ws message sending
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- backend/matching/src/workers/index.ts
+++ backend/matching/src/workers/index.ts
@@ -1,13 +1,24 @@
 import { fork } from 'child_process';
 import path from 'path';
-import { logger } from '@/lib/utils';
 
-export const initWorker = (name: string) => {
+import type { Server } from 'socket.io';
+
+import { logger } from '@/lib/utils';
+import { MATCH_SVC_EVENT, type IChildProcessMessage } from '@/ws';
+
+export const initWorker = (name: string, io: Server) => {
   const lCaseName = name.toLowerCase();
   const worker = fork(path.join(__dirname, `${lCaseName}.js`));
   const upperCaseName = name.replace(/^[A-Za-z]/, (c) => c.toUpperCase());
   worker.on('message', (message) => {
-    logger.info(`[${upperCaseName}]: ${message}`);
+    logger.info(`Received message from '${upperCaseName}': ${JSON.stringify(message)}`);
+
+    const { rooms, event, message: payload } = message as IChildProcessMessage;
+    if (event === MATCH_SVC_EVENT.DISCONNECT) {
+      io.sockets.in(rooms).disconnectSockets();
+      return;
+    }
+    io.sockets.in(rooms).emit(event, payload);
   });
   worker.on('exit', (code) => {
     logger.error(`${upperCaseName} exited with code ${code}.`);
