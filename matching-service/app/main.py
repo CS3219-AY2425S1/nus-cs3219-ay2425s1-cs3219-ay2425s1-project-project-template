1d942617887be3d87ff27f0243323b894b8ba073
TanPingZhi <josiahtpz@gmail.com>
2024-10-13 00:11:35 +0800

    Change default port to 6969

--- matching-service/app/main.py
+++ matching-service/app/main.py
@@ -28,6 +28,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000)),
+        port=int(os.environ.get("PORT", 6969)),
         reload=(os.environ.get("BUILD_ENV", "dev") == "dev")
     )
