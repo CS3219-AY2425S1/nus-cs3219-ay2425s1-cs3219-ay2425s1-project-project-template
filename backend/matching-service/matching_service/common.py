ab31f08e49d19724da5b39255dc01b36b2007312
arkarsg <alxarkar@gmail.com>
2024-10-12 23:08:04 +0800

    create redis config and request model

--- backend/matching-service/matching_service/common.py
+++ backend/matching-service/matching_service/common.py
@@ -1,7 +1,41 @@
+import time
 from enum import Enum
+
+from pydantic import BaseModel, computed_field
+
+from .config import settings
 
 
 class Difficulty(Enum):
     Easy = "Easy"
     Medium = "Medium"
     Hard = "Hard"
+
+
+class MatchRequest(BaseModel):
+    """
+    Represents a MatchRequest from client
+
+    Required fields:
+    - user (ObjectId or email)
+    - difficulty
+    - topic
+
+    Automatically creates
+    - created_at timestamp (in seconds)
+    - expire_at timestamp (in seconds)
+    """
+
+    user: str = ...
+    difficulty: Difficulty
+    topic: str = ...
+
+    @computed_field
+    @property
+    def created_at(self) -> float:
+        return time.time()
+
+    @computed_field
+    @property
+    def expire_at(self) -> float:
+        return self.created_at + settings.MATCH_TIMEOUT
--- backend/matching-service/matching_service/config.py
+++ backend/matching-service/matching_service/config.py
@@ -10,10 +10,13 @@ env file values
 """
 
 import os
+from enum import Enum
 from pathlib import Path
 
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
+
+from .common import Difficulty
 
 load_dotenv()
 this_file = os.path.abspath(os.path.dirname(__file__))
@@ -22,8 +25,12 @@ ENV_PATH = Path(PROJECT_ROOT) / ".env"
 
 
 class Settings(BaseSettings):
+    # Env vars
     REDIS_URL: str = "redis://localhost:6379"
     ORIGINS: list[str] = ["http://localhost", "http://localhost:3000"]
+
+    # Match expiry
+    MATCH_TIMEOUT: int = 30
 
     class Config:
         env_file = ENV_PATH
@@ -32,3 +39,19 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+
+class RedisSettings:
+    class Channels(Enum):
+        EASY: str = "easy_chan"
+        MED: str = "med_chan"
+        HARD: str = "hard_chan"
+        MATCHES: str = "matches"
+
+    db_map = {Difficulty.Easy: 0, Difficulty.Medium: 1, Difficulty.Hard: 2, Channels.MATCHES: 3}
+
+    @staticmethod
+    def redis_url(db: Difficulty | Channels) -> str:
+        if db not in RedisSettings.db_map:
+            raise ValueError("No such Redis DB")
+        return f"{settings.REDIS_URL}/{RedisSettings.db_map[db]}"
