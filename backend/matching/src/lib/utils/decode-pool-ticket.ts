6016b6d481b49cb8886c76e758510405a9353fdd
SeeuSim <seeusimong@gmail.com>
2024-10-14 00:00:33 +0800

    (feature): Add correct multi-topic matching
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- backend/matching/src/lib/utils/decode-pool-ticket.ts
+++ backend/matching/src/lib/utils/decode-pool-ticket.ts
@@ -9,7 +9,11 @@ export const decodePoolTicket = (ticket: IStreamMessage) => {
     userId,
     socketPort,
     timestamp,
-    topic,
+    topic: topic
+      ? typeof topic === 'string'
+        ? topic.split(',').join('|') // For OR match as opposed to AND in sequence
+        : topic.join('|')
+      : undefined,
     difficulty,
   };
 };
