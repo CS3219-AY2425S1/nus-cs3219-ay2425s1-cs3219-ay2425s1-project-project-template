6e44aaa99122fa1a683c511bf2fd16f680fca68e
seanpzk <seanpzk2@gmail.com>
2024-10-13 20:12:36 +0800

    Add backend service to connect to google cloud storage

---
+++ profile-picture-service/src/main/java/peerprep/profile_picture_service/config/ApiConfig.java
@@ -0,0 +1,5 @@
+package peerprep.profile_picture_service.config;
+
+public class ApiConfig {
+    public static final String FRONTEND_URL = "http://localhost:5173";
+}
---
+++ profile-picture-service/src/test/java/peerprep/profile_picture_service/ProfilePictureServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package peerprep.profile_picture_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilePictureServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
