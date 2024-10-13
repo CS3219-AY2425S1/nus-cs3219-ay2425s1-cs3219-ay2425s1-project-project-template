305ae22df030343a7e4b3755224e39e785d986c3
Darren Sim <e0725166@u.nus.edu>
2024-10-13 18:12:21 +0800

    Change timeout logic to come from js instead of redis

--- backend/matching-service/controller/redis.js
+++ backend/matching-service/controller/redis.js
@@ -1,39 +1,39 @@
 import Redis from 'ioredis';
-import { io } from '../server';
+import { io } from '../server.js';
 
 let socketChannel;
 
 export const startRedis = async () => {
   socketChannel = new Redis();
-  socketChannel.subscribe('__keyevent@0__:expired');
-  socketChannel.on('message', async (channel, message) => {
-    const socketId = message;
-    onSocketExpiry(socketId);
-  });
 };
 
-export const enqueueSocket = async (socketId) => {
-  const complexity = socket.handshake.query.complexity;
+export const enqueueSocket = async (socketId, complexity) => {
   // Check if there is another socket in the queue with the same complexity
   const complexityContainsSocket = await socketChannel.get(complexity);
   if (complexityContainsSocket) {
     // Found a match
-    const socketId2 = socketChannel.get(complexity);
+    const socketId2 = await socketChannel.get(complexity);
     socketChannel.del(complexity);
 
     // TODO: Replace with collaboration service room id
     const roomId = Math.random().toString(36).substring(7);
-    io.to(socketId).emit('match', roomId);
-    io.to(socketId2).emit('match', roomId);
+    io.to(socketId).emit('matchFound', roomId);
+    io.to(socketId2).emit('matchFound', roomId);
+    console.log(`Matched ${socketId} with ${socketId2}`);
+    io.sockets.sockets.get(socketId).disconnect();
+    io.sockets.sockets.get(socketId2).disconnect();
   } else {
     // No match found
+    console.log('Waiting for match');
+    io.to(socketId).emit('message', 'Waiting for a match');
     await storeSocket(socketId, complexity);
+    setTimeout(() => onSocketExpiry(socketId, complexity), 20000);
   }
 };
 
 const storeSocket = async (socketId, complexity) => {
   try {
-    await socketChannel.set(complexity, socketId);
+    await socketChannel.set(complexity, socketId, 'EX', 60);
   } catch (error) {
     console.log(error.message);
     io.to(socketId).emit('error', 'Failed to store socket');
@@ -41,12 +41,18 @@ const storeSocket = async (socketId, complexity) => {
   }
 };
 
-const onSocketExpiry = async (socketId) => {
+const onSocketExpiry = async (socketId, complexity) => {
   try {
-    // TODO: socket send message to user saying that match is not found
-    io.to(socketId).emit('message', 'Failed to find match');
-    // TODO: disconnect socket
-    io.to(socketId).disconnect();
+    // Check if socket is still in the queue
+    const retrievedSocketId = socketChannel.get(complexity);
+    if (retrievedSocketId === socketId) {
+      console.log(`Socket ${socketId} expired`);
+      // Remove socket from queue
+      socketChannel.del(complexity);
+      console.log(`Socket ${socketId} expired`);
+      io.to(socketId).emit('message', 'Failed to find match');
+      io.to(socketId).disconnect();
+    }
   } catch (error) {
     console.log(error.message);
   }
--- backend/matching-service/server.js
+++ backend/matching-service/server.js
@@ -6,15 +6,19 @@ const port = process.env.PORT || 3003;
 
 export const io = new Server(port);
 startRedis();
+console.log('Matching Service listening on port ' + port);
 
 io.on('connection', async (socket) => {
   try {
-    // TODO: Verify socket by checking with jwt
-    if (notVerifiedUser) {
-      console.log('User not verified');
-      socket.disconnect();
-    }
-    enqueueSocket(socket.id);
+    console.log('New socket connected: ' + socket.id);
+    const jwt = socket.handshake.auth.token;
+    // Do checks here to verify the user
+    // if (notVerifiedUser) {
+    //   console.log('User not verified');
+    //   socket.disconnect();
+    // }
+    const complexity = socket.handshake.query.complexity;
+    enqueueSocket(socket.id, complexity);
   } catch (err) {
     console.log(err.message);
     socket.disconnect();
