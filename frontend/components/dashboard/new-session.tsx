a4046dab0afc8c69703662546556497e6d0d8462
Glemen Neo <glemenneo@gmail.com>
2024-10-14 14:40:12 +0800

    [#73] feat: add matchmaking modal

--- frontend/components/dashboard/new-session.tsx
+++ frontend/components/dashboard/new-session.tsx
@@ -1,13 +1,15 @@
 'use client'
 
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
-
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { Category, Complexity, Proficiency } from '@repo/user-types'
 import { useSession } from 'next-auth/react'
-import { addUserToMatchmaking } from '@/services/matching-service-api'
+import Image from 'next/image'
+import React from 'react'
 import { toast } from 'sonner'
+import { addUserToMatchmaking } from '../../services/matching-service-api'
+import CustomModal from '../customs/custom-modal'
+import Loading from '../customs/loading'
 
 export const NewSession = () => {
     const TopicOptions = Object.values(Category).map((category) => ({
@@ -25,24 +27,92 @@ export const NewSession = () => {
     const [selectedTopic, setSelectedTopic] = React.useState('')
     const [selectedComplexity, setSelectedComplexity] = React.useState('')
 
+    const [modalData, setModalData] = React.useState({
+        isOpen: false,
+        isMatchmaking: false,
+        isMatchFound: false,
+    })
+    const [timeElapsed, setTimeElapsed] = React.useState({
+        minutes: 0,
+        seconds: 0,
+    })
+    React.useEffect(() => {
+        if (modalData.isMatchmaking) {
+            const timer = setTimeout(() => {
+                if (timeElapsed.seconds === 59) {
+                    setTimeElapsed({
+                        minutes: timeElapsed.minutes + 1,
+                        seconds: 0,
+                    })
+                } else {
+                    setTimeElapsed({
+                        minutes: timeElapsed.minutes,
+                        seconds: timeElapsed.seconds + 1,
+                    })
+                }
+            }, 1000)
+
+            return () => clearTimeout(timer)
+        }
+    }, [modalData.isMatchmaking, timeElapsed.minutes, timeElapsed.seconds])
+
     const handleMatchmaking = async () => {
         if (!selectedTopic || !selectedComplexity) {
             toast.error('Please select a topic and complexity level to start matchmaking.')
             return
         }
+        setTimeElapsed({
+            minutes: 0,
+            seconds: 0,
+        })
+        setModalData({
+            isOpen: true,
+            isMatchFound: false,
+            isMatchmaking: true,
+        })
+
+        //TODO: Modify this response to match the response from the API
+        let response: { wsId: string } | undefined
 
         try {
-            await addUserToMatchmaking({
+            response = await addUserToMatchmaking({
                 userId: session?.user.id ?? '',
                 proficiency: session?.user.proficiency as Proficiency,
                 complexity: selectedComplexity as Complexity,
                 topic: selectedTopic as Category,
             })
-            // TODO: Add Modal logic here
-            toast.success('Successfully added to the matchmaking queue.')
-        } catch (error) {
+        } catch {
             toast.error('Failed to add to matchmaking queue. Please try again later.')
+            setModalData({
+                isOpen: false,
+                isMatchmaking: false,
+                isMatchFound: false,
+            })
+            return
         }
+
+        toast.success('Successfully added to the matchmaking queue.')
+        //TODO: Add WS logic here to listen for match found events
+        await new Promise((resolve, _) => setTimeout(resolve, 10000))
+        await handleMatchFound()
+    }
+
+    const handleCancelMatchmaking = async () => {
+        setModalData({
+            isOpen: false,
+            isMatchmaking: false,
+            isMatchFound: false,
+        })
+        //TODO: Add logic to call API to blacklist user/ remove user from matchmaking queue here
+    }
+
+    const handleMatchFound = async () => {
+        setModalData({
+            isOpen: true,
+            isMatchFound: true,
+            isMatchmaking: false,
+        })
+        //TODO: Add routing to collaborative coding page here
     }
 
     return (
@@ -83,10 +153,40 @@ export const NewSession = () => {
                     </SelectContent>
                 </Select>
             </div>
-
             <Button className="mt-4 bg-purple-600 hover:bg-[#A78BFA]" onClick={handleMatchmaking}>
                 Start matchmaking
             </Button>
+
+            {modalData.isOpen && (
+                <CustomModal className={`h-3/5 w-2/5 rounded-lg`}>
+                    <div className="flex flex-col items-center justify-center">
+                        <Image
+                            priority={true}
+                            src="/matchmaking.png"
+                            width={343}
+                            height={234}
+                            alt="pros sitting around a monitor"
+                        />
+                        <h2 className="text-xl font-bold mt-6 mb-4">
+                            {modalData.isMatchmaking ? 'Finding collaborator' : 'Match found!'}
+                        </h2>
+                        {modalData.isMatchmaking && <Loading />}
+                        {modalData.isMatchmaking && (
+                            <h2 className="text-medium font-medium mt-2">
+                                {timeElapsed.minutes.toString() + ':' + timeElapsed.seconds.toString().padStart(2, '0')}
+                            </h2>
+                        )}
+                        <Button
+                            className="mt-4 bg-purple-600 hover:bg-[#A78BFA]"
+                            variant={'primary'}
+                            size={'lg'}
+                            onClick={handleCancelMatchmaking}
+                        >
+                            Cancel matchmaking
+                        </Button>
+                    </div>
+                </CustomModal>
+            )}
         </div>
     )
 }
