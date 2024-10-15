ba2b5bd79e48084beb3de033c9afe3d63095c65d
Ujjwal Gaurav <ujjwalgaurav.me@gmail.com>
2024-10-15 14:18:39 +0800

    Improve Interview Questions Card

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -33,26 +33,19 @@ const peerSessions = [
   },
 ];
 
-const suggestedQuestion = {
-  title: "Binary Search Tree Insertion",
-  category: "Data Structures",
-};
 
 const firstname = "Jared";
-const lastname = "Wong";
-const fullname = firstname + " " + lastname;
-const username = "wongjared";
 
 export default function Dashboard() {
   return (
     <PeerPrep>
       <main className="flex-1 overflow-auto rounded-3xl">
         <div className="flex space-x-5">
-          <Welcome username={firstname} suggestedQuestion={suggestedQuestion} />
+          <Welcome username={firstname} />
           <ProgressOverview />
         </div>
         <div className="mt-5 flex space-x-5">
-          <Questions />
+          <Questions isAdmin={false}/>
           <History sessions={peerSessions} />
           <Calendar />
         </div>
