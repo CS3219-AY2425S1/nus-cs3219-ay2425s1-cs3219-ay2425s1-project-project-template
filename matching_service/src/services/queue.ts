297b46ddff933a86183a7cedec363f5e0df95665
Lucas Teo <jawsandeur@gmail.com>
2024-10-15 12:41:34 +0800

    Update dockerfile for kafka

--- matching_service/src/services/queue.ts
+++ matching_service/src/services/queue.ts
@@ -45,7 +45,8 @@ export class Queue implements IQueue {
     const kafka = new Kafka({
       clientId: "match-queue",
       brokers: [
-        `${process.env.KAFKA_BROKER_ROUTE}:${process.env.KAFKA_BROKER_PORT}`,
+        // `${process.env.KAFKA_BROKER_ROUTE}:${process.env.KAFKA_BROKER_PORT}`,
+        "broker:19092",
       ],
     });
     // Setup producer and consumer
