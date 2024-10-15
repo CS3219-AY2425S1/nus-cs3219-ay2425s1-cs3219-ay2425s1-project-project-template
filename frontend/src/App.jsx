9d0f72eff4881f956a075605045daf5f93b2ad96
erohsikivar <ravikishore1809@gmail.com>
2024-10-15 21:29:54 +0800

    Finished integration of Frontend to Backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import MatchingServicePage from "./pages/MatchingService";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Dashboard from "./pages/Dashboard";
+// import MatchingServicePage from "./pages/MatchingService";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import ErrorPage from "./pages/ErrorPage"; // Assuming ErrorPage exists
+// import ErrorPage from "./pages/ErrorPage"; // Assuming ErrorPage exists
 
 export default function App() {
   return (
-    <Router>
+    <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/matching-service" element={<MatchingServicePage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </Router>
+      <Home />
+    </>
   );
 }
