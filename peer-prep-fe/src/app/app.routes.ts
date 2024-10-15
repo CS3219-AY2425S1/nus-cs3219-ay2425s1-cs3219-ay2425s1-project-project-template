b54df25bc2cc05edc8e293d50782ec95e7a0cae7
AidenLYT <ytstirlingite@hotmail.com>
2024-10-16 04:03:51 +0800

    add queuing popup window

--- peer-prep-fe/src/app/app.routes.ts
+++ peer-prep-fe/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { AppComponent } from "./app.component"
 import { CreateAccountComponent } from "./create-account/create-account.component"
 import { HomeComponent } from "./home/home.component"
 import { LoginComponent } from "./login/login.component"
+import {MatchModalComponent} from "../loading-screen/match-modal/match-modal.component";
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
@@ -16,5 +17,6 @@ export const routes: Routes = [
   { path: "admin-controls", component: AdminComponent },
   { path: "add-question", component: AddPageComponent },
   { path: "edit-question", component: EditPageComponent },
-  { path: "question-list", component: QuestionListComponent }
+  { path: "question-list", component: QuestionListComponent },
+  { path: "loading-screen", component: MatchModalComponent }
 ]
