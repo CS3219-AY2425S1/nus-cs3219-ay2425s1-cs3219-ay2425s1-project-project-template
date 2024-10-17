a963d2a6a41bbe41b26d345124ce232376c58cc4
Kervyn <73232587+kervyntan@users.noreply.github.com>
2024-10-17 22:57:19 +0800

    Updated consumer (use of pullDataFromExchange)

--- message-queue/src/index.ts
+++ message-queue/src/index.ts
@@ -1,8 +1,9 @@
-import express , { Express, Request, Response } from "express"
-import amqp, { Connection, Channel, ConsumeMessage } from "amqplib"
+import amqp, { Channel, Connection, ConsumeMessage } from "amqplib"
 import dotenv from "dotenv"
-import { RoutingKey, UserData } from "./types"
+import express, { Express, Request, Response } from "express"
+
 import { DIFFICULTY_QUEUE } from "./constants"
+import { UserData } from "./types"
 
 const app: Express = express()
 
@@ -30,23 +31,46 @@ const connectRabbitMQ = async () => {
 
 connectRabbitMQ()
 
+const handleIncomingNotification = (msg: string) => {
+  try {
+    const parsedMessage = JSON.parse(msg)
+
+    console.log(`Received Notification`, parsedMessage)
+  } catch (error) {
+    console.error(`Error While Parsing the message`)
+  }
+}
+
+// Decide again if needs to be asynchronous
 const addDataToExchange = async (userData: UserData, key: string) => {
   await channel.publish(EXCHANGE, key, Buffer.from(JSON.stringify(userData)))
 }
 
 const pullDataFromExchange = async (queueName: string) => {
-  await channel.assertQueue(DIFFICULTY_QUEUE, {
-    durable: true,
-  });
+  await channel.assertQueue(queueName, {
+    durable: true
+  })
 
-  await channel.consume(queueName, )
+  await channel.consume(
+    queueName,
+    (msg) => {
+      if (!msg) {
+        return console.error("Invalid incoming message")
+      }
+      handleIncomingNotification(msg?.content?.toString())
+      channel.ack(msg)
+    },
+    {
+      noAck: false
+    }
+  )
 }
 
 // Publish message to exchange
-app.post("/", (req: Request, res: Response) => {
+app.post("/match", async (req: Request, res: Response) => {
   try {
-    const { userData, key } = req.body //key-value pair of userData will be <difficulty>.<topic(s)> 
-    addDataToExchange(userData, key)
+    const { userData, key } = req.body //key-value pair of userData will be <difficulty>.<topic(s)>
+    await addDataToExchange(userData, key)
     console.log("Data Sent: ", req.body)
     res.json({ message: "Data Sent" })
   } catch (e) {
@@ -56,16 +80,19 @@ app.post("/", (req: Request, res: Response) => {
   }
 })
 
-app.get("/", (req: Request, res: Response, next) => {
+app.get("/match", async (req: Request, res: Response, next) => {
   console.log("Hello World")
-  pullDataFromExchange(req.body.queueName)
-  res.json({
-    message: "This is message queue."
-  })
+  // console.log(req);
+  if (req.query.hasOwnProperty("queueName")) {
+    await pullDataFromExchange(req.query.queueName as string)
+    res.json({
+      message: "This is message queue."
+    })
+  }
 })
 
 app.use((req: Request, res: Response, next) => {
-  const error : Error = new Error("Route Not Found")
+  const error: Error = new Error("Route Not Found")
   next(error)
 })
 
@@ -81,3 +108,4 @@ app.use((error, req: Request, res: Response, next) => {
 app.listen(3002, () => {
   console.log("Publisher running.")
 })
+ 
