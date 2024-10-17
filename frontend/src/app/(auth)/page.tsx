6a3abc25d4c0222a582b01250a3ef9ec8d74b039
Izz Hafeez <izzhafeez@gmail.com>
2024-10-17 14:05:42 +0800

    Remove LCDashboard from homepage

--- frontend/src/app/(auth)/page.tsx
+++ frontend/src/app/(auth)/page.tsx
@@ -1,16 +1,9 @@
 "use client";
 
-import { AuthStatus, useAuth } from "@/components/auth/AuthContext";
 import LandingPage from "./components/landing-page/LandingPage";
-// import Dashboard from "./components/dashboard/Dashboard";
-import LeetcodeDashboard from "./leetcode-dashboard/LeetcodeDashboard";
 
 const Home = () => {
-  const { authStatus } = useAuth();
-
-  return (
-    authStatus === AuthStatus.ADMIN ? <LeetcodeDashboard/> : <LandingPage/>
-  );
+  return <LandingPage/>;
 };
 
 export default Home;
