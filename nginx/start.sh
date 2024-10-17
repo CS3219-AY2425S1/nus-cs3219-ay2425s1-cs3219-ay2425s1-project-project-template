734cdb063d8dbd72bdbd1bdf8268813ee96105f8
Zoe Ang <za.zoeang@gmail.com>
2024-10-17 21:46:00 +0800

    Add nginx route for matching notification service

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-envsubst '\$USER_SERVICE_ADDR \$QUESTION_SERVICE_ADDR' < /tmp/default.conf > /etc/nginx/conf.d/default.conf
+envsubst '\$USER_SERVICE_ADDR \$QUESTION_SERVICE_ADDR' '\$MATCH_NOTIFICATION_SERVICE_ADDR'< /tmp/default.conf > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
