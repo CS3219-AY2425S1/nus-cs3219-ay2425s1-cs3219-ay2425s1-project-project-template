42a0411b3293bd0af44ccd6c877c29ac2282c32a
pughal <pughalraj77@gmail.com>
2024-10-18 00:03:31 +0800

    set condition for UserMatchingModdal

--- frontend/src/pages/DashBoardForUsersPage.tsx
+++ frontend/src/pages/DashBoardForUsersPage.tsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar.tsx";
 import Dashboard from "../components/Dashboard/Dashboard.tsx";
 import useQuestionList from "../hooks/useQuestionList.tsx";
 
 const DashboardForUsersPage: React.FC = () => {
   const {questions, fetchData} = useQuestionList();
+  // const [isUserMatchingModalOpen, setIsUserMatchingModalOpen] = useState(false);
   useEffect(() => {
     fetchData();
   }, []); // IMPT Do not put fetchData in the dependency array so as to avoid infinite renders
@@ -12,6 +13,7 @@ const DashboardForUsersPage: React.FC = () => {
   return (
     <div className="w-screen h-screen flex flex-col">
       <NavBar />
+      {/* { isUserMatchingModalOpen && <UserMatchingModal onClose={() => setIsUserMatchingModalOpen(false)} /> } */}
       <Dashboard questions={questions} fetchData={fetchData} />
     </div>
   );
