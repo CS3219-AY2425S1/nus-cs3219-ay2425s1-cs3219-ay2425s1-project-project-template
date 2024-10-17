4f343872b58dc292f384642cd0faa2eae902be82
Rayson <122922486+raysonchia@users.noreply.github.com>
2024-10-18 05:26:52 +0800

    Update topics according to selected difficulty

--- frontend/src/pages/NewSessionPage.jsx
+++ frontend/src/pages/NewSessionPage.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-prototype-builtins */
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom"; 
 import { useAuth } from "../AuthContext";
@@ -10,6 +9,8 @@ const NewSessionPage = () => {
   const { accessToken } = useAuth();
   const navigate = useNavigate();
   const [topicsArray, setTopicsArray] = useState([]);
+  const [targetTopicsArray, setTargetTopicsArray] = useState([]);
+  const [selectedTopic, setSelectedTopic] = useState('');
 
   const getHeaders = () => {
     return {
@@ -39,27 +40,57 @@ const NewSessionPage = () => {
   }, []);
 
   const getTopics = (questions) => {
-    const topicCount = [];
+    const topicCount = {
+      Easy: {},
+      Medium: {},
+      Hard: {}
+    };
 
     questions.forEach(qn => {
       const topicArr = qn.category;
+      const difficulty = qn.complexity;
 
       topicArr.forEach(tp => {
-        if (topicCount[tp]) {
-            topicCount[tp]++;
+        if (topicCount[difficulty][tp]) {
+            topicCount[difficulty][tp]++;
         } else {
-            topicCount[tp] = 1;
+            topicCount[difficulty][tp] = 1;
         }
       });
     });
-  
-    const topics = Object.keys(topicCount).map(topic => ({
-        name: topic,
-        count: topicCount[topic]
+
+    const easyTopics = Object.keys(topicCount['Easy']).map(topic => ({
+      name: topic,
+      count: topicCount['Easy'][topic]
     }));
 
-    topics.sort((a, b) => a.name.localeCompare(b.name));
-    setTopicsArray(topics);
+    const medTopics = Object.keys(topicCount['Medium']).map(topic => ({
+      name: topic,
+      count: topicCount['Medium'][topic]
+    }));
+
+    const hardTopics = Object.keys(topicCount['Hard']).map(topic => ({
+        name: topic,
+        count: topicCount['Hard'][topic]
+    }));
+
+    easyTopics.sort((a, b) => a.name.localeCompare(b.name));
+    medTopics.sort((a, b) => a.name.localeCompare(b.name));
+    hardTopics.sort((a, b) => a.name.localeCompare(b.name));
+
+    setTopicsArray({
+        easy: easyTopics,
+        medium: medTopics,
+        hard: hardTopics
+    });
+  };
+
+  const handleDifficultyChange = (e) => {
+    setTargetTopicsArray(topicsArray[e.target.value]);
+  };
+
+  const handleTopicChange = (e) => {
+    setSelectedTopic(e.target.value);
   };
 
   const handleSubmit = (e) => {
@@ -85,28 +116,29 @@ const NewSessionPage = () => {
                 <div className="difficulty-selection">
                     <p>Select a Difficulty Level:</p>
                     <div className="options">
-                      <input type="radio" id="easy" name="difficulty" value="easy"/>
+                      <input type="radio" id="easy" name="difficulty" value="easy" onChange={handleDifficultyChange} />
                       <label className="radio-label" htmlFor="easy">Easy</label>
-                      <input type="radio" id="medium" name="difficulty" value="medium" />
+                      <input type="radio" id="medium" name="difficulty" value="medium" onChange={handleDifficultyChange} />
                       <label className="radio-label" htmlFor="medium">Medium</label>
-                      <input type="radio" id="hard" name="difficulty" value="hard" />
+                      <input type="radio" id="hard" name="difficulty" value="hard" onChange={handleDifficultyChange} />
                       <label className="radio-label" htmlFor="hard">Hard</label>
                     </div>
                 </div>
-                <div className="topic-selection">
+                <div className={`topic-selection ${targetTopicsArray.length === 0 ? '' : 'section-shown'}`}>
                     <p>Select a Topic:</p>
                     <div className="options">
                       {
-                        topicsArray.length === 0 ? (
+                        targetTopicsArray.length === 0 ? (
                           <img src={loading} alt="Loading..." />
                         ) : (
-                          topicsArray.map((topic) => (
+                          targetTopicsArray.map((topic) => (
                             <React.Fragment key={topic.name}>
                               <input
                                 type="radio"
                                 id={topic.name}
                                 name="topic"
                                 value={topic.name}
+                                onChange={handleTopicChange}
                               />
                               <label className="radio-label" htmlFor={topic.name}>
                                 {`${topic.name} (${topic.count})`}
@@ -117,7 +149,7 @@ const NewSessionPage = () => {
                       }
                     </div>
                 </div>
-                <button className="start-btn">Start Matching</button>
+                <button className={`start-btn ${selectedTopic === '' ? '' : 'show-btn'}`}>Start Matching</button>
             </form>
         </div>
     </div>
