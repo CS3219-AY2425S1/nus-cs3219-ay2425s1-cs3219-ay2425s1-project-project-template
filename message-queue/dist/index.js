e82c06fd808dede8c47d1785012da8877300688c
smolegz <zhuohui.koo@gmail.com>
2024-10-18 20:31:31 +0800

    Edit dockerfile

--- message-queue/dist/index.js
+++ message-queue/dist/index.js
@@ -12,10 +12,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
 const amqplib_1 = __importDefault(require("amqplib"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const constants_1 = require("./constants");
+const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 dotenv_1.default.config();
 app.use(express_1.default.json());
@@ -36,21 +35,45 @@ const connectRabbitMQ = () => __awaiter(void 0, void 0, void 0, function* () {
     }
 });
 connectRabbitMQ();
-const addDataToExchange = (userData, key) => __awaiter(void 0, void 0, void 0, function* () {
-    yield channel.publish(EXCHANGE, key, Buffer.from(JSON.stringify(userData)));
-});
+const handleIncomingNotification = (msg) => {
+    try {
+        const parsedMessage = JSON.parse(msg);
+        console.log(`Received Notification`, parsedMessage);
+        return parsedMessage;
+    }
+    catch (error) {
+        console.error(`Error while parsing the message`);
+    }
+};
+// Decide again if needs to be asynchronous
+const addDataToExchange = (userData, key) => {
+    channel.publish(EXCHANGE, key, Buffer.from(JSON.stringify(userData)));
+};
 const pullDataFromExchange = (queueName) => __awaiter(void 0, void 0, void 0, function* () {
-    yield channel.assertQueue(constants_1.DIFFICULTY_QUEUE, {
-        durable: true,
+    let message;
+    yield channel.assertQueue(queueName, {
+        durable: true
     });
-    yield channel.consume(queueName);
+    channel.prefetch(1);
+    yield channel.consume(queueName, (msg) => {
+        var _a;
+        if (!msg) {
+            return console.error("Invalid incoming message");
+        }
+        message = msg;
+        handleIncomingNotification((_a = msg === null || msg === void 0 ? void 0 : msg.content) === null || _a === void 0 ? void 0 : _a.toString());
+    });
+    return {
+        channel,
+        message
+    };
 });
 // Publish message to exchange
-app.post("/", (req, res) => {
+app.post("/match", (req, res) => {
     try {
-        const { userData, key } = req.body; //key-value pair of userData will be <difficulty>.<topic(s)> 
+        const { userData, key } = req.body; //key-value pair of userData will be <difficulty>.<topic(s)>
         addDataToExchange(userData, key);
-        console.log("Data Sent: ", req.body);
+        console.log("abcd Data Sent: ", req.body);
         res.json({ message: "Data Sent" });
     }
     catch (e) {
@@ -59,13 +82,19 @@ app.post("/", (req, res) => {
         res.json({ message: "Data failed to send" });
     }
 });
-app.get("/", (req, res, next) => {
-    console.log("Hello World");
-    pullDataFromExchange(req.body.queueName);
-    res.json({
-        message: "This is message queue."
-    });
-});
+app.get("/match", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    if (req.query.hasOwnProperty("queueName")) {
+        const { channel, message } = yield pullDataFromExchange(req.query.queueName);
+        if (message) {
+            // Do some logic, then ACK
+            console.log("Now then we acknowledge, so we force 1 message to be received at each time");
+            channel.ack(message);
+        }
+        res.json({
+            message: JSON.parse(message.content.toString())
+        });
+    }
+}));
 app.use((req, res, next) => {
     const error = new Error("Route Not Found");
     next(error);
@@ -81,4 +110,3 @@ app.use((error, req, res, next) => {
 app.listen(3002, () => {
     console.log("Publisher running.");
 });
-
