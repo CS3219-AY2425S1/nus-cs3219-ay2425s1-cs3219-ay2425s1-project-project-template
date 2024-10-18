01bda1803143de8f27dd2a05471b74ebc1473c51
jannnice <e0969059@u.nus.edu>
2024-10-18 11:58:21 +0800

    fixing rabbitmq connection

--- backend/matching-backend/index.js
+++ backend/matching-backend/index.js
@@ -14,21 +14,17 @@ app.use(express.json());
 
 let channel, connection;
 
-async function initRabbitMQ() {
-    for (let i = 0; i < 5; i++) { // Retry 5 times
-        try {
-            connection = await amqp.connect('amqp://rabbitmq:5672');
-            channel = await connection.createChannel();
-            await channel.assertQueue('matching-queue', { durable: true });
-            console.log('Connected to RabbitMQ');
-            startConsumer(channel); // Start consumer
-            return; // Exit the function if successful
-        } catch (error) {
-            console.error('Error connecting to RabbitMQ, retrying...', error);
-            await new Promise(resolve => setTimeout(resolve, 2000)); // Wait 2 seconds before retrying
         }
+async function initRabbitMQ() {
+    try {
+        connection = await amqp.connect('amqp://rabbitmq:5672');
+        channel = await connection.createChannel();
+        await channel.assertQueue('matching-queue', { durable: true });
+        console.log('Connected to RabbitMQ');
+        startConsumer(channel); // Start consumer
+    } catch (error) {
+        console.error('Error connecting to RabbitMQ: ', error);
     }
-    console.error('Failed to connect to RabbitMQ after multiple attempts');
 }
 
 
