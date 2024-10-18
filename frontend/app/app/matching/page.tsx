e8cda63f3783d864b56bb5e7aa5682dff5bfc1d9
wr1159 <chuweiron18@gmail.com>
2024-10-19 02:21:07 +0800

    Update name of matching page

--- frontend/app/app/matching/page.tsx
+++ frontend/app/app/matching/page.tsx
@@ -2,7 +2,7 @@ import AuthPageWrapper from "@/components/auth/auth-page-wrapper";
 import FindMatch from "@/components/matching/find-match";
 import { Suspense } from "react";
 
-export default function QuestionListingPage() {
+export default function MatchingPage() {
   return (
     <AuthPageWrapper requireLoggedIn>
       <Suspense>
