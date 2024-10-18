88979ee019828f1bebcafe8aec05055845f2afcd
Tahsin Hasem <tahsinhasem@gmail.com>
2024-10-19 07:56:13 +0800

    Add Match Data Structures

---
+++ frontend/types/Match.ts
@@ -0,0 +1,24 @@
+import { QuestionComplexity } from "./Question";
+import { QuestionTopic } from "./Question";
+
+export type MatchRequest = {
+  userID: "string";
+  topic: QuestionTopic;
+  difficulty: QuestionComplexity;
+  timestamp: number;
+};
+
+export type MatchResponse = {
+  message: string;
+  error?: string;
+};
+
+export type MatchStatus = "PENDING" | "MATCHED" | "CANCELLED" | "NONE";
+
+//TODO: request to standarise the responses to be sent by the backend
+
+export type CheckMatchResponse = {
+  status: MatchStatus;
+  matchedWithUserId: string;
+  topic: string; //TODO: Change to QuestionTopic and QuestionComplexity from backend
+};
--- frontend/types/Question.ts
+++ frontend/types/Question.ts
@@ -1,37 +1,36 @@
-
 export enum QuestionComplexity {
-    EASY = 'easy',
-    MEDIUM = 'medium',
-    HARD = 'hard',
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
 }
 
 export enum QuestionTopic {
-    ARRAY = 'array',
-    BINARY = "binary",
-    BINARY_SEARCH = "binary_search",
-    BINARY_SEARCH_TREE = "binary_search_tree",
-    BINARY_TREE = "binary_tree",
-    DYNAMIC_PROGRAMMING = "dynamic_programming",
-    GRAPH = "graph",
-    GREEDY = "greedy",
-    HASH_TABLE = "hash_table",
-    HEAP = "heap",
-    LINKED_LIST = "linked_list",
-    MATH = "math",
-    MATRIX = "matrix",
-    QUEUE = "queue",
-    RECURSION = "recursion",
-    SORTING = "sorting",
-    STACK = "stack",
-    STRING = "string",
-    TRIE = "trie"
+  ARRAY = "array",
+  BINARY = "binary",
+  BINARY_SEARCH = "binary_search",
+  BINARY_SEARCH_TREE = "binary_search_tree",
+  BINARY_TREE = "binary_tree",
+  DYNAMIC_PROGRAMMING = "dynamic_programming",
+  GRAPH = "graph",
+  GREEDY = "greedy",
+  HASH_TABLE = "hash_table",
+  HEAP = "heap",
+  LINKED_LIST = "linked_list",
+  MATH = "math",
+  MATRIX = "matrix",
+  QUEUE = "queue",
+  RECURSION = "recursion",
+  SORTING = "sorting",
+  STACK = "stack",
+  STRING = "string",
+  TRIE = "trie",
 }
 
 export type Question = {
-    _id?: string; // TODO: Remove optional 
-    title: string;
-    description: string;
-    topics: QuestionTopic[];
-    complexity: QuestionComplexity;
-    link: string;
-}
+  _id?: string; // TODO: Remove optional
+  title: string;
+  description: string;
+  topics: QuestionTopic[];
+  complexity: QuestionComplexity;
+  link: string;
+};
