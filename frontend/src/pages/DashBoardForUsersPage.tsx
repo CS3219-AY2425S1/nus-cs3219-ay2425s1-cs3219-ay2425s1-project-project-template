602afe878028fada2eb1e71fd4ecc8925b3df055
seanpzk <seanpzk2@gmail.com>
2024-10-18 21:51:47 +0800

    Clean up code

--- frontend/src/pages/DashBoardForUsersPage.tsx
+++ frontend/src/pages/DashBoardForUsersPage.tsx
@@ -2,18 +2,17 @@ import { useEffect } from "react";
 import NavBar from "../components/NavBar.tsx";
 import Dashboard from "../components/Dashboard/Dashboard.tsx";
 import useQuestionList from "../hooks/useQuestionList.tsx";
-import { useUser } from "../context/UserContext.tsx";
 
 const DashboardForUsersPage: React.FC = () => {
   const {questions, fetchData} = useQuestionList();
-  const { user } = useUser();
+
   useEffect(() => {
     fetchData();
   }, []); // IMPT Do not put fetchData in the dependency array so as to avoid infinite renders
 
   return (
     <div className="w-screen h-screen flex flex-col">
-      <NavBar currUser={user}/>
+      <NavBar />
       <Dashboard questions={questions} fetchData={fetchData} />
     </div>
   );
