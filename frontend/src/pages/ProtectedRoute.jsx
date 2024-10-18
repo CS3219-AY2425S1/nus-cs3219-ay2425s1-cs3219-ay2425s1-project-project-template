af3268a539ca098106da7eed8e0eb0881c64d91f
erohsikivar <ravikishore1809@gmail.com>
2024-10-18 22:16:56 +0800

    Fixed token error while login

--- frontend/src/pages/ProtectedRoute.jsx
+++ frontend/src/pages/ProtectedRoute.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("jwtToken");
 
   // If no token, redirect to the login page
   if (!token) {
+    console.log("No token found");
     return <Navigate to="/login" replace />;
   }
 
