8cbb00b8f30a8481000f9eff5cd840fec62392ff
Ivan Koo Zhuo Hui <zhuohui.koo@gmail.com>
2024-10-18 20:32:19 +0800

    Delete message-queue/dist directory

--- message-queue/dist/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DIFFICULTY_ROUTING_KEY = exports.DIFFICULTY_QUEUE = void 0;
-exports.DIFFICULTY_QUEUE = "DIFFICULTY_QUEUE";
-exports.DIFFICULTY_ROUTING_KEY = "difficulty";
