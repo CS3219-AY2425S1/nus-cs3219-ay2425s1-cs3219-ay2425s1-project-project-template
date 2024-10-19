9399abe4d1d1439dfb12fc4b2485981d4dcdaa30
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-10-19 18:59:41 +0800

    refactor: Update MatchingRequest model to use "difficultyLevel" instead of "difficulty"

--- backend/service-matching/src/main/java/g55/cs3219/backend/matchingservice/model/MatchingRequest.java
+++ backend/service-matching/src/main/java/g55/cs3219/backend/matchingservice/model/MatchingRequest.java
@@ -3,10 +3,12 @@ package g55.cs3219.backend.matchingservice.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class MatchingRequest {
+public class MatchingRequest implements Serializable {
     private String userId;
     private String topic;
-    private String difficulty;
+    private String difficultyLevel;
 }
