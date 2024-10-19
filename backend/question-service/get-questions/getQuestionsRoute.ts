c33443ca88e4a0a3b927b8b371f745675f027e22
choo tze jie <119966583+tjch-o@users.noreply.github.com>
2024-10-20 00:49:26 +0800

    Implement endpoint for getting random question

--- backend/question-service/get-questions/getQuestionsRoute.ts
+++ backend/question-service/get-questions/getQuestionsRoute.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
 import { getQuestionsByFilter } from './getQuestionsController'
+import { getRandomQuestion } from './getRandomQuestionController'
 
 const router = Router()
+
 router.get('/get-questions', getQuestionsByFilter)
+router.get('/get-random-question', getRandomQuestion)
 
 export { router }
---
+++ backend/question-service/get-questions/getRandomQuestionController.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from 'express'
+import Question from '../models/question'
+import logger from '../utils/logger'
+
+const getRandomQuestion = async (req: Request, res: Response) => {
+    try {
+        const { categories, difficulty } = req.query
+        let filter: any = {}
+
+        if (categories) {
+            const categoryArray =
+                typeof categories === 'string'
+                    ? categories.split(',')
+                    : categories
+            filter.categories = { $in: categoryArray }
+        }
+
+        if (difficulty) {
+            filter.difficulty = difficulty
+        }
+
+        const retrievedQuestions = await Question.find(filter)
+        const n = retrievedQuestions.length
+        const randomIndex = Math.floor(Math.random() * n)
+        const randomQuestion = retrievedQuestions[randomIndex]
+
+        if (!retrievedQuestions) {
+            logger.error('No questions found')
+            return res.status(400).json({ message: 'No questions found' })
+        }
+
+        let logMessage = `Question chosen`
+
+        if (difficulty) {
+            logMessage += ` with difficulty ${difficulty}`
+        }
+
+        if (categories) {
+            logMessage += ` and category ${categories}`
+        }
+
+        logMessage += `: ${randomQuestion.questionId} ${randomQuestion.title} `
+        logger.info(logMessage)
+        return res.status(200).json(randomQuestion)
+    } catch (e) {
+        logger.error('Error appeared when retrieving questions', e)
+        return res
+            .status(500)
+            .json({ message: 'Error appeared when retrieving questions' })
+    }
+}
+
+export { getRandomQuestion }
