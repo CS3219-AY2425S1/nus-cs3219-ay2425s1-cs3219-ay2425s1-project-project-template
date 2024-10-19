5fab623736dc1c7a360a8524ebbacf6755e32ac7
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-10-19 18:58:49 +0800

    refactor: Update RabbitMQConfig to include matchingQueue and message converter

--- backend/service-matching/src/main/java/g55/cs3219/backend/matchingservice/config/RabbitMQConfig.java
+++ backend/service-matching/src/main/java/g55/cs3219/backend/matchingservice/config/RabbitMQConfig.java
@@ -1,10 +1,15 @@
 package g55.cs3219.backend.matchingservice.config;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class RabbitMQConfig {
@@ -17,5 +22,24 @@ public class RabbitMQConfig {
     @Bean
     public TopicExchange matchingExchange() {
         return new TopicExchange("matchingExchange");
+    }
+
+    @Bean
+    public Queue matchingQueue() {
+        return new Queue("matchingQueue", false);
+    }
+
+    @Bean
+    public SimpleMessageConverter simpleMessageConverter() {
+        SimpleMessageConverter converter = new SimpleMessageConverter();
+        converter.setAllowedListPatterns(List.of("g55.cs3219.backend.matchingservice.model.*"));
+        return converter;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(simpleMessageConverter());
+        return template;
     }
 }
