cb792fb101ff30c95c430949f10cb4aa26164f54
Vedant Sinha <vedantsinha2003@gmail.com>
2024-10-19 13:43:41 +0800

    Add startConsume to LoginPage

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -14,6 +14,26 @@ export default function LoginPage() {
 		return <Navigate to={'/matching-page'} />;
 	}
 
+	const startConsume = async () => {
+        try {
+            // Assuming login is successful and user token is saved
+            const response = await fetch('http://localhost:3009/rabbitmq/consume', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.ok) {
+                console.log('Consuming started successfully');
+            } else {
+                console.error('Failed to start consuming queue');
+            }
+        } catch (error) {
+            console.error('Error while consuming messages:', error);
+        }
+    };
+
 	const onSuccess = async (credentialResponse: CredentialResponse) => {
 		try {
 		const token = credentialResponse.credential;
@@ -27,6 +47,7 @@ export default function LoginPage() {
 			setLoggedIn(true);
 			setToken(jwtToken);
 			localStorage.setItem("access_token", jwtToken);
+			startConsume();
 			navigate('/matching-page');
 		}
 	} catch (error) {
