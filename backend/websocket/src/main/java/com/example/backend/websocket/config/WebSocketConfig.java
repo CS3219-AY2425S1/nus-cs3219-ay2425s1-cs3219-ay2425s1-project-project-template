91fc17e742ee8636c4821e039f2ce220935e22e5
Mai Ting Kai <wheatgrass.mc@gmail.com>
2024-10-19 09:25:38 +0800

    Fix compile errors

--- backend/websocket/src/main/java/com/example/backend/websocket/config/WebSocketConfig.java
+++ backend/websocket/src/main/java/com/example/backend/websocket/config/WebSocketConfig.java
@@ -1,7 +1,5 @@
 package com.example.backend.websocket.config;
 
-import java.security.Principal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
