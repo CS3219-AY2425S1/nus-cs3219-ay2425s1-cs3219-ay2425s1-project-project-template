2c22c508d188054ea19058c0d6670ea41cfb0dfe
NivethaUdayakumar <nivethaus4@gmail.com>
2024-10-19 12:08:08 +0800

    Updated loading page with SSE logic

--- frontend/src/pages/loadingpage.tsx
+++ frontend/src/pages/loadingpage.tsx
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Loader } from 'semantic-ui-react';
 import { useNavigate } from 'react-router-dom';
+import { jwtDecode, JwtPayload } from 'jwt-decode';
 import 'semantic-ui-css/semantic.min.css';
+
+interface CustomJwtPayload extends JwtPayload {
+  email?: string; 
+  name?: string;  
+}
 
 const LoadingPage: React.FC = () => {
   const navigate = useNavigate();
@@ -9,33 +15,48 @@ const LoadingPage: React.FC = () => {
   const [matchFound, setMatchFound] = useState(false);
 
   useEffect(() => {
-
     const timer = setInterval(() => {
       setCountdown((prev) => {
         if (prev === 1) {
           clearInterval(timer);
-          // navigate('/profile');
           return 0;
         }
         return prev - 1;
       });
     }, 1000);
 
-    return () => clearInterval(timer);
-  }, [navigate]);
+    const jwtToken = localStorage.getItem("access_token");
+    let decodedToken: CustomJwtPayload | null = null;
+    if (jwtToken) {
+        decodedToken = jwtDecode<CustomJwtPayload>(jwtToken);
+    }
 
+    const eventSource = new EventSource(`http://localhost:3009/rabbitmq/${decodedToken.email}`);
+    eventSource.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (data.matchFound) {
+        setMatchFound(true);
+        clearInterval(timer);
+      }
+    };
+
+    return () => {
+      clearInterval(timer);
+      eventSource.close();
+    };
+  }, []);
 
   const resetTimer = () => {
-    setCountdown(5);
+    setCountdown(30);
+    setMatchFound(false);
   };
 
   const conditionalRender = () => {
-    if (countdown && !matchFound) {
+    if (countdown > 0 && !matchFound) {
       return (
         <Loader active inverted indeterminate size="massive" content={`Matching in ${countdown} seconds`} />
-      )
-    }
-    else if (!countdown && !matchFound) {
+      );
+    } else if (countdown === 0 && !matchFound) {
       return (
         <div className='text-center'>
           <h1 className="text-4xl font-bold mb-4">Unable to find a match</h1>
@@ -49,9 +70,8 @@ const LoadingPage: React.FC = () => {
             </button>
           </div>
         </div>
-      )
-    }
-    else {
+      );
+    } else {
       return (
         <div>
           <h1 className="text-4xl font-bold mb-6">Match Found</h1>
@@ -64,9 +84,9 @@ const LoadingPage: React.FC = () => {
             </button>
           </div>
         </div>
-      )
+      );
     }
-  }
+  };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
