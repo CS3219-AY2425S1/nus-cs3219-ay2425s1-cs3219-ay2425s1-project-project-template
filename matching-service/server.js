7eb1132ccb2df23205c37f252a666b91e15b07d5
Yingming23 <liyingming13@gmail.com>
2024-10-19 22:56:21 +0800

    Updated matching notification service

--- matching-service/server.js
+++ matching-service/server.js
@@ -1,9 +1,26 @@
-// matchingService.js
 const amqp = require('amqplib');
 const express = require('express');
+const redis = require('redis');
 
 const app = express();
 const PORT = process.env.MATCHING_PORT || 4000;
+
+const redisClient = redis.createClient({
+  socket: {
+    port: 6379,
+    reconnectStrategy: function (retries) {
+      if (retries > 20) {
+        console.log(
+          'Too many attempts to reconnect. Redis connection was terminated',
+        );
+        return new Error('Too many retries.');
+      } else {
+        return retries * 500;
+      }
+    },
+  },
+});
+
 
 app.use(express.json());
 
@@ -16,9 +33,9 @@ async function initRabbitMQ() {
   channel = await connection.createChannel();
 
   // Declare queues
-  await channel.assertQueue('search_queue')
-  await channel.assertQueue('match_found_queue'); 
-  await channel.assertQueue('disconnect_queue'); 
+  await channel.assertQueue('search_queue');
+  await channel.assertQueue('match_found_queue');
+  await channel.assertQueue('disconnect_queue');
 
   // Start listening for search requests
   channel.consume('search_queue', (msg) => {
@@ -41,46 +58,108 @@ async function initRabbitMQ() {
   });
 }
 
+async function initRedis() {
+  redisClient.on('error', (err) => console.log('Redis Client Error', err));
+  await redisClient.connect();
+}
+
 async function matchUsers(searchRequest) {
   const { userId, difficulty, topics } = searchRequest;
 
-  const matchedUser = findMatchByTopics(topics) || findMatchByDifficulty(difficulty);
+  const matchedByTopics = await findMatchByTopics(topics);
+  const matchedByDifficulty = await findMatchByDifficulty(difficulty);
+
+  const matchedByDifficultyIds = new Set(
+    matchedByDifficulty.map((item) => item.id),
+  );
+  const combinedMatches = matchedByTopics.filter((item) =>
+    matchedByDifficultyIds.has(item.id),
+  );
+
+  const combinedMatchesWValues = await Promise.all(
+    combinedMatches.map(async (key) => {
+      const value = await redisClient.get(key);
+      return { key, value };
+    }),
+  );
+
+  combinedMatchesWValues.sort((a, b) => {
+    if (a.value < b.value) return -1; // a comes before b
+    if (a.value > b.value) return 1; // a comes after b
+    return 0; // a and b are equal
+  });
+  const matchedUser = combinedMatchesWValues[0]?.key;
 
   if (matchedUser) {
     const matchMessage = {
       userId,
-      matchUserId: matchedUser.userId,
+      matchUserId: matchedUser,
     };
 
-    channel.sendToQueue('match_found_queue', Buffer.from(JSON.stringify(matchMessage)));
-    console.log(`Match found: User ID ${userId} matched with ${matchedUser.userId}`);
+    channel.sendToQueue(
+      'match_found_queue',
+      Buffer.from(JSON.stringify(matchMessage)),
+    );
+    console.log(
+      `Match found: User ID ${userId} matched with ${matchMessage.matchUserId}`,
+    );
+    redisClient.del(matchMessage.matchUserId);
     delete activeSearches[userId];
     delete activeSearches[matchedUser.userId];
+  } else {
+    console.log(`No match found for User ID: ${userId}`);
+    redisClient.set(userId, Date.now());
+    difficulty.forEach((tag) => {
+      redisClient.SADD(`difficulty:${tag}`, userId);
+    });
+    topics.forEach((tag) => {
+      redisClient.SADD(`topics:${tag}`, userId);
+    });
   }
 }
 
-function findMatchByTopics(topics) {
-  if (topics.includes("simulate cannot find match")) {
-    return null;
-  }
-  return { userId: 'mockUser123' };
+async function findMatchByTopics(topics) {
+  const multi = redisClient.multi();
+
+  topics.forEach((tag) => multi.sMembers(`topics:${tag}`));
+
+  const replies = await
+    multi.exec((err, replies) => {
+      if (err) return reject(err);
+      console.log('Replies from Redis:', replies); // Log replies
+      resolve(replies);
+    });
+
+  const keys = [...new Set(replies.flat())];
+  return keys;
 }
 
-function findMatchByDifficulty(difficulty) {
-  if (difficulty == "simulate cannot find match") {
-    return null;
-  }
-  return { userId: 'mockUser456' };
+async function findMatchByDifficulty(difficulty) {
+  const multi = redisClient.multi();
+  
+  difficulty.forEach((tag) => multi.sMembers(`difficulty:${tag}`));
+  
+  const replies = await
+    multi.exec((err, replies) => {
+      if (err) return reject(err);
+      console.log('Replies from Redis:', replies); // Log replies
+      resolve(replies);
+    });
+  
+  const keys = [...new Set(replies.flat())];
+  return keys;
 }
 
 function handleDisconnection(userId) {
   if (activeSearches[userId]) {
+    redisClient.del(userId);
     delete activeSearches[userId];
     console.log(`User ID: ${userId} has been removed from active searches.`);
   }
 }
 
 initRabbitMQ();
+initRedis();
 
 app.listen(PORT, () => {
   console.log(`Matching service is running and listening on port ${PORT}`);
