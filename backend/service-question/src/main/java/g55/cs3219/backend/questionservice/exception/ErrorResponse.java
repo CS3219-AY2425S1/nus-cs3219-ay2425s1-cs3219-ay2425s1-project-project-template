dce74e98791f444c570fb0cce7b8319176e36471
jnjy <jefferyng2000@gmail.com>
2024-10-20 21:23:06 +0800

    edit: Update error response class

--- backend/service-question/src/main/java/g55/cs3219/backend/questionservice/exception/ErrorResponse.java
+++ backend/service-question/src/main/java/g55/cs3219/backend/questionservice/exception/ErrorResponse.java
@@ -1,18 +1,24 @@
+// File: service-question/src/main/java/g55/cs3219/backend/questionservice/exception/ErrorResponse.java
 package g55.cs3219.backend.questionservice.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
 
+    @JsonProperty("status")
     private int status;
+
+    @JsonProperty("message")
     private String message;
 
     public ErrorResponse(int status, String message) {
         this.status = status;
         this.message = message;
     }
-
 }
