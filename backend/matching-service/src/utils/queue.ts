88a271bd4ccebd36e0221e92baa8d428d881200a
Ye Xinjian <e0544100@u.nus.edu>
2024-10-20 11:11:00 +0800

    Add utility queue

---
+++ backend/matching-service/src/utils/queue.ts
@@ -0,0 +1,61 @@
+import { Mutex } from 'async-mutex';
+
+export class Queue<T> {
+    private items: T[] = [];
+    private mutex: Mutex = new Mutex();
+
+    async clean(predicate: (item: T) => boolean): Promise<void> {
+        // higher priority
+        const release = await this.mutex.acquire(1);
+        try {
+            this.items = this.items.filter(obj => predicate(obj));
+        } finally {
+            release();
+        } 
+    }
+
+    async enqueue(element: T): Promise<void> {
+        const release = await this.mutex.acquire();
+        try {
+            this.items.push(element);
+        } finally {
+            release();
+        }   
+    }
+
+    async dequeue(): Promise<T | undefined> {
+        const release = await this.mutex.acquire();
+        try {
+            if (this.isEmpty()) {
+                return undefined;
+            }
+            return this.items.shift();  // Remove from the front
+        } finally {
+            release();
+        }
+    }
+
+    // retrieve in order
+    async retrieve(predicate: (item: T) => boolean): Promise<T | undefined> {
+        const release = await this.mutex.acquire();
+        try {
+            for (let i = 0; i < this.size(); i++) {
+                if (predicate(this.items[i])) {
+                    return this.items.splice(i, 1)[0];
+                }
+            }
+            return undefined;
+        } finally {
+            release();
+        }
+    }
+
+    // no need to expose
+    private isEmpty(): boolean {
+        return this.items.length === 0;
+    }
+
+    size(): number {
+        return this.items.length;
+    }
+}
