94a4d29456f0ff1915c85272cdf8df42fa20afc4
Nam Do Hyun <brandonnam2020@gmail.com>
2024-10-21 02:11:29 +0800

    Fix env variable uses

--- question-service/config/db.js
+++ question-service/config/db.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config();
 
 const connectDB = async() => {
     try {
-        const con = await mongoose.connect(process.env.MONGODB_URI)
+        const con = await mongoose.connect(process.env.DB_CLOUD_URI_QUESTION)
         console.log(`MongoDB connected: ${con.connection.host}`)
     } catch(error) {
         console.log(error)
--- user-service/model/repository.js
+++ user-service/model/repository.js
@@ -5,7 +5,7 @@ import { connect } from "mongoose";
 export async function connectToDB() {
   let mongoDBUri =
     process.env.ENV === "PROD"
-      ? process.env.DB_CLOUD_URI
+      ? process.env.DB_CLOUD_URI_USERS
       : process.env.DB_LOCAL_URI;
 
   await connect(mongoDBUri);
