1304bb753af584317933913aafb088bd57d5ec9d
Mai Ting Kai <wheatgrass.mc@gmail.com>
2024-10-20 18:44:12 +0800

    Fix hydration error

--- frontend/src/app/(auth)/page.tsx
+++ frontend/src/app/(auth)/page.tsx
@@ -4,15 +4,25 @@ import Navbar from "@/app/(auth)/home/components/navbar/Navbar";
 import LandingPage from "@/app/(auth)/home/components/landing-page/LandingPage";
 import { getToken } from "@/api/user";
 import AuthDashboard from "./components/dashboard/Dashboard";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [userToken, setUserToken] = useState<string | null>(null);
+  useEffect(() => {
     const token = getToken();
-    return (
-        !!token ? <AuthDashboard/> : <>
-            <Navbar />
-            <LandingPage />
-        </>
-    );
+    if (token) {
+      setUserToken(token);
+    }
+  }, []);
+
+  return !!userToken ? (
+    <AuthDashboard />
+  ) : (
+    <>
+      <Navbar />
+      <LandingPage />
+    </>
+  );
 };
 
 export default Home;
