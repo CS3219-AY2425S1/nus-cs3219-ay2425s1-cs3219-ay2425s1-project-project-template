c78295369c91c434b4d963d9834618487bc043c2
shishirbychapur <shishirbychapur@gmail.com>
2024-10-20 18:24:58 +0800

    fix: comment out routing

--- frontend/components/dashboard/new-session.tsx
+++ frontend/components/dashboard/new-session.tsx
@@ -178,7 +178,7 @@ export const NewSession = () => {
             isMatchFound: true,
             isMatchmaking: false,
         }))
-        router.push('/code')
+        // router.push('/code')
     }
 
     return (
