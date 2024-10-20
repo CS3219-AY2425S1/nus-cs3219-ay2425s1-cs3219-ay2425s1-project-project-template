2fe959e4e34ff69ea510cd3b87b18c743fa13d38
Jun Jie <62635032+TJun-Jie@users.noreply.github.com>
2024-10-21 00:49:00 +0800

    add script to update shared types version for all repos

---
+++ peerprep_fe/src/app/actions/room.tsx
@@ -0,0 +1,24 @@
+export async function getRoomById(id: string, token?: string | null) {
+  const response = await fetch(
+    `http://${process.env.GATEWAY_SERVICE_ROUTE}:${process.env.API_GATEWAY_PORT}/room/${id}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `bearer ${token}`,
+      },
+    }
+  );
+
+  try {
+    const data = await response.json();
+    return {
+      message: data,
+      errors: {
+        errorMessage: ["Unable to get room"],
+      },
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
