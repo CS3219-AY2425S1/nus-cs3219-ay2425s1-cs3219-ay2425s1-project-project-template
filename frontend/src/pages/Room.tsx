682d1f8d7038aa69e3c008807b3324cc2e755efb
jasonqiu212 <jason_qiu@hotmail.com>
2024-10-21 14:16:27 +0800

    Add skeleton for video

--- frontend/src/pages/Room.tsx
+++ frontend/src/pages/Room.tsx
@@ -7,7 +7,10 @@ function Room() {
   return (
     <Group h="100vh" bg="slate.8" gap="10px" p="10px">
       <Stack h="100%" w="500px" gap="10px">
-        <Skeleton h="150px" />
+        <Group gap="10px">
+          <Skeleton h="150px" w="calc(50% - 5px)" />
+          <Skeleton h="150px" w="calc(50% - 5px)" />
+        </Group>
         <RoomTabs />
       </Stack>
 
