179041d948020a5793d7df9c30edd598ad685a29
kenvynKwek <kenvynkwek@gmail.com>
2024-10-22 04:36:12 +0800

    Create backend api executeCode and route

--- backend/collab_service/routes/collabRoutes.js
+++ backend/collab_service/routes/collabRoutes.js
@@ -2,12 +2,16 @@
 const express = require('express');
 const router = express.Router();
 const CollabController = require('../controllers/collabController');
+const ExecuteCodeController = require('../controllers/executeCodeController');
 
 // Route for joining a session
 router.post('/join-session', CollabController.joinSession);
 
 // Route to terminate a session
 router.post('/terminate-session', CollabController.terminateSession);
+
+// Route for executing code
+router.post('/execute-code', ExecuteCodeController.executeCode);
 
 module.exports = router;
 
