9c517bac37cf919a71ca54dda38a70caaaea2ea0
jmsandiegoo <jmsandiego0710@gmail.com>
2024-10-23 20:39:18 +0800

    Add cursor indicators for collaboration ide

--- frontend/src/app/collaboration/_components/CollabCodePanel/CodeEditorTabContent.tsx
+++ frontend/src/app/collaboration/_components/CollabCodePanel/CodeEditorTabContent.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useCallback, useEffect, useState } from "react";
-import Editor from "@monaco-editor/react";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import draculaTheme from "monaco-themes/themes/Dracula.json";
+import Editor, { Monaco } from "@monaco-editor/react";
 import * as Y from "yjs";
 import { WebsocketProvider } from "y-websocket";
 import { MonacoBinding } from "y-monaco";
@@ -9,21 +10,20 @@ import { editor } from "monaco-editor";
 
 export default function CodeEditorTabContent() {
   const [editorRef, setEditorRef] = useState<editor.IStandaloneCodeEditor>();
+  const [provider, setProvider] = useState<WebsocketProvider>();
 
   useEffect(() => {
-    console.log("use effect triggered");
-    
     if (!editorRef) return;
-    
+
     const yDoc = new Y.Doc();
     const yText = yDoc.getText("monaco");
     const yProvider = new WebsocketProvider(
       `${location.protocol === "http:" ? "ws:" : "wss:"}//localhost:1234`,
-      "cs3219",
+      "cs3219", // session id
       yDoc
     );
+    setProvider(yProvider);
 
-    // Attaching Yjs to Monaco
     const binding = new MonacoBinding(
       yText,
       editorRef?.getModel() as editor.ITextModel,
@@ -37,19 +37,98 @@ export default function CodeEditorTabContent() {
     };
   }, [editorRef]);
 
-  const handleEditorOnMount = useCallback((e: editor.IStandaloneCodeEditor) => {
-    setEditorRef(e);
-  }, []);
+  const handleEditorOnMount = useCallback(
+    (e: editor.IStandaloneCodeEditor, monaco: Monaco) => {
+      // @ts-expect-error: we ignore since monaco-theme library is outdated with the latest
+      // monaco expected theme types but it still works
+      monaco.editor.defineTheme("dracula", draculaTheme);
+      monaco.editor.setTheme("dracula");
+      setEditorRef(e);
+    },
+    []
+  );
 
   return (
     <div className="h-full overflow-scroll">
+      {provider ? <InjectableCursorStyles yProvider={provider} /> : null}
       <Editor
+        defaultValue={"class Solution {\n" + "  \n" + "}"}
         onMount={handleEditorOnMount}
         height={"100%"}
         width={"100%"}
-        theme="vs-dark"
+        theme="dracula"
         language={"java"}
+        options={{
+          tabSize: 4,
+          padding: { top: 20 },
+        }}
       />
     </div>
   );
+}
+
+interface InjectableCursorStylesProps {
+  yProvider: WebsocketProvider;
+}
+
+interface AwarenessUser {
+  name: string;
+  color: string;
+}
+
+type AwarenessList = [number, AwarenessUser][];
+
+function InjectableCursorStyles({ yProvider }: InjectableCursorStylesProps) {
+  const [awarenessUsers, setAwarenessUsers] = useState<AwarenessList>([]);
+
+  // Set up current user's presence and also update whenever remote client submits its own presence
+  useEffect(() => {
+    const awareness = yProvider.awareness;
+
+    // TODO: make this dynamic with the context api and choose random hex colors
+    awareness.setLocalStateField("user", {
+      name: "Jm San Diego",
+      color: "#FF8C00",
+    });
+
+    const updateAwarenessUsers = () => {
+      const states = awareness.getStates(); // Get all user states
+      const users: AwarenessList = [];
+      states.forEach((val, key) => {
+        users.push([key, val.user]);
+      });
+
+      setAwarenessUsers(users);
+    };
+
+    // Attach change event listener
+    awareness.on("change", updateAwarenessUsers);
+    updateAwarenessUsers();
+
+    return () => {
+      awareness.off("change", updateAwarenessUsers);
+    };
+  }, [yProvider]);
+
+  const styleSheet = useMemo(() => {
+    let cursorStyles = "";
+
+    awarenessUsers.forEach((client, _idx) => {
+      console.log(client, client[0], client[1]);
+      cursorStyles += `
+          .yRemoteSelection-${client[0]},
+          .yRemoteSelectionHead-${client[0]}  {
+            --user-color: ${client[1].color};
+          }
+
+          .yRemoteSelectionHead-${client[0]}::after {
+            content: "${client[1].name}";
+          }
+        `;
+    });
+
+    return { __html: cursorStyles };
+  }, [awarenessUsers]);
+
+  return <style dangerouslySetInnerHTML={styleSheet} />;
 }
