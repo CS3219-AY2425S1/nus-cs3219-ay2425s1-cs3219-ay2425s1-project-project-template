ff4ea87760265eda7c441a4ca4d48b864659eada
Kidsnd274 <samuel_tee_gy@hotmail.com>
2024-10-23 18:49:49 +0800

    Connect firestore to user service

--- backend/user_service/app/main/history_routes.py
+++ backend/user_service/app/main/history_routes.py
@@ -1,19 +1,23 @@
-from flask import Blueprint, request, jsonify
 from app.auth_api.helper import verify_token
+from flask import Blueprint, request, jsonify
+from firebase_admin import firestore
 
 history = Blueprint('attempt_history', __name__)
 
-match_history = []  # Will be stored in firebase
+firebase_db = firestore.client()
 
 @history.route("/history", methods=["POST"])
 def record_attempt():
     try:
         match_data = request.json
+        print(match_data)
+        return jsonify({"message": "Success!"}), 200
 
     except Exception as e:
         return jsonify({"error": "Failed to record match", 
                         "details": str(e)}), 500
     
+
 @history.route("/<uid>/history", methods=["GET"])
 def get_user_history(uid):
     decoded_token, error, status_code = verify_token()
