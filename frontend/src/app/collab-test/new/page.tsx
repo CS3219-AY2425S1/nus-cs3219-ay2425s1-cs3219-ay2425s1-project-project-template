12f8c3c6a87a30a44cadaf206e3631ccfcbc6cde
Rick <rick.kohjiaxuan@gmail.com>
2024-10-23 11:19:50 +0800

    Add server.js for Socket.io and YJS integration

---
+++ frontend/src/app/collab-test/new/page.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import YTester2 from "../_components/YTester2";
+
+/** A hook to read and set a YText value. */
+
+export default function App() {
+  return (
+    <div>
+      <YTester2 userId="1" />
+    </div>
+  );
+}
