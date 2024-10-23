9e11fd09e5ca12e229fb7a1e02c89467e4203a79
jmsandiegoo <jmsandiego0710@gmail.com>
2024-10-23 14:49:00 +0800

    Basic collab editor function done

--- frontend/src/app/collaboration/_components/CollabCodePanel/CodeEditorTabContent.tsx
+++ frontend/src/app/collaboration/_components/CollabCodePanel/CodeEditorTabContent.tsx
@@ -1,14 +1,55 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Editor from "@monaco-editor/react";
+import * as Y from "yjs";
+import { WebsocketProvider } from "y-websocket";
+import { MonacoBinding } from "y-monaco";
+import { editor } from "monaco-editor";
 
 export default function CodeEditorTabContent() {
-  const [value, setValue] = useState("a {\n     new\n}");
+  const [editorRef, setEditorRef] = useState<editor.IStandaloneCodeEditor>();
+
+  useEffect(() => {
+    console.log("use effect triggered");
+    
+    if (!editorRef) return;
+    
+    const yDoc = new Y.Doc();
+    const yText = yDoc.getText("monaco");
+    const yProvider = new WebsocketProvider(
+      `${location.protocol === "http:" ? "ws:" : "wss:"}//localhost:1234`,
+      "cs3219",
+      yDoc
+    );
+
+    // Attaching Yjs to Monaco
+    const binding = new MonacoBinding(
+      yText,
+      editorRef?.getModel() as editor.ITextModel,
+      new Set([editorRef]),
+      yProvider.awareness
+    );
+
+    return () => {
+      yDoc.destroy();
+      binding.destroy();
+    };
+  }, [editorRef]);
+
+  const handleEditorOnMount = useCallback((e: editor.IStandaloneCodeEditor) => {
+    setEditorRef(e);
+  }, [editorRef]);
 
   return (
     <div className="h-full overflow-scroll">
-      <Editor value={value} height={"100%"} width={"100%"} theme="vs-dark" language={"java"}/>
+      <Editor
+        onMount={handleEditorOnMount}
+        height={"100%"}
+        width={"100%"}
+        theme="vs-dark"
+        language={"java"}
+      />
     </div>
   );
 }
