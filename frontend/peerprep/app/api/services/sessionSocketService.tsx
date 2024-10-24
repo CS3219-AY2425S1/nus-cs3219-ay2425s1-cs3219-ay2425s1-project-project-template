38433cffc9abd72b254a8025b0c906b20cd532ba
jayllo-c <jaylotc@gmail.com>
2024-10-24 23:51:19 +0800

    Update sessionSocketService.tsx

--- frontend/peerprep/app/api/services/sessionSocketService.tsx
+++ frontend/peerprep/app/api/services/sessionSocketService.tsx
@@ -1,3 +1,14 @@
 import io from 'socket.io-client';
+import { getAccessToken } from '../auth/actions';   
 
-export const socket = io('http://localhost:8010', { auth: { token: localStorage.getItem('jwtToken') } });
+const token = async () => {
+    const token = await getAccessToken();
+
+    if (!token) {
+        console.error('Access token not found');
+        return;
+    }
+    return token;
+};
+
+export const socket =  io('http://localhost:8010', { auth: { token } });
