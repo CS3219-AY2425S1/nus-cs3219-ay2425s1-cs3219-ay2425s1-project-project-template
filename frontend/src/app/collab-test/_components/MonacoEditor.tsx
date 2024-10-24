bc1795f5883abfa70d8f35717863181c028467f2
Rick <rick.kohjiaxuan@gmail.com>
2024-10-24 13:45:49 +0800

    Collab-test (to be removed)

--- frontend/src/app/collab-test/_components/MonacoEditor.tsx
+++ frontend/src/app/collab-test/_components/MonacoEditor.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import { useEffect, useState, useCallback } from "react";
@@ -20,17 +19,24 @@ export default function MonacoEditor() {
   const [editorRef, setEditorRef] = useState<editor.IStandaloneCodeEditor>();
   const [isConnected, setIsConnected] = useState(false);
 
-  useEffect(() => {
-    console.log("use effect triggered");
+  const [otherUser, setOtherUser] = useState<string>();
 
-    if (!editorRef) return;
+  useEffect(() => {
+    if (!editorRef) {
+      return;
+    }
 
     const yDoc = new Y.Doc();
     const yText = yDoc.getText("monaco");
     const yProvider = new WebsocketProvider(
       `${location.protocol === "http:" ? "ws:" : "wss:"}//localhost:1234`,
       roomId,
-      yDoc
+      yDoc,
+      {
+        params: {
+          name: "aaauser-" + userId,
+        },
+      }
     );
 
     // Attaching Yjs to Monaco
@@ -43,29 +49,68 @@ export default function MonacoEditor() {
 
     setIsConnected(yProvider.wsconnected);
 
+    // Listen for other users joining or leaving the session
+    yProvider.awareness.on("update", ({ added }: { added: number[] }) => {
+      // Can have an array or map for otherUser
+      // Add the new user to the list of users
+      // In our style sheet, we can map each user to a color
+      // Ideally should communicate with the backend to get the user's color
+      // User joined room, check added, assign color to user
+      const states = yProvider.awareness.getStates();
+
+      console.log(states);
+
+      if (added.length > 0) {
+        const newUser = String(added[0]);
+        if (newUser) {
+          setOtherUser(newUser);
+          console.log(`User joined: ${newUser}`);
+        }
+      }
+    });
+
     return () => {
       yDoc.destroy();
+      yProvider.disconnect();
       binding.destroy();
     };
   }, [editorRef]);
 
   const handleEditorOnMount = useCallback((e: editor.IStandaloneCodeEditor) => {
+    if (!window) {
+      return;
+    }
     setEditorRef(e);
   }, []);
 
   return (
-    <div className="flex flex-col w-full h-screen">
-      <h1>
-        Monaco Editor with Yjs {isConnected ? "connected" : "disconnected"}
-      </h1>
-      <p>Room: {roomId}</p>
-      <p>User: {userId}</p>
-      <Editor
-        height="90vh"
-        defaultLanguage="yaml"
-        defaultValue={`a: 2\nb: a + 30`}
-        onMount={handleEditorOnMount}
-      />
-    </div>
+    <>
+      {otherUser && (
+        <style>
+          {`.yRemoteSelectionHead-${otherUser} {
+                border-left: blue solid 2px;
+                border-top: blue solid 2px;
+                border-bottom: blue solid 2px;
+              }
+
+              .yRemoteSelectionHead-${otherUser}::after {
+                border: blue 3px solid;
+              }`}
+        </style>
+      )}
+      <div className="flex flex-col w-full h-screen">
+        <h1>
+          Monaco Editor with Yjs {isConnected ? "connected" : "disconnected"}
+        </h1>
+        <p>Room: {roomId}</p>
+        <p>User: {userId}</p>
+        <Editor
+          height="90vh"
+          defaultLanguage="yaml"
+          defaultValue={`a: 2\nb: a + 30`}
+          onMount={handleEditorOnMount}
+        />
+      </div>
+    </>
   );
 }
--- frontend/src/app/collab-test/new/page.tsx
+++ frontend/src/app/collab-test/new/page.tsx
@@ -1,11 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-// "use client";
-// import YTester2 from "../_components/YTester2";
-
 import { Suspense } from "react";
 import MonacoEditor from "../_components/MonacoEditor";
-
-/** A hook to read and set a YText value. */
 
 export default function App() {
   return (
