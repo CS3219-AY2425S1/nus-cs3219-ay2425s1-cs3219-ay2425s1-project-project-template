b67d78363e9b60132e6647cb7fb6359e6c99f1cf
Jamie Leow <e0772404@u.nus.edu>
2024-10-24 16:24:04 +0800

    Implemented CodeMirror for code editor

--- frontend/Peerprep/pages/collaboration/index.vue
+++ frontend/Peerprep/pages/collaboration/index.vue
@@ -1,10 +1,14 @@
 <script setup>
 import { useCollaborationStore } from '~/stores/collaborationStore';
+import CodeEditor from '~/components/CodeEditor.vue';
 const collaborationStore = useCollaborationStore()
 console.log(collaborationStore.getCollaborationInfo)
 </script>
 
 <template>
-    <div>Collab page</div>
-    <pre>{{ collaborationStore.getCollaborationInfo }}</pre>
+    <div>
+        <h2>Collab Page</h2>
+        <pre>{{ collaborationStore.getCollaborationInfo }}</pre>
+        <CodeEditor/>
+    </div>
 </template>
