6bbe01d8b93490e7738ce684ad058462519d318d
Rick <rick.kohjiaxuan@gmail.com>
2024-10-24 21:27:10 +0800

    Refactor async function to fetch and display question (temp)

--- frontend/src/app/collaboration/_components/QuestionPanel/DescriptionTabContent.tsx
+++ frontend/src/app/collaboration/_components/QuestionPanel/DescriptionTabContent.tsx
@@ -1,8 +1,22 @@
-export default function DescriptionTabContent() {
+import { getQuestions } from "@/services/questionService";
+import { QuestionsSchema } from "@/types/Question";
+
+export default async function DescriptionTabContent() {
+  const questionsResponse = await getQuestions();
+
+  if (questionsResponse.statusCode !== 200) {
+    return <div>You are not authenticated</div>;
+  }
+
+  // TODO: Based on the session, get the actual question
+  const questions = QuestionsSchema.parse(questionsResponse.data.questions);
+
+  const question = questions[0];
+
   return (
-    <>
-      <h2>Coin Change</h2>
-      <p>Test problem</p>
-    </>
+    <div className="p-4">
+      <h2 className="text-2xl font-bold mb-4">{question.title}</h2>
+      <p>{question.description}</p>
+    </div>
   );
 }
