87ecac478d496f5a122bc3e7b168d96ad42c14db
mounilsankar <mounil974@gmail.com>
2024-10-24 16:24:06 +0800

    update question service cors

--- backend/question-service/cmd/main.go
+++ backend/question-service/cmd/main.go
@@ -68,7 +68,7 @@ func main() {
 	// Enable CORS
 	headers := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
 	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
-	origins := handlers.AllowedOrigins([]string{"http://localhost:3000"}) // Allow all origins or specify your frontend's origin
+	origins := handlers.AllowedOrigins([]string{"http://localhost:3000", "http://localhost:8080"}) // Allow all origins or specify your frontend's origin
 	credentials := handlers.AllowCredentials()                            // Enable credentials
 
 	// Register the routes for the API
