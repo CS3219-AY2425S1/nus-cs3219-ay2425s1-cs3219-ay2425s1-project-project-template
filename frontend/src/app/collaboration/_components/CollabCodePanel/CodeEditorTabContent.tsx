295442889ed54768a54921c30957602c0429147b
Rick <rick.kohjiaxuan@gmail.com>
2024-10-24 21:28:57 +0800

    Get user and add todo

--- frontend/src/app/collaboration/_components/CollabCodePanel/CodeEditorTabContent.tsx
+++ frontend/src/app/collaboration/_components/CollabCodePanel/CodeEditorTabContent.tsx
@@ -1,134 +1,23 @@
-"use client";
+import { getCurrentUser } from "@/services/userService";
+import { UserProfileResponse, UserProfileSchema } from "@/types/User";
+import CollaborativeEditor from "./CollaborativeEditor/CollaborativeEditor";
 
-import { useCallback, useEffect, useMemo, useState } from "react";
-import draculaTheme from "monaco-themes/themes/Dracula.json";
-import Editor, { Monaco } from "@monaco-editor/react";
-import * as Y from "yjs";
-import { WebsocketProvider } from "y-websocket";
-import { MonacoBinding } from "y-monaco";
-import { editor } from "monaco-editor";
+export default async function CodeEditorTabContent() {
+  const userProfileResponse: UserProfileResponse = await getCurrentUser();
 
-export default function CodeEditorTabContent() {
-  const [editorRef, setEditorRef] = useState<editor.IStandaloneCodeEditor>();
-  const [provider, setProvider] = useState<WebsocketProvider>();
+  // TODO: Get actual session via getSessionInfo();
+  const getSession = "cs3219";
 
-  useEffect(() => {
-    if (!editorRef) return;
+  if (userProfileResponse.statusCode !== 200) {
+    return <div>Something went wrong...</div>;
+  }
 
-    const yDoc = new Y.Doc();
-    const yText = yDoc.getText("monaco");
-    const yProvider = new WebsocketProvider(
-      `${location.protocol === "http:" ? "ws:" : "wss:"}//localhost:1234`,
-      "cs3219", // session id
-      yDoc
-    );
-    setProvider(yProvider);
-
-    const binding = new MonacoBinding(
-      yText,
-      editorRef?.getModel() as editor.ITextModel,
-      new Set([editorRef]),
-      yProvider.awareness
-    );
-
-    return () => {
-      yDoc.destroy();
-      binding.destroy();
-    };
-  }, [editorRef]);
-
-  const handleEditorOnMount = useCallback(
-    (e: editor.IStandaloneCodeEditor, monaco: Monaco) => {
-      // @ts-expect-error: we ignore since monaco-theme library is outdated with the latest
-      // monaco expected theme types but it still works
-      monaco.editor.defineTheme("dracula", draculaTheme);
-      monaco.editor.setTheme("dracula");
-      setEditorRef(e);
-    },
-    []
-  );
+  const userProfile = UserProfileSchema.parse(userProfileResponse.data);
 
   return (
-    <div className="h-full overflow-scroll">
-      {provider ? <InjectableCursorStyles yProvider={provider} /> : null}
-      <Editor
-        defaultValue={"class Solution {\n" + "  \n" + "}"}
-        onMount={handleEditorOnMount}
-        height={"100%"}
-        width={"100%"}
-        theme="dracula"
-        language={"java"}
-        options={{
-          tabSize: 4,
-          padding: { top: 20 },
-        }}
-      />
+    <div className="flex flex-col w-full h-full">
+      {/* <div className="w-full h-4 bg-background-200"></div> */}
+      <CollaborativeEditor sessionId={getSession} currentUser={userProfile} />
     </div>
   );
-}
-
-interface InjectableCursorStylesProps {
-  yProvider: WebsocketProvider;
-}
-
-interface AwarenessUser {
-  name: string;
-  color: string;
-}
-
-type AwarenessList = [number, AwarenessUser][];
-
-function InjectableCursorStyles({ yProvider }: InjectableCursorStylesProps) {
-  const [awarenessUsers, setAwarenessUsers] = useState<AwarenessList>([]);
-
-  // Set up current user's presence and also update whenever remote client submits its own presence
-  useEffect(() => {
-    const awareness = yProvider.awareness;
-
-    // TODO: make this dynamic with the context api and choose random hex colors
-    awareness.setLocalStateField("user", {
-      name: "Jm San Diego",
-      color: "#FF8C00",
-    });
-
-    const updateAwarenessUsers = () => {
-      const states = awareness.getStates(); // Get all user states
-      const users: AwarenessList = [];
-      states.forEach((val, key) => {
-        users.push([key, val.user]);
-      });
-
-      setAwarenessUsers(users);
-    };
-
-    // Attach change event listener
-    awareness.on("change", updateAwarenessUsers);
-    updateAwarenessUsers();
-
-    return () => {
-      awareness.off("change", updateAwarenessUsers);
-    };
-  }, [yProvider]);
-
-  const styleSheet = useMemo(() => {
-    let cursorStyles = "";
-
-    awarenessUsers.forEach((client, _idx) => {
-      console.log(client, client[0], client[1]);
-      cursorStyles += `
-          .yRemoteSelection-${client[0]},
-          .yRemoteSelectionHead-${client[0]}  {
-            --user-color: ${client[1].color};
-          }
-
-          .yRemoteSelectionHead-${client[0]}::after {
-            content: "${client[1].name}";
-          }
-        `;
-    });
-
-    return { __html: cursorStyles };
-  }, [awarenessUsers]);
-
-  return <style dangerouslySetInnerHTML={styleSheet} />;
 }
