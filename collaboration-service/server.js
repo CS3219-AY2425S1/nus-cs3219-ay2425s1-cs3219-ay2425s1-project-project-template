5003dde42166cd2deee6e603bf2c3d74ae2f05c6
jeraldkiew <jeraldkiew007@gmail.com>
2024-10-25 05:37:47 +0800

    add middleware to server.js

--- collaboration-service/server.js
+++ collaboration-service/server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const sessionRoutes = require('./routes/sessionRoutes');
+const { authMiddleware } = require('./middleware/authMiddleware');
+
+const PORT = process.env.COLLAB_PORT || 8004;
+const USER_SERVICE_API = process.env.USER_SERVICE_API || 'http://localhost/api/user/'
 
 const app = express();
 const server = http.createServer(app);
@@ -12,10 +16,9 @@ const io = new Server(server);
 app.use(express.static('public'));
 
 // Use the session routes at the '/api/collab' path (passing `io` for socket handling)
-app.use('/api/collab', sessionRoutes(io));
+app.use('/api/collab', authMiddleware, sessionRoutes(io));
 
 // Start the server
-const PORT = process.env.COLLAB_PORT || 8004;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
