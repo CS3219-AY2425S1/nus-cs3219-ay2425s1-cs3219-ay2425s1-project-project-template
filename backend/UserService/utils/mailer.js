9c4a77b86670fd8be97af52bdc0801002a8a128c
Chenglong <e0958791@u.nus.edu>
2024-10-24 15:07:20 +0800

    Change email provider to gmail and some bug fixing

--- backend/UserService/utils/mailer.js
+++ backend/UserService/utils/mailer.js
@@ -5,28 +5,34 @@ import { updateUserForgetPasswordTokenById as _updateUserForgetPasswordTokenById
 export const sendEmail = async (email, userId) => {
   try {
     const hashedToken = await bcrypt.hash(userId.toString(), 10);
+    while (hashedToken.charAt(hashedToken.length - 1) === '.') {
+      hashedToken = await bcrypt.hash(userId.toString(), 10);
+    }
     await _updateUserForgetPasswordTokenById(
       userId,
       hashedToken,
       Date.now() + 3600000 // 1 hour
     );
     const transporter = nodemailer.createTransport({
-      host: "sandbox.smtp.mailtrap.io",
-      port: 2525,
+      host: "smtp.gmail.com",
+      port: 587,
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
       },
     });
+
+    const domain = process.env.ENV === "PROD" ? process.env.WEB_DOMAIN: process.env.LOCAL_DOMAIN + ":" + process.env.FRONTEND_PORT;
+    const messageBody =
+      `<p>Please click <a href="${domain}/resetpassword}?token=${hashedToken}">here</a> to Reset your password or copy paste the link below in your browser. The link is only valid for 1 hour.
+          <br>${domain}/?token=${hashedToken}</br>
+      </p>`
 
     const mailOptions = {
       from: "",
       to: email,
       subject: "Reset your password",
-      html: `<p>Please click <a href="${process.env.DOMAIN}/resetpassword}?token=${hashedToken}">here</a> to Reset your password
-                or copy paste the link below in your browser. The link is only valid for 1 hour.
-                <br>${process.env.DOMAIN}/verifyemail?token=${hashedToken}</br>
-            </p>`,
+      html: messageBody,
     };
 
     const mailResponse = await transporter.sendMail(mailOptions);
