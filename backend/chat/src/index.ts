33c4debc4a5a7e9d7f496e7615684379bdc4c107
Chu Yi Ting <ytyt3902@outlook.com>
2024-10-26 01:07:02 +0800

    PEER-236: Graceful termination

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -1,12 +1,34 @@
 import { EXPRESS_PORT } from '@/config';
 import { logger } from '@/lib/utils';
-import server from '@/server';
+import server, { io } from '@/server';
 import { dbHealthCheck } from '@/server';
 
-const port = Number.parseInt(EXPRESS_PORT ?? '8001');
+const port = Number.parseInt(EXPRESS_PORT || '8001');
 
 const listenMessage = `App listening on port: ${port}`;
 server.listen(port, () => {
   void dbHealthCheck();
   logger.info(listenMessage);
 });
+
+const shutdown = () => {
+  logger.info('Shutting down gracefully...');
+
+  server.close((err) => {
+    if (err) {
+      logger.error('Error closing HTTP server', err);
+      process.exit(1);
+    }
+
+    void io
+      .close(() => {
+        logger.info('WS Server shut down');
+      })
+      .then(() => {
+        logger.info('App shut down');
+      });
+  });
+};
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
