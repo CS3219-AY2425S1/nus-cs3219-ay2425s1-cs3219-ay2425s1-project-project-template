8ba48833a52d5e446b8a792de36923d2bb0ef2b0
tituschewxj <tituschewxj@gmail.com>
2024-10-25 15:13:22 +0800

    chore: remove deprecated funcs

--- apps/matching-service/servers/grpc.go
+++ apps/matching-service/servers/grpc.go
@@ -5,6 +5,7 @@ import (
 	pb "matching-service/proto"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
@@ -13,7 +14,7 @@ var (
 
 func InitGrpcServer() *grpc.ClientConn {
 	// Dial the server
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Did not connect: %v", err)
 	}
