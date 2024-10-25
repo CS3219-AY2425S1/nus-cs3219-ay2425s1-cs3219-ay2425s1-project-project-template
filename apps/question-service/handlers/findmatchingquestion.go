e98e0bdebd46ce930688941289e846dcc1277516
tituschewxj <tituschewxj@gmail.com>
2024-10-25 16:09:27 +0800

    feat: improve logs

--- apps/question-service/handlers/findmatchingquestion.go
+++ apps/question-service/handlers/findmatchingquestion.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (s *GrpcServer) FindMatchingQuestion(ctx context.Context, req *pb.MatchQuestionRequest) (*pb.QuestionFound, error) {
-	log.Printf("Received find matching question request: %v", req)
+	log.Printf("Received matching question request: %v", req)
 	return &pb.QuestionFound{
 		QuestionId:         1,
 		QuestionName:       "abc",
