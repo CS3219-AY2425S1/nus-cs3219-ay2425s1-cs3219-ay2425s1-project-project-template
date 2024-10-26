e7d8fdf7c830346c22eede5c79d72a796cbd0fc7
Glemen Neo <glemenneo@gmail.com>
2024-10-26 21:46:23 +0800

    [#121] feat: change websocket to use underlying http server

--- backend/matching-service/src/server.ts
+++ backend/matching-service/src/server.ts
@@ -3,13 +3,15 @@ import http, { Server } from 'http'
 import config from './common/config.util'
 import logger from './common/logger.util'
 import connectToDatabase from './common/mongodb.util'
-import index from './index'
 import connectToRabbitMQ from './common/rabbitmq.util'
+import index from './index'
+import { WebSocketConnection } from './services/ws.service'
 
 connectToRabbitMQ()
 connectToDatabase(config.DB_URL)
 
 const server: Server = http.createServer(index)
+export const wsConnection = new WebSocketConnection(server)
 
 server.listen(config.PORT, async () => {
     logger.info(`[Init] Server is listening on port ${config.PORT}`)
