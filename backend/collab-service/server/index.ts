d7c954194c0c9ecf4c03526dbd57cf8181c23ee5
choo tze jie <119966583+tjch-o@users.noreply.github.com>
2024-10-26 21:19:37 +0800

    Add boilerplate code

--- backend/collab-service/server/index.ts
+++ backend/collab-service/server/index.ts
@@ -5,6 +5,7 @@ import { createServer } from 'http'
 import logger from '../utils/logger'
 import * as Y from 'yjs'
 import { WebsocketProvider } from 'y-websocket'
+import { WebSocket } from 'ws'
 import { router as submitCodeRouter } from '../submit-code/submitCodeRouter'
 
 dotenv.config({ path: './.env' })
@@ -16,10 +17,27 @@ app.use(express.json())
 app.use(submitCodeRouter)
 
 const server = createServer(app)
+const wss = new WebSocket.Server({ server })
 const yDocMap = new Map()
+
+wss.on('connection', (ws: WebSocket) => {
+    const roomName = 'dummy'
+
+    if (!yDocMap.has(roomName)) {
+        yDocMap.set(roomName, new Y.Doc())
+    }
+
+    const sharedDoc = yDocMap.get(roomName)
+
+    const provider = new WebsocketProvider(
+        'ws://localhost:1234', // placeholder
+        roomName,
+        sharedDoc,
+    )
+})
 
 const PORT = process.env.PORT
 
 server.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+    logger.info(`Server running on port ${PORT}`)
 })
