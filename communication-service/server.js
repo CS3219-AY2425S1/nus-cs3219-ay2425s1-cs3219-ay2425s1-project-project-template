3917b7cdf3e62b7dcfc37eb33f66a299693ca752
jeraldkiew <jeraldkiew007@gmail.com>
2024-10-27 01:24:39 +0800

    run on https

--- communication-service/server.js
+++ communication-service/server.js
@@ -3,10 +3,39 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
+const https = require('https');
+const { SSL_CERT, SSL_KEY } = require('./config');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+
+const MY_NETWORK_IP = '192.168.1.248'; // for testing
+const LOCALHOST = 'localhost';
+
+// set to run on localhost or network ip here
+const addr = LOCALHOST;
+
+// ssl certs
+const options = {
+  key: SSL_KEY,
+  cert: SSL_CERT
+}
+
+const httpsServer = https.createServer(options, app);
+
+const httpServer = http.createServer((req, res) => {
+  res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+  res.end();
+});
+
+httpServer.listen(80);
+httpsServer.listen(443);
+
+// server.listen(PORT, addr, () => {
+//   console.log(`Server is running on http://${addr}:${PORT}`);
+// });
+
+const io = new Server(https);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -36,13 +65,4 @@ io.on('connection', (socket) => {
   });
 });
 
-const MY_NETWORK_IP = '192.168.1.248';
-const LOCALHOST = 'localhost';
-const PORT = 8080;
 
-// set to run on localhost or network ip here
-const addr = LOCALHOST;
-
-server.listen(PORT, addr, () => {
-  console.log(`Server is running on http://${addr}:${PORT}`);
-});
