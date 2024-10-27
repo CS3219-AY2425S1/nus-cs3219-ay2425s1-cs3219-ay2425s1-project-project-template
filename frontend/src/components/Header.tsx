61e7c085ace6d573bd694985669ff2056e4a9a14
jasonqiu212 <jason_qiu@hotmail.com>
2024-10-28 03:01:06 +0800

    refactor: refactor header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,25 +1,17 @@
-import {
-  AppShell,
-  Avatar,
-  Group,
-  Title,
-  UnstyledButton,
-} from '@mantine/core';
+import { AppShell, Avatar, Group, Title, UnstyledButton } from '@mantine/core';
 
 function Header() {
   return (
-    <AppShell.Header px="40px" pt="20px" bg="slate.9">
+    <AppShell.Header px="40px" pt="16px" bg="slate.9">
       <Group justify="space-between">
         <a href="." className="logo">
           <Title c="white">PeerPrep</Title>
         </a>
         <UnstyledButton>
-          <Group>
-            <Avatar
-              src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png"
-              radius="xl"
-            />
-          </Group>
+          <Avatar
+            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png"
+            radius="xl"
+          />
         </UnstyledButton>
       </Group>
     </AppShell.Header>
