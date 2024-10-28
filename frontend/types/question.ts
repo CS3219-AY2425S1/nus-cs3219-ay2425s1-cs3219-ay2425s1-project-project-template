112cce7e03af306177d58479121834c8e3a99faf
yunruu <yunruuchin@gmail.com>
2024-10-28 18:30:15 +0800

    feat: update testcase type

--- frontend/types/question.ts
+++ frontend/types/question.ts
@@ -6,6 +6,12 @@ export enum QuestionStatus {
     FAILED = 'failed',
     NOT_ATTEMPTED = 'not_attempted',
 }
+
+export interface ITestcase {
+    input: string
+    output: string
+}
+
 export interface IQuestion {
     id?: string
     title: string
@@ -14,6 +20,17 @@ export interface IQuestion {
     categories: string[]
     status?: QuestionStatus
     link?: string
+    testCases?: ITestcase[]
+}
+
+export interface CreateQuestionDto {
+    title: string
+    description: string
+    complexity: Difficulty
+    categories: string[]
+    link?: string
+    testInputs: string[]
+    testOutputs: string[]
 }
 
 export interface IQuestionsApi {
