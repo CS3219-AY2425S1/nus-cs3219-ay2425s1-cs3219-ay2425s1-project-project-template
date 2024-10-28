04f597693762e119d702f0b506661e1320f774b6
jeraldkiew <jeraldkiew007@gmail.com>
2024-10-29 02:14:00 +0800

    fix running in docker

--- code-execution-service/controllers/codeController.js
+++ code-execution-service/controllers/codeController.js
@@ -17,35 +17,37 @@ const executeCode = (req, res) => {
       return res.status(400).json({ error: 'Code and language are required.' });
     }
 
-    let filename, dockerCommand;
+    let filename, command;
 
-    // Set filename and Docker command based on language
+    // Set filename and command based on language
     switch (language) {
       case 'python':
         filename = 'tempCode.py';
-        dockerCommand = `echo "${input}" | docker run --rm -i -v "${codeDir}/${filename}:/tempCode.py:ro" python:latest python /tempCode.py`;
+        command = `echo "${input}" | python3 ${path.join(codeDir, filename)}`;
         break;
       case 'javascript':
         filename = 'tempCode.js';
-        dockerCommand = `echo "${input}" | docker run --rm -i -v "${codeDir}/${filename}:/app/tempCode.js:ro" node:latest node /app/tempCode.js`;
+        command = `echo "${input}" | node ${path.join(codeDir, filename)}`;
         break;
       case 'cpp':
         filename = 'tempCode.cpp';
-        dockerCommand = `echo "${input}" | docker run --rm -i -v "${codeDir}/${filename}:/tempCode.cpp:ro" gcc:latest sh -c "g++ /tempCode.cpp -o /tempCode && ./tempCode"`;
+        command = `g++ ${path.join(codeDir, filename)} -o ${path.join(codeDir, 'tempCode')} && echo "${input}" | ${path.join(codeDir, 'tempCode')}`;
         break;
       default:
         return res.status(400).json({ error: 'Unsupported language.' });
     }
-    
 
     // Save code to file
     const filePath = path.join(codeDir, filename);
     fs.writeFileSync(filePath, code);
 
-    // Execute code in Docker container
-    exec(dockerCommand, (error, stdout, stderr) => {
-      // Clean up the generated file after execution
+    // Execute code
+    exec(command, (error, stdout, stderr) => {
+      // Clean up generated files after execution
       fs.unlinkSync(filePath);
+      if (language === 'cpp') {
+        fs.unlinkSync(path.join(codeDir, 'tempCode'));
+      }
 
       if (error) {
         return res.status(500).json({ error: stderr || error.message });
