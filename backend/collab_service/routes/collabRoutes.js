57c33300b56091104680683a6401955d1d40a524
Eugene <ekweirui@gmail.com>
2024-10-28 22:17:37 +0800

    Remove unnecessary import

--- backend/collab_service/routes/collabRoutes.js
+++ backend/collab_service/routes/collabRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { collabController, socketSessions } = require('../controllers/collabController');
+const { collabController } = require('../controllers/collabController');
 const ExecuteCodeController = require('../controllers/executeCodeController');
 
 
