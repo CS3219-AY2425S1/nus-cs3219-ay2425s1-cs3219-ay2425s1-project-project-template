7cc22a5979f0951638dab0543f902d2eaef676b7
shishirbychapur <shishirbychapur@gmail.com>
2024-10-28 23:30:28 +0800

    refactor: change console log to logger

--- backend/collaboration-service/src/server.ts
+++ backend/collaboration-service/src/server.ts
@@ -12,14 +12,13 @@ const server: Server = http.createServer(index)
 const wss = new WebSocket.Server({ noServer: true })
 
 server.on('upgrade', (request, socket, head) => {
-    // auth middelware
     const token = request.headers['sec-websocket-protocol']
     if (!token) {
         socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n')
         socket.destroy()
         return
     }
-    console.log(token)
+    logger.info(`[WS] Token: ${token}`)
     const user = {
         id: '1',
     }
@@ -27,13 +26,13 @@ server.on('upgrade', (request, socket, head) => {
         wss.emit('connection', ws, request, user)
     })
 })
-// Handle Yjs WebSocket connection
+
 wss.on('connection', (ws, req, user) => {
-    const docName = req.url.slice(1) // Use the URL as the document name
+    const docName = req.url.slice(1)
     setupWSConnection(ws, req, { docName })
-    console.log(`User ${user.id} connected to document: ${docName}`)
+    logger.info(`User ${user.id} connected to document: ${docName}`)
     ws.on('close', () => {
-        console.log(`User ${user.id} disconnected`)
+        logger.info(`User ${user.id} disconnected from document: ${docName}`)
     })
 })
 
