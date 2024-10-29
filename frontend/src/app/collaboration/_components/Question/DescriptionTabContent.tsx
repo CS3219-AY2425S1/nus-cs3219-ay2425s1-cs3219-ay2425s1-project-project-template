1a3b1e0e088ac2e1c8f6513d8168b9a46fc1ce0e
shavonneg <>
2024-10-30 01:25:18 +0800

    Update DescriptionTabContent.tsx to display updated description format

--- frontend/src/app/collaboration/_components/Question/DescriptionTabContent.tsx
+++ frontend/src/app/collaboration/_components/Question/DescriptionTabContent.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { getQuestions } from "@/services/questionService";
 import { QuestionsSchema } from "@/types/Question";
 
@@ -8,15 +10,23 @@ export default async function DescriptionTabContent() {
     return <div>You are not authenticated</div>;
   }
 
-  // TODO: Based on the session, get the actual question
   const questions = QuestionsSchema.parse(questionsResponse.data.questions);
-
   const question = questions[0];
 
   return (
-    <div className="flex flex-col p-4 overflow-scroll min-w-96">
-      <h2 className="mb-4 text-2xl font-bold">{question.title}</h2>
-      <p>{question.description}</p>
+    <div className="flex flex-col p-4 h-full">
+      <div
+        className="flex flex-col overflow-y-auto"
+        style={{
+          maxHeight: 'calc(100vh - 150px)',
+        }}
+      >
+        <h2 className="mb-4 text-2xl font-bold">{question.title}</h2>
+        <div
+          className="description-content"
+          dangerouslySetInnerHTML={{ __html: question.description }}
+        />
+      </div>
     </div>
   );
 }
