6d3feaf0991260982d84309d327ed18328d6c1d7
gaoyunfan <gaoyunfan8@gmail.com>
2024-10-29 10:36:40 +0800

    Remove collaboration from navbar, build UI for production and redirect to root page from /collaboration when collabstore is empty.

--- frontend/Peerprep/middleware/auth.global.ts
+++ frontend/Peerprep/middleware/auth.global.ts
@@ -1,26 +1,32 @@
 import { useAuthStore } from "~/stores/auth";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-    const authStore = useAuthStore();
+  const authStore = useAuthStore();
+  const collabStore = useCollaborationStore();
 
-    if (!authStore.user) {
-        await authStore.refreshUser();
+  if (!collabStore.isCollaborating && to.path == "/collaboration") {
+    return navigateTo("/");
+  }
+
+  if (!authStore.user) {
+    await authStore.refreshUser();
+  }
+
+  if (authStore.user) {
+    // Check if the route requires admin access
+    if (to.meta.requiresAdmin && !authStore.isAdmin) {
+      return navigateTo("/unauthorized");
     }
 
-    if (authStore.user) {
-        // Check if the route requires admin access
-        if (to.meta.requiresAdmin && !authStore.isAdmin) {
-            return navigateTo('/unauthorized');
-        }
-
-        // If logged in, but trying to access Login or Register page, redirect to Home
-        if (to.path == "/users/login" || to.path == "/users/register") {
-            return navigateTo("/");
-        }
-    } else {
-        // If user is not logged in and the route rquires auth
-        if (to.meta.requiresAuth !== false) {  // Assume requireAuth is true by default
-            return navigateTo("/users/login");
-        }
+    // If logged in, but trying to access Login or Register page, redirect to Home
+    if (to.path == "/users/login" || to.path == "/users/register") {
+      return navigateTo("/");
     }
+  } else {
+    // If user is not logged in and the route rquires auth
+    if (to.meta.requiresAuth !== false) {
+      // Assume requireAuth is true by default
+      return navigateTo("/users/login");
+    }
+  }
 });
