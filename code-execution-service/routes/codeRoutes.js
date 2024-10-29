080d52635248949e5020408d9901f6f95e3db0d6
jeraldkiew <jeraldkiew007@gmail.com>
2024-10-29 20:39:42 +0800

    add get /languages

--- code-execution-service/routes/codeRoutes.js
+++ code-execution-service/routes/codeRoutes.js
@@ -9,6 +9,10 @@ router.get('/', (req, res) => {
     return res.status(200).send('Hello world!');
 });
 
+router.get('/languages', (req, res) => {
+    return res.status(200).json(['python', 'javascript', 'cpp']);
+});
+
 // Route to execute code
 router.post('/', authMiddleware, executeCode);
 
