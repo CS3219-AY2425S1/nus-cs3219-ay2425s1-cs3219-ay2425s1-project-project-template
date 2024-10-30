03b507657630bb7a519cc500d63d141b08be4b3d
jeraldkiew <jeraldkiew007@gmail.com>
2024-10-31 03:40:23 +0800

    use vite env instead of dotenv

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,14 +1,9 @@
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
 interface Config {
   ROOT_BASE_API: string;
 }
 
 const config: Config = {
-  // replace localhost with your IP addr
-  ROOT_BASE_API: 'http://localhost/', 
+  ROOT_BASE_API: import.meta.env.VITE_ROOT_BASE_API || 'http://localhost/', 
 };
 
 export default config;
