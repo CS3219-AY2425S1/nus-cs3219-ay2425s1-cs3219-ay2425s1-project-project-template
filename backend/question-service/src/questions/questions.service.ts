bfecbafd6043900ea5f68289b9b4fe6d83db2cd5
maxng17 <maxngjl@gmail.com>
2024-11-01 02:29:20 +0800

    Create endpoint for fetching question for collab

--- backend/question-service/src/questions/questions.service.ts
+++ backend/question-service/src/questions/questions.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateQuestionDto } from './dto/create-question.dto';
 import { UpdateQuestionDto } from './dto/update-question.dto';
 import { QuestionDB } from './questions.model';
 import { Question } from './schemas/question.schema';
 import { FilterQuestionDto } from './dto/filter-question.dto';
+import { CollabQuestionDto } from './dto/collab-question.dto';
 
 @Injectable()
 export class QuestionsService {
@@ -11,6 +12,20 @@ export class QuestionsService {
 
   async create(createQuestionDto: CreateQuestionDto): Promise<Question> {
     return this.questionDB.createQuestionInDB(createQuestionDto);
+  }
+
+  async findCollabQuestion(
+    collabQuestionDto: CollabQuestionDto,
+  ): Promise<Question | null> {
+    const question =
+      await this.questionDB.findCollabQuestionInDB(collabQuestionDto);
+
+    if (!question) {
+      throw new NotFoundException(
+        'No question found, Please try a different category or complexity'
+      );
+    }
+    return question;
   }
 
   async findAll(filterDto: FilterQuestionDto): Promise<Partial<Question>[]> {
