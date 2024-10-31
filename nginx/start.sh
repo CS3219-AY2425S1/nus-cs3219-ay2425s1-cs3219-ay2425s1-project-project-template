4c1bf0ea452dfe28d23aa0342765abe16a8b847f
Zoe Ang <za.zoeang@gmail.com>
2024-10-31 20:04:27 +0800

    Add CORS access control

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-envsubst '\$USER_SERVICE_ADDR \$QUESTION_SERVICE_ADDR \$MATCH_NOTIFICATION_SERVICE_ADDR \$COLLABORATION_SERVICE_ADDR'< /tmp/default.conf > /etc/nginx/conf.d/default.conf
+envsubst '\$USER_SERVICE_ADDR \$QUESTION_SERVICE_ADDR \$MATCH_NOTIFICATION_SERVICE_ADDR \$COLLABORATION_SERVICE_ADDR \$DOMAIN_NAME'< /tmp/default.conf > /etc/nginx/conf.d/default.conf
 mkdir -p /etc/nginx/conf.d/templates
-cp /tmp/templates/api.conf /etc/nginx/conf.d/templates/api.conf
+envsubst '\$DOMAIN_NAME' < /tmp/templates/api.conf > /etc/nginx/conf.d/templates/api.conf
 cp /tmp/templates/socket.conf /etc/nginx/conf.d/templates/socket.conf
-rm /tmp
 nginx -g 'daemon off;'
