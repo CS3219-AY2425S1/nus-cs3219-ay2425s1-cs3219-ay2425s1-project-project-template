9bdea4c92e30b69be27fb1cd80680174289df171
Bearypop <looyl2k@gmail.com>
2024-10-31 22:00:32 +0800

    Update URLs

--- backend/matching-service/app.py
+++ backend/matching-service/app.py
@@ -14,7 +14,7 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 sio = socketio.Client()
-NOTIFICATION_SERVICE = "https://notification-service-61871891344.asia-southeast1.run.app" \
+NOTIFICATION_SERVICE = "https://notification-service-333971556026.asia-southeast1.run.app" \
 if os.getenv('ENV') == "PROD" else os.getenv('NOTIFICATION_SERVICE', 'http://localhost:5000')
 
 PORT = int(os.environ.get('PORT', 5001))
