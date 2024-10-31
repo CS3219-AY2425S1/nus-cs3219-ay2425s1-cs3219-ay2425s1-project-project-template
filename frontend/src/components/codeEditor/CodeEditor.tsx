3b5fa0f539fe41a271315aedec499f393dc25210
jeraldkiew <jeraldkiew007@gmail.com>
2024-10-31 16:51:34 +0800

    revert

--- frontend/src/components/codeEditor/CodeEditor.tsx
+++ frontend/src/components/codeEditor/CodeEditor.tsx
@@ -1,6 +1,5 @@
 import { sublimeInit } from '@uiw/codemirror-theme-sublime';
-import CodeMirror, { Extension, EditorView } from '@uiw/react-codemirror'; // Import EditorView from CodeMirror
-import { useRef } from 'react';
+import CodeMirror, { Extension } from '@uiw/react-codemirror';
 
 import './CodeEditor.css';
 import classes from './CodeEditor.module.css';
@@ -18,26 +17,13 @@ const customSublime = sublimeInit({
 });
 
 function CodeEditor({ code, setCode, extensions }: CodeEditorProps) {
-  const editorRef = useRef<EditorView | null>(null); // Reference for the EditorView
-
-  const handleChange = (value: string) => {
-    setCode(value);
-    
-    // Find the current cursor position
-    if (editorRef.current) {
-      const cursorPos = editorRef.current.state.selection.main.head; // Get the cursor position
-      console.log(`Cursor Position: ${cursorPos}`); // Log cursor position
-    }
-  };
-
   return (
     <CodeMirror
       value={code}
       theme={customSublime}
       className={classes.codeMirror}
       extensions={extensions}
-      onChange={handleChange} // Use handleChange to log cursor position
-      ref={editorRef} // Attach ref to the CodeMirror component
+      onChange={setCode}
     />
   );
 }
