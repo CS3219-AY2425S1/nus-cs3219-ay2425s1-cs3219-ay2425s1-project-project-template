deadbef33ac56e6dfa4d8cc04d4ad1cb6a4a92c5
thedesalizes <altaria99@gmail.com>
2024-10-31 15:34:49 +0800

    Restore missing functions in UserFunctions

--- frontend/src/services/UserFunctions.ts
+++ frontend/src/services/UserFunctions.ts
@@ -1,15 +1,15 @@
 import { UserCredential } from "firebase/auth"; // Import UserCredential type
 import { SuccessObject, callFunction, HTTP_SERVICE_USER } from "@/lib/utils";
 
-export async function addToUserCollection(userCredential: UserCredential): Promise<SuccessObject> {
-    const { uid, email, displayName } = userCredential.user;
+export async function addToUserCollection(userCredential: UserCredential, username: string): Promise<SuccessObject> {
+    const { uid, email } = userCredential.user;
     // Prepare the data to send in the request body
     const userData = {
         uid,
         email,
-        displayName: displayName || '', // Use an empty string if displayName is not provided
+        username: username,
     };
-    console.log("sending user to collection...")
+    
     const res = await callFunction(HTTP_SERVICE_USER, "user/addToUserCollection", "POST", userData);
 
     return res;
@@ -17,10 +17,11 @@ export async function addToUserCollection(userCredential: UserCredential): Promi
 
 export const fetchAdminStatus = async () => {
     const res = await callFunction(HTTP_SERVICE_USER, "admin/checkAdminStatus", "GET");
-    return res
+    return res.data.isAdmin
 }
 
-export async function getUsernameByUid(uid: string) {
+export async function getUsernameByUid() {
+    const uid = sessionStorage.getItem("uid");
     try {
         const result = await callFunction(HTTP_SERVICE_USER, `user/username/${uid}`)
         if (result.success) {
@@ -33,3 +34,18 @@ export async function getUsernameByUid(uid: string) {
         throw error; // rethrow the error for further handling
     }
 }
+
+export const doesUserExist = async (username: string) => {
+    try {
+      const result = await callFunction(HTTP_SERVICE_USER, `check-username?username=${encodeURIComponent(username)}`);
+      
+      if (result.success) {
+        return result.data.exists; // returns true if username is taken
+      } else {
+        throw new Error('Failed to check username');
+      }
+    } catch (error) {
+      console.error("Error checking username:", error);
+      throw error; // rethrow the error for further handling
+    }
+};
