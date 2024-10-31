e290c000feee79ccdc8f06c382add91b0ab0c594
lynnetteeee <lynnette.oxh@gmail.com>
2024-11-01 03:20:12 +0800

    Update match url in config

--- peer-prep-collab/src/config/config.ts
+++ peer-prep-collab/src/config/config.ts
@@ -3,6 +3,6 @@ dotenv.config();
 
 export const config = {
   port: process.env.PORT || 4003,
-  matchingServiceUrl: process.env.MATCHING_SERVICE_URL || 'http://localhost:3002/match',
+  matchingServiceUrl: process.env.MATCHING_SERVICE_URL || 'http://localhost:3002',
   questionsServiceUrl: process.env.QUESTIONS_SERVICE_URL || 'http://localhost:8080',
 }
