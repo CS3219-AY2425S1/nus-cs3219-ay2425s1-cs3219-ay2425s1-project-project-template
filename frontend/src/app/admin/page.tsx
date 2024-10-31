5aba88f773b5e3148f41202124920e954ab4227c
Izz Hafeez <izzhafeez@gmail.com>
2024-11-01 00:51:39 +0800

    Major refactoring of code

---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import QuestionDashboard from "@/app/admin/components/QuestionDashboard";
+
+const QuestionDashboardPage = () => {
+  return <QuestionDashboard />;
+};
+
+export default QuestionDashboardPage;
