25d6aaefc7af7547920da72d2213fce16b7bfd1a
Bearypop <looyl2k@gmail.com>
2024-10-31 20:33:53 +0800

    Update URLs

--- frontend/src/Services.js
+++ frontend/src/Services.js
@@ -1,11 +1,11 @@
 export const QUESTIONS_SERVICE = process.env.REACT_APP_ENV === "PROD" ? 
-"https://question-service-565303241532.asia-southeast1.run.app" : "http://localhost:3001";
+"https://question-service-582636322752.asia-southeast1.run.app" : "http://localhost:3001";
 
 export const USER_SERVICE = process.env.REACT_APP_ENV === "PROD" ?
-"https://user-service-565303241532.asia-southeast1.run.app" : "http://localhost:3002";
+"https://user-service-582636322752.asia-southeast1.run.app" : "http://localhost:3002";
 
 export const NOTIFICATION_SERVICE = process.env.REACT_APP_ENV === "PROD" ?
-"https://notification-service-565303241532.asia-southeast1.run.app" : "http://localhost:5000";
+"https://notification-service-582636322752.asia-southeast1.run.app" : "http://localhost:5000";
 
 export const MATCHING_SERVICE = process.env.REACT_APP_ENV === "PROD" ?
-"https://matching-service-565303241532.asia-southeast1.run.app" : "http://localhost:5001";
+"https://matching-service-582636322752.asia-southeast1.run.app" : "http://localhost:5001";
