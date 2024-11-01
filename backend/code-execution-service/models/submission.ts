652a5c54348d1c1ee8567ae2ed0398f2bbea0c16
choo tze jie <119966583+tjch-o@users.noreply.github.com>
2024-11-02 03:50:11 +0800

    Shift submission model to code execution microservice

---
+++ backend/code-execution-service/models/submission.ts
@@ -0,0 +1,32 @@
+import { Schema, model, models, Document } from 'mongoose';
+
+export interface ISubmission extends Document {
+    submissionId: number
+    collaborators: number[]
+    questionId: number
+    language: string
+    code: string
+    solved: boolean
+    testCasesPassed: number
+    testCasesTotal: number
+}   
+
+const SubmissionSchema = new Schema<ISubmission>(
+    {
+        submissionId: { type: Number, required: true, unique: true },
+        collaborators: { type: [Number], required: true, default: [] },
+        questionId: { type: Number, required: true, index: true },
+        language: { type: String, required: true },
+        code: { type: String, required: true, default: '' },
+        solved: { type: Boolean },
+        testCasesPassed: { type: Number, default: 0 },
+        testCasesTotal: { type: Number, default: 0 },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Submission = models.Submission || model<ISubmission>('Submission', SubmissionSchema);
+
+export default Submission;
