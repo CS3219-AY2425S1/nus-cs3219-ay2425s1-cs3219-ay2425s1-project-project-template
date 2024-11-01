a596583053a853dd5df2771511edbf3c4691748d
jeraldkiew <jeraldkiew007@gmail.com>
2024-11-02 01:04:51 +0800

    add testCases for q2 roman to integer

--- question-service/dev/add_sample_data.js
+++ question-service/dev/add_sample_data.js
@@ -6,26 +6,30 @@ import Question from '../model/Question.js';
 
 const mongoDB = process.env.QUESTION_MONGO_CLOUD_URI;
 console.log(mongoDB);
-const Admin = mongoose.mongo.Admin;
 
-main().catch((err) => console.log(err));
+main();
 
 async function main() {
-  await mongoose.connect(mongoDB);
-  
-  // delete all
-  // await Question.deleteMany({});
-
-  // add sample questions
-  fs.readFile('./data/sample_questions.json', async (err, data) => {
-    if (err) throw err;
-    const sample_questions = JSON.parse(data);
+  console.log('hello');
+  try {
     await mongoose.connect(mongoDB);
-    for (let i = 0; i < sample_questions.length; i++) { // Loop through the entire array
-      const sample_question = sample_questions[i];
-      const question = new Question(sample_question);
-      await question.save();
-      // console.log('success for: ', sample_question.title);
-    }
-  });
+
+    // delete all
+    // await Question.deleteMany({});
+
+    // add sample questions
+    fs.readFile('./data/sample_questions.json', async (err, data) => {
+      if (err) throw err;
+      const sample_questions = JSON.parse(data);
+      console.log(sample_questions)
+      for (let i = 0; i < sample_questions.length; i++) { // Loop through the entire array
+        const sample_question = sample_questions[i];
+        console.log('success for: ', sample_question.title);
+        const question = new Question(sample_question);
+        await question.save();
+      }
+    })
+  } catch (error) {
+    console.log("error: ", error)
+  }
 }
