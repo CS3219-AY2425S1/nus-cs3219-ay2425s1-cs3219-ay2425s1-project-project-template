e9c7371db123df683b428375b296d19264a8aa0e
smolegz <zhuohui.koo@gmail.com>
2024-11-01 23:36:53 +0800

    Remove console log

--- message-queue/src/helper.ts
+++ message-queue/src/helper.ts
@@ -19,8 +19,7 @@ export const deepEqual = (x, y) => {
   }
 
 export const generateSessionId = (user1Id: UserData, user2Id: UserData) => {
-  const combinedId = [user1Id.user_id, user2Id.user_id].sort().join('-')
-  console.log(combinedId)
+  const combinedId = [user1Id.user_id, user2Id.user_id].sort().join('-') // e.g. user1-user2
   const sessionId = crypto.createHash('sha256').update(combinedId).digest('hex')
   return sessionId
 }
