d82143f274c86b34dd4bd8f411e4671ed1837eb1
choo tze jie <119966583+tjch-o@users.noreply.github.com>
2024-11-02 05:37:40 +0800

    Remove unnecessary files and packages

--- backend/collab-service/submit-code/submitCodeController.ts
+++
@@ -1,5 +0,0 @@
-import logger from "../utils/logger"
-
-const submitCode = async () => {}
-
-export { submitCode }
--- backend/collab-service/submit-code/submitCodeRouter.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { submitCode } from './submitCodeController'
-
-const router = Router()
-router.post('/submit-code', submitCode)
-
-export { router }
