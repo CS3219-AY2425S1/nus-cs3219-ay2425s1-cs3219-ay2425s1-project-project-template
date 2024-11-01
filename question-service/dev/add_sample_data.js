c8bddd059bc1979cba39a2dabdecf8180f029f2b
jeraldkiew <jeraldkiew007@gmail.com>
2024-11-02 00:24:22 +0800

    fix sample_questions and add_sample_data

--- question-service/dev/add_sample_data.js
+++ question-service/dev/add_sample_data.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 
 import Question from '../model/Question.js';
 
-const mongoDB = process.env.MONGO_URI;
+const mongoDB = process.env.QUESTION_MONGO_CLOUD_URI;
 console.log(mongoDB);
 const Admin = mongoose.mongo.Admin;
 
@@ -12,18 +12,19 @@ main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(mongoDB);
+  // delete all
   await Question.deleteMany({});
+
+  // add sample questions
   fs.readFile('./data/sample_questions.json', async (err, data) => {
     if (err) throw err;
     const sample_questions = JSON.parse(data);
-    console.log(sample_questions[0]);
     await mongoose.connect(mongoDB);
     for (let i = 0; i < sample_questions.length; i++) { // Loop through the entire array
       const sample_question = sample_questions[i];
-      console.log('sample question: ', sample_question);
       const question = new Question(sample_question);
       await question.save();
-      console.log('success for: ', sample_question.title);
+      // console.log('success for: ', sample_question.title);
     }
   });
 }
