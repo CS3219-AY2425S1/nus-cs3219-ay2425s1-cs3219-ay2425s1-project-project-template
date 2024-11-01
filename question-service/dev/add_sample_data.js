9910c712d72021a31a87162f9eb29dd4a3ed018b
jeraldkiew <jeraldkiew007@gmail.com>
2024-11-01 20:57:19 +0800

    add_sample_data

--- question-service/dev/add_sample_data.js
+++ question-service/dev/add_sample_data.js
@@ -8,17 +8,17 @@ const mongoDB = process.env.MONGO_URI;
 console.log(mongoDB);
 const Admin = mongoose.mongo.Admin;
 
-mongoose.connect(mongoDB);
-
 main().catch((err) => console.log(err));
 
 async function main() {
+  await mongoose.connect(mongoDB);
+  await Question.deleteMany({});
   fs.readFile('./data/sample_questions.json', async (err, data) => {
     if (err) throw err;
     const sample_questions = JSON.parse(data);
     console.log(sample_questions[0]);
     await mongoose.connect(mongoDB);
-    for (let i = 0; i < 19; i++) {
+    for (let i = 0; i < sample_questions.length; i++) { // Loop through the entire array
       const sample_question = sample_questions[i];
       console.log('sample question: ', sample_question);
       const question = new Question(sample_question);
