6042804f8e2372f5f7d1f906f02bd31c9c3bd943
wr1159 <chuweiron18@gmail.com>
2024-11-02 00:15:27 +0800

    Add .env.sample for DB and Port

--- collab-service/app/server.js
+++ collab-service/app/server.js
@@ -4,7 +4,7 @@ import index from "./index.js";
 import { Server } from "socket.io";
 import { addMessageToChat } from "./model/repository.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3002;
 const server = http.createServer(index);
 
 const io = new Server(server, {
