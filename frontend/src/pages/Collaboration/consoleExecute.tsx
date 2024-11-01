78a1ba9187c324c5d8a4210a9ae6e903e39c2e67
linuschancs <mrlinuschan@gmail.com>
2024-11-01 11:13:06 +0800

    Use self-hosted code execution engine

--- frontend/src/pages/Collaboration/consoleExecute.tsx
+++ frontend/src/pages/Collaboration/consoleExecute.tsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { LANGUAGE_VERSIONS } from "../../components/Collaboration/languageSelector";
 
+// const API = axios.create({
+//   baseURL: "https://emkc.org/api/v2/piston",
+// });
 const API = axios.create({
-  baseURL: "https://emkc.org/api/v2/piston",
+  baseURL: `http://localhost:${process.env.REACT_APP_PISTON_PORT}/api/v2`,
 });
 
 export const executeCode = async (language:string, sourceCode:string) => {
