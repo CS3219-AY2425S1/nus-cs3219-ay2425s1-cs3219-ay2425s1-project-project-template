c95909e4f46e208b5893b52cd709d3e12e782f2a
Zoe Ang <za.zoeang@gmail.com>
2024-11-01 23:54:27 +0800

    Add verification routes

--- user-service/app.js
+++ user-service/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
+const verifyRoutes = require('./routes/verificationRoutes');
 const User = require('./model/User');
 
 // Initialize Express app
@@ -41,6 +42,7 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/verify', verifyRoutes);
 
 // Health check route
 app.get('/', (req, res) => {
