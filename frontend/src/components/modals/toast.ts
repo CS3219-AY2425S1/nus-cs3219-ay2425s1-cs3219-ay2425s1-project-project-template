cbec6a0d6517aa8f5aa3a614c7ee3c79c6e55ecf
jamz903 <jamietohhl@gmail.com>
2024-11-03 01:07:37 +0800

    Fix more linting errors

--- frontend/src/components/modals/toast.ts
+++ frontend/src/components/modals/toast.ts
@@ -1,23 +1,15 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-let toast: any;
+import Swal from "sweetalert2/dist/sweetalert2.js";
 
-// Function to initialize and retrieve the toast instance
-const getToastInstance = async () => {
-  if (!toast && typeof window !== "undefined") {
-    const Swal = (await import("sweetalert2")).default;
-    toast = Swal.mixin({
-      toast: true,
-      position: "top-end",
-      showConfirmButton: false,
-      timer: 3000,
-      timerProgressBar: true,
-      didOpen: (toast) => {
-        toast.onmouseenter = Swal.stopTimer;
-        toast.onmouseleave = Swal.resumeTimer;
-      }
-    });
+const toast = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
   }
-  return toast;
-};
+});
 
-export default getToastInstance;
+export default toast;
