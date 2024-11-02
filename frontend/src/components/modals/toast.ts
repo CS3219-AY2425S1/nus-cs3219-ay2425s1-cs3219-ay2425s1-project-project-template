f3cc23ffb38634718ad5388df6df868fc270a0f9
jamz903 <jamietohhl@gmail.com>
2024-11-03 00:53:52 +0800

    Fix webpack error

--- frontend/src/components/modals/toast.ts
+++ frontend/src/components/modals/toast.ts
@@ -1,15 +1,23 @@
-import Swal from "sweetalert2";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+let toast: any;
 
-const toast = Swal.mixin({
-  toast: true,
-  position: "top-end",
-  showConfirmButton: false,
-  timer: 3000,
-  timerProgressBar: true,
-  didOpen: (toast) => {
-    toast.onmouseenter = Swal.stopTimer;
-    toast.onmouseleave = Swal.resumeTimer;
+// Function to initialize and retrieve the toast instance
+const getToastInstance = async () => {
+  if (!toast && typeof window !== "undefined") {
+    const Swal = (await import("sweetalert2")).default;
+    toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    });
   }
-});
+  return toast;
+};
 
-export default toast;
+export default getToastInstance;
