23c6373420729132754f7df8b879ef4f826a7884
Mai Ting Kai <wheatgrass.mc@gmail.com>
2024-11-03 01:25:06 +0800

    Fix lints

--- frontend/src/app/(auth)/collaboration/components/icon/RedoIcon.tsx
+++ frontend/src/app/(auth)/collaboration/components/icon/RedoIcon.tsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
-import Link from "next/link";
 
 const RedoIcon = () => {
   const iconSize = 24;
   return (
     <div className="inline-block">
-        <Image
-          alt="Redo Icon"
-          src="/redo.svg"
-          width={iconSize}
-          height={iconSize}
-        />
+      <Image
+        alt="Redo Icon"
+        src="/redo.svg"
+        width={iconSize}
+        height={iconSize}
+      />
     </div>
   );
 };
