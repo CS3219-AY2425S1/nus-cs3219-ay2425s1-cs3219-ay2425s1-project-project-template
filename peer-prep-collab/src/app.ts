49546efd2cea47a389f50fe6cc82ec751b7e08db
smolegz <zhuohui.koo@gmail.com>
2024-11-02 08:05:31 +0800

    Add consumer for collab svc

--- peer-prep-collab/src/app.ts
+++ peer-prep-collab/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import collabRoutes from './routes/collaboration.routes';
+import { connectToRabbitMQ, initConsumer } from '.';
 
 dotenv.config();
 
@@ -12,7 +13,18 @@ app.use('/collab', collabRoutes);
 
 // process.env = built-in object in Node.js that contains all the environment variables 
 // process.env.PORT allows us to change the port the server runs on without modifying code (for diff environments)
-const PORT = process.env.PORT || 4003;
-app.listen(PORT, () => {
-    console.log(`Collaboration service is running on port ${PORT}`);
-})
+
+connectToRabbitMQ()
+    .then(() => {
+        initConsumer() //Start consuming
+    })
+    .then(() => {
+        const PORT = process.env.PORT || 4003;
+        app.listen(PORT, () => {
+            console.log(`Collaboration service is running on port ${PORT}`);
+        })
+    })
+    .catch((err) => {
+        throw new Error(err) // Throw back error if caught,
+    })
+
---
+++ peer-prep-collab/src/index.ts
@@ -0,0 +1,40 @@
+import amqp, { Channel, Connection, ConsumeMessage, Message } from "amqplib"
+
+let channel: Channel
+
+export const connectToRabbitMQ = async () => {
+    try {
+        const amqpServer = "amqps://lguugvwb:UtQY1D0zOoX8s0ZvR4GunuRDk0xv8UuI@octopus.rmq3.cloudamqp.com/lguugvwb"
+        const connection = await amqp.connect(amqpServer)
+        channel = await connection.createChannel()
+        console.log("Connected to RabbitMQ")
+    } catch (err) {
+      console.log(err)
+    }
+}
+
+
+const startConsume = async<T>(onMessage: (message: Message) => void) => {
+  try {
+    await channel.assertQueue('collab_queue', {
+      durable: false,
+      messageTtl: 30000,
+    })
+    channel.consume('collab_queue', msg => {
+      if (msg) {
+        onMessage(JSON.parse(msg.content.toString()))
+        channel.ack(msg)
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+// To implement generation of session URL @LYNETTE @YUANTING -
+function onMessage(message: any) {
+  console.log(message.sessionId) 
+}
+
+
+export const initConsumer = () => startConsume(onMessage)
