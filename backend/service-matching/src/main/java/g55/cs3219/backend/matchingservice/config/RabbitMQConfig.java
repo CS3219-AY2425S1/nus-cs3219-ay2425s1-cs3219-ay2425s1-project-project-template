d74485a94bd9c1f9486c8a5b0a1c54a187700be1
zsheng <zsheng.dev@gmail.com>
2024-11-02 09:16:16 +0800

    Update message converter to Jackson2Json

--- backend/service-matching/src/main/java/g55/cs3219/backend/matchingservice/config/RabbitMQConfig.java
+++ backend/service-matching/src/main/java/g55/cs3219/backend/matchingservice/config/RabbitMQConfig.java
@@ -3,30 +3,34 @@ package g55.cs3219.backend.matchingservice.config;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.SimpleMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
 public class RabbitMQConfig {
+    public static final String MATCHING_QUEUE = "matchingQueue";
+    public static final String MATCH_FOUND_QUEUE = "matchFoundQueue";
+
     @Bean
     public Queue matchingQueue() {
-        return new Queue("matchingQueue", false);
+        return new Queue(MATCHING_QUEUE, false);
     }
 
     @Bean
-    public SimpleMessageConverter simpleMessageConverter() {
-        SimpleMessageConverter converter = new SimpleMessageConverter();
-        converter.setAllowedListPatterns(List.of("g55.cs3219.backend.matchingservice.model.*"));
-        return converter;
+    public Queue matchFoundQueue() {
+        return new Queue(MATCH_FOUND_QUEUE, false);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(simpleMessageConverter());
+        template.setMessageConverter(messageConverter());
         return template;
     }
 }
