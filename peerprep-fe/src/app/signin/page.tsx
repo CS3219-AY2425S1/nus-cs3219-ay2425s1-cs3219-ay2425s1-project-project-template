dedc491a15790ce38ea9e579da50d156ad28517c
kurt <kurtyjlee@gmail.com>
2024-11-03 01:42:55 +0800

    remove google OAuth

--- peerprep-fe/src/app/signin/page.tsx
+++ peerprep-fe/src/app/signin/page.tsx
@@ -112,7 +112,7 @@ export default function LoginForm() {
             </span>
           </div>
         </div>
-        <div className="grid grid-cols-2 gap-3">
+        <div className="flex w-full justify-center gap-3">
           <Button
             variant="outline"
             className="rounded-md border border-gray-600 bg-gray-700 py-2 text-sm font-medium text-white hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -120,18 +120,6 @@ export default function LoginForm() {
           >
             <GithubIcon className="mr-2 h-5 w-5" />
             GitHub
-          </Button>
-          <Button
-            variant="outline"
-            className="rounded-md border border-gray-600 bg-gray-700 py-2 text-sm font-medium text-white hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-800"
-          >
-            <svg className="mr-2 h-5 w-5" viewBox="0 0 24 24">
-              <path
-                fill="currentColor"
-                d="M12.545,10.239v3.821h5.445c-0.712,2.315-2.647,3.972-5.445,3.972c-3.332,0-6.033-2.701-6.033-6.032s2.701-6.032,6.033-6.032c1.498,0,2.866,0.549,3.921,1.453l2.814-2.814C17.503,2.988,15.139,2,12.545,2C7.021,2,2.543,6.477,2.543,12s4.478,10,10.002,10c8.396,0,10.249-7.85,9.426-11.748L12.545,10.239z"
-              />
-            </svg>
-            Google
           </Button>
         </div>
         <div className="flex justify-center text-center text-sm text-gray-400">
