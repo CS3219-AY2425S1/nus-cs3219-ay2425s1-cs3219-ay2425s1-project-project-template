81578ca463add32b005fe0d8c8ab37c8134b81fc
simbayippy <simbayippy@gmail.com>
2024-11-03 20:20:14 +0800

    fix comments

--- peerprep-fe/src/lib/utils.ts
+++ peerprep-fe/src/lib/utils.ts
@@ -65,3 +65,12 @@ export const validateUsername = (username: string): string[] => {
   }
   return errors;
 };
+
+export const stringToColor = (str: string) => {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  const color = Math.abs(hash).toString(16).substring(0, 6);
+  return `#${'0'.repeat(6 - color.length)}${color}`;
+};
