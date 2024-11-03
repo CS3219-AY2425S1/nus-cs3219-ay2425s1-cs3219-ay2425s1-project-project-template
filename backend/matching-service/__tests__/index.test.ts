b97a54d63004567a4048813d6270cb5840bc1ee3
Davian Kho <daviankho1@gmail.com>
2024-11-04 02:10:22 +0800

    feat: add base jest test for matching service

--- backend/matching-service/__tests__/index.test.ts
+++ backend/matching-service/__tests__/index.test.ts
@@ -3,6 +3,8 @@ import configMock from '../__mocks__/config.mock'
 import app from '../src/index'
 
 jest.mock('../src/common/config.util', () => configMock)
+jest.mock('../src/common/rabbitmq.util', () => () => {})
+jest.mock('../src/services/rabbitmq.service', () => {})
 
 describe('Index', () => {
     describe('GET /', () => {
