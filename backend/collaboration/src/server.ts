2b80677e32b392ceff48aa2d672d58027f06010d
Mai Ting Kai <wheatgrass.mc@gmail.com>
2024-11-03 18:43:47 +0800

    Rename routes internally for API gateway

--- backend/collaboration/src/server.ts
+++ backend/collaboration/src/server.ts
@@ -1,8 +1,8 @@
-import express from "express";
-import collaborationRoutes from "./routes/collaborationRoutes";
 import cors from "cors";
 import dotenv from "dotenv";
+import express from "express";
 import mongoose from "mongoose";
+import collaborationRoutes from "./routes/collaborationRoutes";
 dotenv.config();
 
 /**
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/api", collaborationRoutes);
+app.use("/api/collaboration", collaborationRoutes);
 
 const PORT = process.env.PORT || "3001";
 
--- backend/question/src/server.ts
+++ backend/question/src/server.ts
@@ -1,8 +1,8 @@
-import express from "express";
-import questionRoutes from "./routes/questionRoutes";
 import cors from "cors";
 import dotenv from "dotenv";
+import express from "express";
 import mongoose from "mongoose";
+import questionRoutes from "./routes/questionRoutes";
 dotenv.config();
 
 /**
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/api", questionRoutes);
+app.use("/api/question", questionRoutes);
 
 const PORT = process.env.PORT || "3003";
 
