0d828d3632dc2bc2db40900ff7db190e3351eb6e
Rick <rick.kohjiaxuan@gmail.com>
2024-11-03 13:08:48 +0800

    Improve UI

--- frontend/src/app/collaboration/_components/TestResult/TestCasesTabContent.tsx
+++ frontend/src/app/collaboration/_components/TestResult/TestCasesTabContent.tsx
@@ -9,27 +9,31 @@ export default function TestCasesTabContent({
   question,
 }: TestCasesTabContentProps) {
   return (
-    <Tabs defaultValue={`test-case-0`} className="w-full h-full pt-2">
-      <TabsList className="flex flex-row justify-start gap-2 bg-transparent rounded-t-none">
-        {/* Dynamic test cases */}
+    <Tabs defaultValue={`test-case-0`} className="w-full h-full p-2">
+      <TabsList className="flex flex-row justify-start gap-2 bg-transparent p-0">
         {question.testCases.map((_, index: number) => (
           <TabsTrigger value={`test-case-${index}`} key={index}>
             Test case {index + 1}
           </TabsTrigger>
         ))}
       </TabsList>
-      {/* Dynamic test case content */}
       {question.testCases.map((testCase: TestCase, index: number) => (
-        <TabsContent value={`test-case-${index}`} key={index}>
-          <div className="flex flex-row gap-8 p-4">
+        <TabsContent
+          value={`test-case-${index}`}
+          key={index}
+          className="p-0 text-sm"
+        >
+          <div className="flex flex-col gap-2">
             <label>
-              <p>Input</p>
+              <p className="text-xs text-foreground-100 mb-1">Input =</p>
               <p className="px-2 py-1 rounded-md bg-background-100">
                 {testCase.input}
               </p>
             </label>
             <label>
-              <p>Expected output</p>
+              <p className="text-xs text-foreground-100 mb-1">
+                Expected output =
+              </p>
               <p className="px-2 py-1 rounded-md bg-background-100">
                 {testCase.expectedOutput}
               </p>
