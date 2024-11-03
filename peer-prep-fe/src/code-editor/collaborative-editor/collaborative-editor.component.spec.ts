89e6348799d0d3546ae8fe38d66268fb78c6836f
AidenLYT <ytstirlingite@hotmail.com>
2024-11-03 21:51:42 +0800

    Pass ng test

--- peer-prep-fe/src/code-editor/collaborative-editor/collaborative-editor.component.spec.ts
+++ peer-prep-fe/src/code-editor/collaborative-editor/collaborative-editor.component.spec.ts
@@ -1,5 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { NGX_MONACO_EDITOR_CONFIG } from 'ngx-monaco-editor-v2';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 import { CollaborativeEditorComponent } from './collaborative-editor.component';
 
 describe('CollaborativeEditorComponent', () => {
@@ -8,9 +10,24 @@ describe('CollaborativeEditorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CollaborativeEditorComponent]
-    })
-    .compileComponents();
+      imports: [CollaborativeEditorComponent],
+      providers: [
+        {
+          provide: NGX_MONACO_EDITOR_CONFIG,
+          useValue: {
+            baseUrl: 'assets/monaco',
+            defaultOptions: { scrollBeyondLastLine: false },
+          }
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ sessionId: 'test-session', userId: 'test-user' }),
+            snapshot: { params: { sessionId: 'test-session', userId: 'test-user' } }
+          }
+        }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CollaborativeEditorComponent);
     component = fixture.componentInstance;
