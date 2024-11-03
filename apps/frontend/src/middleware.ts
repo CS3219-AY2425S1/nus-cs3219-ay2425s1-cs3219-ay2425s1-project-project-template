3b810cf5174dc79a5559d290833064ac0ecfa3e0
tituschewxj <tituschewxj@gmail.com>
2024-11-03 17:52:53 +0800

    fix: add token expire check and comment out backend token check request

--- apps/frontend/src/middleware.ts
+++ apps/frontend/src/middleware.ts
@@ -1,5 +1,6 @@
-import { NextURL } from 'next/dist/server/web/next-url';
-import { type NextRequest, NextResponse } from 'next/server';
+import { NextURL } from "next/dist/server/web/next-url";
+import { type NextRequest, NextResponse } from "next/server";
+import { jwtDecode } from "jwt-decode";
 
 const PUBLIC_ROUTES = ["/login", "/register"];
 
@@ -17,24 +18,44 @@ async function isValidToken(TOKEN: string): Promise<boolean> {
   return status === 200;
 }
 
+function isTokenExpired(token: string) {
+  if (!token) return true;
+
+  try {
+    const decodedToken = jwtDecode(token);
+    const currentTime = Date.now() / 1000; // Current time in seconds
+    return decodedToken?.exp != undefined && decodedToken.exp < currentTime;
+  } catch (error) {
+    return true; // Return true if token is invalid
+  }
+}
+
 export default async function middleware(request: NextRequest) {
-  const REDIRECT_TO_LOGIN = NextResponse.redirect(new NextURL("/login", request.url));
+  const REDIRECT_TO_LOGIN = NextResponse.redirect(
+    new NextURL("/login", request.url)
+  );
   const TOKEN = request.cookies.get("TOKEN");
   if (TOKEN == undefined) {
     return REDIRECT_TO_LOGIN;
   }
-  
-  if (!await isValidToken(TOKEN.value)) {
+
+  if (isTokenExpired(TOKEN.value)) {
     REDIRECT_TO_LOGIN.cookies.delete("TOKEN");
     return REDIRECT_TO_LOGIN;
   }
 
+  // FIXME: isValidToken check leads to error: not being able to access user service.
+  // if (!(await isValidToken(TOKEN.value))) {
+  //   REDIRECT_TO_LOGIN.cookies.delete("TOKEN");
+  //   return REDIRECT_TO_LOGIN;
+  // }
+
   return NextResponse.next();
-  
 }
 
 export const config = {
-  matcher: "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|login|register).*)",
+  matcher:
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|login|register).*)",
   // matcher: [
   //   "/matching",
   //   "/",
@@ -42,5 +63,4 @@ export const config = {
   //   "/question",
   //   "/question/.*",
   // ],
-}
-
+};
