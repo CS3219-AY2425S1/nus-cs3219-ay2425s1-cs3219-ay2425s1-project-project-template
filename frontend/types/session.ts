eeb27a643de266516d41f926f12215c6c3d76edd
yunruu <yunruuchin@gmail.com>
2024-11-04 02:36:24 +0800

    feat: add session types and class manager

--- frontend/types/session.ts
+++ frontend/types/session.ts
@@ -1,12 +1,63 @@
+import { IPagination, IQuestion, ISortBy } from '.'
+
 interface ISession {
-    id: string
-    name: string
-    email: string
-    question: string
-    description: string
-    status: string
+    isCompleted: string
     complexity: string
     time: number
+    category: string
+    collaboratorName: string
+    question: string
+    createdAt: string
+}
+
+interface ISessionDto {
+    isCompleted: string
+    complexity: string
+    time: number
+    category: string
+    user1Id: string
+    user1Name: string
+    user2Id: string
+    user2Name: string
+    question: IQuestion
+    createdAt: string
+}
+
+export interface IGetSessionsDto {
+    page: number
+    limit: number
+    sortBy?: string
+}
+
+export interface IGetSessions {
+    page: number
+    limit: number
+    sortBy?: ISortBy
+}
+
+export interface ISessionsApi {
+    sessions: ISessionDto[]
+    pagination: IPagination
+}
+
+export class SessionManager {
+    static fromDto(dto: ISessionDto[], username?: string): ISession[] {
+        const getCollaborator = (user1: string, user2: string) => {
+            return user1 === username ? user2 : user1
+        }
+
+        return dto.map((session) => {
+            return {
+                isCompleted: session.isCompleted,
+                complexity: session.complexity,
+                time: session.time,
+                category: session.category,
+                collaboratorName: getCollaborator(session.user1Name, session.user2Name),
+                question: session.question.title,
+                createdAt: session.createdAt,
+            }
+        })
+    }
 }
 
 export type { ISession }
