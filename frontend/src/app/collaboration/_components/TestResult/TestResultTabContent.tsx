d6e209aff8d43020d12bd673a4a5bcf9df4a62a4
Rick <rick.kohjiaxuan@gmail.com>
2024-11-03 13:29:28 +0800

    Temp test result tab content

--- frontend/src/app/collaboration/_components/TestResult/TestResultTabContent.tsx
+++ frontend/src/app/collaboration/_components/TestResult/TestResultTabContent.tsx
@@ -1,7 +1,30 @@
+"use client";
+
+const submitCodeResponse = {
+  statusCode: 200,
+  message: "Success",
+  data: {
+    status: "ok",
+    result: {
+      stdout: "7236\n",
+      stderr: "",
+      executionTime: 22,
+    },
+  },
+};
+
 export default function TestResultTabContent() {
   return (
-    <>
-      <h2>Test Result</h2>
-    </>
+    <div className="p-2 h-full w-full">
+      <div className="flex justify-center items-center w-full h-full">
+        <p>
+          You must submit your code to see the test result:{" "}
+          {submitCodeResponse.statusCode}
+        </p>
+      </div>
+      {/* <div>
+        <p className="p-2">Test result</p>
+      </div> */}
+    </div>
   );
 }
