0079e7d3e34b7ce6cae8e99b139d4442b3984de0
yunruu <yunruuchin@gmail.com>
2024-11-04 02:35:16 +0800

    feat: change input label name

--- frontend/pages/code/testcase-tab.tsx
+++ frontend/pages/code/testcase-tab.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 
 export default function TestcasesTab({ testcases }: { testcases: ITestcase[] }) {
     const [activeTestcaseIdx, setActiveTestcaseIdx] = useState(0)
-    const testcaseTabs = testcases?.map((testcase) => `Case ${testcase.idx}`)
+    const testcaseTabs = testcases?.map((_, id) => `Case ${id}`)
 
     if (!testcases) return null
     return (
@@ -16,7 +16,7 @@ export default function TestcasesTab({ testcases }: { testcases: ITestcase[] }) 
                 size="label"
             />
             <div className="flex flex-col gap-2 text-sm">
-                <span className="mt-2">{testcases[activeTestcaseIdx]?.inputVar} =</span>
+                <span className="mt-2">Expected input =</span>
                 <span className="px-2 py-1 rounded-lg bg-slate-100">{testcases[activeTestcaseIdx]?.input}</span>
                 <span className="mt-2">Expected output = </span>
                 <span className="px-2 py-1 rounded-lg bg-slate-100">{testcases[activeTestcaseIdx]?.output}</span>
