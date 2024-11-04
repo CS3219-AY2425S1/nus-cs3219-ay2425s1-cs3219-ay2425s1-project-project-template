378d4767594732ea4d0a4e2bb7c87675e61b9951
shishirbychapur <shishirbychapur@gmail.com>
2024-11-05 02:06:25 +0800

    fix

--- packages/submission-types/src/SubmissionRequestDto.ts
+++ packages/submission-types/src/SubmissionRequestDto.ts
@@ -1,10 +1,7 @@
 import { IsNotEmpty, IsNumber, IsString, validate, ValidationError } from 'class-validator'
+import 'reflect-metadata'
 
 export class SubmissionRequestDto {
-    @IsNumber()
-    @IsNotEmpty()
-    language_id: number
-
     @IsString()
     source_code: string
 
@@ -13,6 +10,10 @@ export class SubmissionRequestDto {
 
     @IsString()
     expected_output: string
+
+    @IsNumber()
+    @IsNotEmpty()
+    language_id: number
 
     constructor(language_id: number, source_code: string, expected_output: string, stdin?: string) {
         this.language_id = language_id
