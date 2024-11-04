6b2a80cf31b40759ede8692dea657c332d6400a7
shishirbychapur <shishirbychapur@gmail.com>
2024-11-05 01:42:08 +0800

    fix: add reflect-metadata header

--- packages/submission-types/src/SubmissionRequestDto.ts
+++ packages/submission-types/src/SubmissionRequestDto.ts
@@ -18,7 +18,7 @@ export class SubmissionRequestDto {
     constructor(language_id: number, source_code: string, expected_output: string, stdin?: string) {
         this.language_id = language_id
         this.source_code = source_code
-        this.stdin = stdin || ''
+        this.stdin = stdin ?? ''
         this.expected_output = expected_output
     }
 
