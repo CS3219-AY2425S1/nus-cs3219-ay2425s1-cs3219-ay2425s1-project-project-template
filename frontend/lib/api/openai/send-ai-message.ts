3773872f88a16e46eb2ba81a93181c1e94adc7a6
dylkaw <37286651+dylkaw@users.noreply.github.com>
2024-11-04 19:19:56 +0800

    Run prettier

--- frontend/lib/api/openai/send-ai-message.ts
+++ frontend/lib/api/openai/send-ai-message.ts
@@ -1,14 +1,17 @@
 import { collabServiceUri } from "@/lib/api/api-uri";
 
 export const sendAiMessage = async (message: string) => {
-    console.log(`{collabServiceUri(window.location.hostname)}/send-ai-message`);
-    console.log(message);
-    const response = await fetch(`${collabServiceUri(window.location.hostname)}/collab/send-ai-message`, {
-      method: 'POST',
+  console.log(`{collabServiceUri(window.location.hostname)}/send-ai-message`);
+  console.log(message);
+  const response = await fetch(
+    `${collabServiceUri(window.location.hostname)}/collab/send-ai-message`,
+    {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({message: message}),
-    });
-    return response;
-}
+      body: JSON.stringify({ message: message }),
+    }
+  );
+  return response;
+};
