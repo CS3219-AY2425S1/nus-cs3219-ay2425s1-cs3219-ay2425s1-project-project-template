1716efa6ffc86697e2b8166166f5e1b2809f0902
solomonng2001 <solomonng2001@gmail.com>
2024-11-04 21:42:30 +0800

    Dockerise execution of python code

--- apps/execution-service/execution/python/python.go
+++ apps/execution-service/execution/python/python.go
@@ -3,41 +3,22 @@ package python
 import (
 	"bytes"
 	"fmt"
-	"os"
 	"os/exec"
 	"strings"
 )
 
-// RunPythonCode executes the provided Python code with the given input
 func RunPythonCode(code string, input string) (string, string, error) {
-	// Create a temporary Python file to execute
-	tmpFile, err := os.CreateTemp("", "*.py")
-	if err != nil {
-		return "", "", fmt.Errorf("failed to create temporary file: %w", err)
-	}
-	defer os.Remove(tmpFile.Name()) // Clean up the temporary file afterwards
+	cmd := exec.Command(
+		"docker", "run", "--rm",
+		"-i", // allows for standard input to be passed in
+		"python-sandbox",
+		"python", "-c", code,
+	)
 
-	// Write the provided code to the temporary file
-	if _, err := tmpFile.WriteString(code); err != nil {
-		return "", "", fmt.Errorf("failed to write code to temporary file: %w", err)
-	}
-	if err := tmpFile.Close(); err != nil {
-		return "", "", fmt.Errorf("failed to close temporary file: %w", err)
-	}
-
-	// Read the contents of the script file for debugging
-	content, err := os.ReadFile(tmpFile.Name())
-	if err != nil {
-		return "", "", fmt.Errorf("failed to read temporary file: %w", err)
-	}
-	fmt.Printf("Contents of script.py:\n%s\n", content)
-
-	// Prepare the command to execute the Python script
-	cmd := exec.Command("docker", "run", "--rm", "-v", fmt.Sprintf("%s:/app/script.py", tmpFile.Name()), "python-sandbox", "python", "/app/script.py")
-	//cmd := exec.Command("python3", tmpFile.Name())
+	// Pass in any input data to the Python script
 	cmd.Stdin = bytes.NewBufferString(input)
 
-	// Capture the output and error
+	// Capture output and error
 	var output bytes.Buffer
 	var errorOutput bytes.Buffer
 	cmd.Stdout = &output
