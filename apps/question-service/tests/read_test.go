bcd29d32534f3c48681cab3625695667c0ebf8fd
Ryan Chia <ryan_chia8@hotmail.com>
2024-11-04 15:27:42 +0800

    merge n11

--- apps/question-service/tests/read_test.go
+++ apps/question-service/tests/read_test.go
@@ -2,74 +2,74 @@ package tests
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
-	"path/filepath"
 	"question-service/handlers"
+	"question-service/utils"
 	"strings"
 	"testing"
 
 	"cloud.google.com/go/firestore"
-	firebase "firebase.google.com/go/v4"
 	"github.com/go-chi/chi/v5"
-	"github.com/joho/godotenv"
-	"google.golang.org/api/option"
 )
 
 var service *handlers.Service
-
-func createService() *handlers.Service {
-
-	ctx := context.Background()
-	client, err := initFirestore(ctx)
-
-	if err != nil {
-		log.Fatalf("failed to initialize Firestore: %v", err)
-	}
-
-	return &handlers.Service{Client: client}
-}
-
-// initFirestore initializes the Firestore client
-func initFirestore(ctx context.Context) (*firestore.Client, error) {
-	credentialsPath := "../" + os.Getenv("FIREBASE_CREDENTIAL_PATH")
-	opt := option.WithCredentialsFile(credentialsPath)
-	app, err := firebase.NewApp(ctx, nil, opt)
-	if err != nil {
-		return nil, fmt.Errorf("failed to initialize Firebase App: %v", err)
-	}
-
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get Firestore client: %v", err)
-	}
-	return client, nil
-}
+var ctx = context.Background()
 
 func TestMain(m *testing.M) {
-	err := godotenv.Load(filepath.Join("../", ".env"))
+	// Set FIRESTORE_EMULATOR_HOST environment variable.
+	err := os.Setenv("FIRESTORE_EMULATOR_HOST", "localhost:8080")
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Fatalf("could not set env %v", err)
 	}
-	service = createService()
-	defer service.Client.Close()
-	exitCode := m.Run()
-	os.Exit(exitCode)
+	// Create client.
+	client, err := firestore.NewClient(ctx, "my-project-id")
+	service = &handlers.Service{Client: client}
+
+	if err != nil {
+		log.Fatalf("could not create client %v", err)
+	}
+	defer client.Close()
+
+	m.Run()
+	os.Exit(0)
 }
 
-func Test_Read(t *testing.T) {
-	res := httptest.NewRecorder()
+// Sets up the firestore emulator with the sample questions
+// This repopulates the db
+// Returns the docref of one of the questions if a test need it
+func setupDb(t *testing.T) string {
+	// Repopulate document
+	utils.Populate(service.Client)
 
+	coll := service.Client.Collection("questions")
+	if coll == nil {
+		t.Fatalf("Failed to get CollectionRef")
+	}
+	docRef, err := coll.DocumentRefs(ctx).Next()
+	if err != nil {
+		t.Fatalf("Failed to get DocRef: %v", err)
+	}
+	return docRef.ID
+}
+
+func ReadRequestWithId(id string) *http.Request {
 	// adds chi context
 	// https://stackoverflow.com/questions/54580582/testing-chi-routes-w-path-variables
 	rctx := chi.NewRouteContext()
-	rctx.URLParams.Add("docRefID", "6SdbW4Awcfm5x0UQtWmg")
+	rctx.URLParams.Add("docRefID", id)
 
-	req := httptest.NewRequest(http.MethodGet, "http://localhost:8080/questions/6SdbW4Awcfm5x0UQtWmg", strings.NewReader(""))
+	req := httptest.NewRequest(http.MethodGet, "http://localhost:12345/questions/"+id, strings.NewReader(""))
 	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+	return req
+}
+func Test_Read(t *testing.T) {
+	id := setupDb(t)
+
+	res := httptest.NewRecorder()
+	req := ReadRequestWithId(id)
 
 	service.ReadQuestion(res, req)
 
@@ -79,19 +79,15 @@ func Test_Read(t *testing.T) {
 }
 
 func Test_ReadNotFound(t *testing.T) {
+	setupDb(t)
+
 	res := httptest.NewRecorder()
-
-	// adds chi context
-	// https://stackoverflow.com/questions/54580582/testing-chi-routes-w-path-variables
-	rctx := chi.NewRouteContext()
-	rctx.URLParams.Add("docRefID", "not-found-docref")
-
-	req := httptest.NewRequest(http.MethodGet, "http://localhost:8080/questions/not-found-docref", strings.NewReader(""))
-	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+	req := ReadRequestWithId("invalid-docref")
 
 	service.ReadQuestion(res, req)
 
 	if res.Result().StatusCode != 404 {
 		t.Fatalf("expected status code 404 but got response %v", res.Result())
 	}
+
 }
