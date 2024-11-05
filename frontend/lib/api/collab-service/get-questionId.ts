dda92173b3c0a9a57ee30cc110e7387bb870585a
jq1836 <jordanquahshaoxuan@gmail.com>
2024-11-05 19:29:12 +0800

    Fix question id not retrieving bug

--- frontend/lib/api/collab-service/get-questionId.ts
+++ frontend/lib/api/collab-service/get-questionId.ts
@@ -1,11 +1,12 @@
 import { AuthType, collabServiceUri } from "@/lib/api/api-uri";
 
-export const getQuestionId = async (roomId: string) => {
+export const getQuestionId = async (jwtToken: string, roomId: string) => {
   const response = await fetch(
-    `${collabServiceUri(window.location.hostname, AuthType.Public)}/collab/rooms/${roomId}/questionId`,
+    `${collabServiceUri(window.location.hostname, AuthType.Private)}/collab/rooms/${roomId}/questionId`,
     {
       method: "GET",
       headers: {
+        Authorization: `Bearer ${jwtToken}`,
         "Content-Type": "application/json",
       },
     }
