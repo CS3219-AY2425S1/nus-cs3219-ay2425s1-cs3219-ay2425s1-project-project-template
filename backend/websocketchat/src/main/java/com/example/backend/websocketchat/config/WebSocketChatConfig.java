fcebaadc702e44f678dee2b30ed02402c7607a14
Mai Ting Kai <wheatgrass.mc@gmail.com>
2024-11-06 17:04:06 +0800

    Add localhost:3000

--- backend/websocketchat/src/main/java/com/example/backend/websocketchat/config/WebSocketChatConfig.java
+++ backend/websocketchat/src/main/java/com/example/backend/websocketchat/config/WebSocketChatConfig.java
@@ -24,7 +24,7 @@ public class WebSocketChatConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // HTTP URL for the WebSocket connection used by the client
         registry.addEndpoint("/chat-websocket")
-                .setAllowedOriginPatterns("*")
+                .setAllowedOriginPatterns("http://localhost:3000") 
                 .setHandshakeHandler(new UserHandshakeHandler())
                 .withSockJS();
     }
