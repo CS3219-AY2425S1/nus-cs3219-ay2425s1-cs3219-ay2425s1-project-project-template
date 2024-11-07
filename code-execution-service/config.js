d0a796e2d5de5d0c529b39e9e6c9aadd37a2823f
jasonqiu212 <jason_qiu@hotmail.com>
2024-11-08 03:01:10 +0800

    fix: include jwt secret to code execution service

--- code-execution-service/config.js
+++ code-execution-service/config.js
@@ -2,5 +2,5 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
   PORT: process.env.CODEX_PORT || 8050,
-  USER_JWT_SECRET: process.env.USER_JWT_SECRET
-}
+  JWT_SECRET: process.env.JWT_SECRET,
+};
