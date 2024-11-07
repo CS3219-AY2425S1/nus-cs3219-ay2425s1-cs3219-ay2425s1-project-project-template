89a26b922b74588aef21949b29d7e4013f856e05
lambraydon <braydonlam2001@gmail.com>
2024-11-08 01:37:33 +0800

    Add cors config for chat-service

--- ChatService/app.js
+++ ChatService/app.js
@@ -1,24 +1,31 @@
 import express from 'express';
 import { Server } from 'socket.io';
-import http from 'http'; 
-import cors from 'cors'; 
-import socket from './config/socket.js'; 
+import http from 'http';
+import cors from 'cors';
+import socket from './config/socket.js';
 
 const app = express();
 const port = 3003;
 const server = http.createServer(app);
+
+const frontendURL = process.env.FRONTEND_URL || "http://localhost:8080";  
+app.use(cors({
+    origin: frontendURL,
+    credentials: true
+}));
+
 const io = new Server(server, {
     cors: {
-        origin: '*', 
-        methods: ['GET', 'POST']
+        origin: frontendURL, 
+        methods: ['GET', 'POST'],
+        credentials: true
     }
 });
 
-app.use(cors());
 app.use(express.json());
-
 socket.createSocket(io);
 
 server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
+
