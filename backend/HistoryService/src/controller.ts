f4082c8ff4a4d719fe994cd023a66cc7fb5669d9
Er Jun Ze <junzeer@gmail.com>
2024-11-09 03:07:44 +0800

    Extend history backend to track run code and output

--- backend/HistoryService/src/controller.ts
+++ backend/HistoryService/src/controller.ts
@@ -16,15 +16,27 @@ export const getUserHistory = async (req: Request, res: Response, next: NextFunc
 };
 
 export const addUserAttempt = async (req: Request, res: Response, next: NextFunction) => {
-  const { userId, qid } = req.body;
-  if (!(userId && qid && +qid > 0)) {
-    return res.status(400).json({ message: "Missing user or question id" });
+  const { userId, qid, language, code, output, error = false } = req.body;
+  if (!userId) {
+    return res.status(400).json({ message: "Missing user id" });
+  }
+  if (!(qid && +qid > 0)) {
+    return res.status(400).json({ message: "Missing question id" });
+  }
+  if (!language) {
+    return res.status(400).json({ message: "Missing attempt programming language" });
+  }
+  if (!code) {
+    return res.status(400).json({ message: "Missing attempt source code" });
+  }
+  if (!output) {
+    return res.status(400).json({ message: "Missing attempt code output" });
   }
 
   try {
     console.log(`Adding attempt by user ${userId} for Q${qid}`);
 
-    await Attempt.create({ userId, qid });
+    await Attempt.create({ userId, qid, language, code, output, error });
     res.status(200).json("User attempt recorded");
   } catch (err) {
     next(err);
