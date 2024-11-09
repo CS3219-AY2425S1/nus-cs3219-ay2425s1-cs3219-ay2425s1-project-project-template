e94a93c47d016deaf1af5cfe23842f050ea2f561
choo tze jie <119966583+tjch-o@users.noreply.github.com>
2024-11-09 20:42:27 +0800

    Add time to roomId

--- backend/collab-service/utils/utils.ts
+++ backend/collab-service/utils/utils.ts
@@ -1,7 +1,8 @@
 import { createHash } from "crypto"
 
 const createRoomId = (userId1: string, userId2: string) => {
-    const roomId = userId1 < userId2 ? `${userId1}-${userId2}` : `${userId2}-${userId1}`
+    const time = new Date().getTime().toString()
+    const roomId = userId1 < userId2 ? `${userId1}-${userId2}-${time}` : `${userId2}-${userId1}-${time}`
     const hash = createHash('sha256').update(roomId).digest('hex')
     return hash
 }
