fe053edabab16d476dcd9f10eb4b1e86e1e67ba4
Khoo Jing Xiang <122257205+meowwtama@users.noreply.github.com>
2024-11-09 20:23:23 +0800

    Update env file configurations

--- frontend-service/FirebaseConfig.ts
+++ frontend-service/FirebaseConfig.ts
@@ -9,18 +9,19 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.API_KEY || "",
-  authDomain: process.env.AUTH_DOMAIN || "",
-  databaseURL: process.env.DATABASE_URL || "",
-  projectId: process.env.PROJECT_ID || "",
-  storageBucket: process.env.STORAGE_BUCKET || "",
-  messagingSenderId: process.env.MESSAGING_SENDER_ID || "",
-  appId: process.env.APP_ID || "",
-  measurementId: process.env.MEASUREMENT_ID || "",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_FIRESTORE = getFirestore(FIREBASE_APP);
 export const FIREBASE_STORAGE = getStorage(FIREBASE_APP);
-export const FIREBASE_DB = getDatabase(FIREBASE_APP)
+export const FIREBASE_DB = getDatabase(FIREBASE_APP);
+
