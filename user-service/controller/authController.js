4c2ee2befe3371eb5f3f925b548717405c4c94f0
HoJunHao2000 <kennethhojunhao@gmail.com>
2024-11-10 21:32:08 +0800

    change forgot password url to use domain name instead of localhost

--- user-service/controller/authController.js
+++ user-service/controller/authController.js
@@ -78,7 +78,7 @@ const forgotPassword = async (req, res) => {
       subject: 'Password Reset',
       text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
              Please click on the following link, or paste this into your browser to complete the process:\n\n
-             http://localhost/reset-password/${resetToken}\n\n
+             ${process.env.DOMAIN_NAME}/reset-password/${resetToken}\n\n
              If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     };
 
