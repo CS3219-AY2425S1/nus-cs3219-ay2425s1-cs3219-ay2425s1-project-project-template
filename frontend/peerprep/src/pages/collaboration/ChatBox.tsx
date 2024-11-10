2de4e1aecb0bc77ddd0bb9007b4ab42a7cfe01d5
Hwww23 <122247552+Hwww23@users.noreply.github.com>
2024-11-10 23:37:47 +0800

    text change align

--- frontend/peerprep/src/pages/collaboration/ChatBox.tsx
+++ frontend/peerprep/src/pages/collaboration/ChatBox.tsx
@@ -105,7 +105,7 @@ const ChatBox: React.FC<ChatBoxProps> = ({
 
   const renderMessages = () => {
     const notificationMessages = [
-      "Connected, let's chat!",
+      "Your partner has connected, let's chat!",
       "Your partner has disconnected :(",
       "Start chatting to find out who you are paired with!"
     ];
