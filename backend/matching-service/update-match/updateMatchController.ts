60d67d4ec127050bd3bf7c0d38175c13cb03d7c6
choo tze jie <119966583+tjch-o@users.noreply.github.com>
2024-11-13 20:16:06 +0800

    Remove unnecessary files

--- backend/matching-service/update-match/updateMatchController.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express'
-import SuccessfulMatch from '../models/match'
-import logger from '../../collab-service/utils/logger'
-
-const updateMatch = async (req: Request, res: Response) => {}
-
-export { updateMatch }
--- backend/matching-service/update-match/updateMatchRoute.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { updateMatch } from './updateMatchController'
-
-const router = Router()
-router.patch('/update-match', updateMatch)
-
-export { router }
