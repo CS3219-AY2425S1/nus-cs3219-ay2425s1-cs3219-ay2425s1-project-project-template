0d4cb949715720c4726c683aa12ecc21e2ed4abd
jasonqiu212 <jason_qiu@hotmail.com>
2024-11-13 16:28:13 +0800

    feat: change display of questions nav bar into collapsible menu

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,18 +1,52 @@
-import { AppShell, Avatar, Group, Title, UnstyledButton } from '@mantine/core';
+import {
+  ActionIcon,
+  AppShell,
+  Avatar,
+  Group,
+  Menu,
+  Space,
+  Text,
+  Title,
+  UnstyledButton,
+} from '@mantine/core';
+import { IconHelpHexagon, IconSettings } from '@tabler/icons-react';
 import { Link } from 'react-router-dom';
+
+import classes from './Header.module.css';
 
 function Header() {
   return (
-    <AppShell.Header px="40px" pt="16px" bg="slate.9">
-      <Group justify="space-between">
-        <Group gap="48px">
-          <Link to="../dashboard">
-            <Title c="white">PeerPrep</Title>
-          </Link>
-          <Link to="../admin">
-            <Title c="white" fz="lg">Questions</Title>
-          </Link>
-        </Group>
+    <AppShell.Header px="40px" bg="slate.9">
+      <Group h="100%" justify="space-between" gap="20px">
+        <Link to="../dashboard" className="logo">
+          <Title c="white">PeerPrep</Title>
+        </Link>
+        <Space style={{ flexGrow: 1 }} />
+        <Menu
+          shadow="md"
+          width={225}
+          position="bottom-end"
+          trigger="hover"
+          openDelay={100}
+          closeDelay={400}
+        >
+          <Menu.Target>
+            <ActionIcon variant="subtle" color="white" aria-label="Settings">
+              <IconSettings size="32px" />
+            </ActionIcon>
+          </Menu.Target>
+
+          <Menu.Dropdown classNames={{ dropdown: classes.dropdown }}>
+            <Menu.Label>Admin</Menu.Label>
+            <Menu.Item leftSection={<IconHelpHexagon />}>
+              <Link to="../admin">
+                <Text c="white" fz="14px">
+                  Manage Questions
+                </Text>
+              </Link>
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
         <Link to="../profile">
           <UnstyledButton>
             <Avatar
